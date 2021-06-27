f6196897ba6af5b6af17391c9d1dd050e740ca9d
saad <saadbinomar998@outlook.com>
2021-06-27 15:34:17 +0600

    2ndTest

--- APLOS/Areas/SalesManagements/Scripts/salesController.js
+++ APLOS/Areas/SalesManagements/Scripts/salesController.js
@@ -18,6 +18,7 @@ function salesController(cboService, commonMessage, $window, $scope, $rootScope,
     $controller("partyBaseController", { $scope: $scope, $http: $http });
     $controller("baseMaterialAndArticleController", { $scope: $scope, $http: $http });
     //Test Changes
+//2nd Changes
     baseService.init("SalesManagements/Sales/GetMaterialSalesList", null, null, "DESC", "InvoiceDate", "InvoiceNo");
     $scope.getData = function (pageno) {
         baseService.pagination(pageno)
