e52a75670427faac31ea88481147859d657982ca
shakawat hossain <shakawat@apopinternational.com>
2021-06-27 14:20:47 +0600

    Comitting Log Controllers

---
+++ APLOS/Areas/Logs/Controllers/ActionLogController.cs
@@ -0,0 +1,45 @@
+﻿#region Using
+using Aplos.Controllers;
+using Aplos.Properties;
+using Library.Service.Logs;
+using Library.Data;
+using Library.Core;
+using System;
+using System.Web.Mvc;
+
+#endregion
+
+namespace Aplos.Areas.Logs.Controllers
+{
+    public class ActionLogController : BaseController
+    {
+        #region Constructor
+        private readonly IActionLogService _actionLogService;
+        public ActionLogController(IActionLogService actionLogService)
+        {
+            _actionLogService = actionLogService;
+        }
+        #endregion
+
+        public ActionResult Aplos()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult Get(GridParameter parameters, string fromDate, string toDate)
+        {
+            if (!string.IsNullOrEmpty(fromDate) && !string.IsNullOrEmpty(toDate))
+                return Json(_actionLogService.Query(parameters, Convert.ToDateTime(fromDate), Convert.ToDateTime(toDate)), JsonRequestBehavior.AllowGet);
+            else
+                throw new CustomException("Please select date from and date to.");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(DateTime fromDate, DateTime toDate)
+        {
+            _actionLogService.Delete(fromDate, toDate);
+            return Json(new { Message = AplosMessage.Success });
+        }
+    }
+}
---
+++ APLOS/Areas/Logs/Scripts/mailLogController.js
@@ -0,0 +1,43 @@
+﻿'use strict';
+mailLogController.$inject = ['commonMessage', '$scope', '$rootScope', 'baseService', '$routeParams', '$location', '$http', '$filter'];
+function mailLogController(commonMessage, $scope, $rootScope, baseService, $routeParams, $location, $http, $filter) {
+    $rootScope.title = 'Mail Logs';
+    $scope.mailLog = {
+        fromDate: $filter("dateFiltering")(Date.now()),
+        toDate: $filter("dateFiltering")(Date.now())
+    };
+
+    $scope.mailLogListParameters = {
+        limit: 10,
+        offset: 0,
+        order: 'DESC',
+        sort: 'RecordTime',
+        searchBy: "",
+        pageSize: 10,
+        total_count: 0,
+        search: null,
+        serverPagination: true
+    };
+
+    $scope.setClickedRow = function (index) {
+        $scope.selectedRow = index;
+    }
+
+    $scope.getMailLogList = function (fromDate, toDate) {
+        $scope.mailLogList = [];
+        $scope.getMailLogListpagin = function (pageno) {
+            baseService.paginationBase('Logs/MailLog/GetMailLogList?fromDate=' + fromDate + '&toDate=' + toDate, pageno, $scope.mailLogListParameters)
+                .then(function (data) {
+                    $scope.mailLogList = data.Rows;
+                    $scope.mailLogListParameters.total_count = data.Total;
+                }, function () {
+                    ShowResult(commonMessage.NetworkError, 'failure');
+                }).finally(function () {
+                });
+        };
+        $scope.getMailLogListpagin();
+    }
+    $scope.getMailLogList();
+
+ 
+}
