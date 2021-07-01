21813f1d75da325cc9bd9f14e15dc19bd86fe675
SayantoRoy <sroy.sayanto@gmail.com>
2021-07-01 13:45:28 +0530

    DropDownList Sayanto

--- APLOS/Areas/Payrolls/Scripts/salaryPaymentStatementsController.js
+++ APLOS/Areas/Payrolls/Scripts/salaryPaymentStatementsController.js
@@ -214,8 +214,9 @@ function salaryPaymentStatementsController(cboService, commonMessage, $scope, $r
     //$scope.GetSalaryPaymentStatementBankCSV = function (paymentMode) {
     //    try {
     //        var DropDownListMonth = $("#ddlMonthList").data("ejDropDownList");
-    //        var DropDownListYear = $("#ddlYearList").data("ejDropDownList");
-    //        var DropDownListBank = $("#ddlBankList").data("ejDropDownList");
+    //Sayanto
+      //      var DropDownListYear = $("#ddlYearList").data("ejDropDownList");
+         var DropDownListBank = $("#ddlBankList").data("ejDropDownList");
 
     //        $scope.month = DropDownListMonth.getSelectedValue();
     //        $scope.year = DropDownListYear.getSelectedValue();
