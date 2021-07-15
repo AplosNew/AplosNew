7c18726caffec358cda273b55b8c393734537d3f
ShashAplos <shashankssharma93@gmail.com>
2021-07-15 10:46:28 +0530

    JW  PO, Transformation mst error solved and committed.

--- Aplos.MaterialManagement/JobWork/JobWorkCommon.cs
+++ Aplos.MaterialManagement/JobWork/JobWorkCommon.cs
@@ -896,7 +896,8 @@ LEFT JOIN (SELECT A.JobWorkTransformationContractMasterId, SUM(A.Quantity) AS Tr
             {
                 string strSql = "";
 
-                strSql = @"SELECT * FROM JWTransformationPurchaseOrderDetail WHERE JWTransformationPurchaseOrderId = '" + JWPOId + @"'";
+                //strSql = @"SELECT * FROM JWTransformationPurchaseOrderDetail WHERE JWTransformationPurchaseOrderId = '" + JWPOId + @"'";
+                strSql = @"SELECT * FROM dbo.JobWorkTransformationContractChild WHERE JobWorkTransformationContractMasterId = '" + JWPOId + @"'";
 
                 dtJWPODetail = _sqlRepository.GetDataTable(strSql);
 
@@ -975,15 +976,15 @@ LEFT JOIN (SELECT A.JobWorkTransformationContractMasterId, SUM(A.Quantity) AS Tr
 						--LEFT JOIN(Select  BOQDetailId,sum(TransactionQty) TransactionQty from [TRN].[POBOQMAP] group by BOQDetailId)POMAP ON POMAP.BOQDetailId=b.Id
 						LEFT JOIN (SELECT  POBOQMAP1.BOQDetailId,sum(POBOQMAP1.TransactionQty) TransactionQty 
 									FROM JWPOBOQMAP POBOQMAP1
-									LEFT JOIN  JWTransformationPurchaseOrderDetail POD ON POD.Id=POBOQMAP1.JWPODetailId
-									LEFT JOIN JWTransformationPurchaseOrder POM ON POM.Id=POD.JWTransformationPurchaseOrderId
+									LEFT JOIN dbo.JobWorkTransformationContractChild POD ON POD.Id=POBOQMAP1.JWPODetailId
+									LEFT JOIN JWTransformationPurchaseOrder POM ON POM.Id=POD.JobWorkTransformationContractMasterId
 									WHERE POM.Id ='" + JWPOId + @"'
 									GROUP by POBOQMAP1.BOQDetailId								
 									)POMAP ON POMAP.BOQDetailId=b.Id
 						LEFT JOIN(SELECT  POBOQMAP1.BOQDetailId,sum(POBOQMAP1.POBOQQty) TransactionQty 
 									FROM JWPOBOQMAP POBOQMAP1
-									LEFT JOIN  JWTransformationPurchaseOrderDetail POD ON POD.Id=POBOQMAP1.JWPODetailId
-									LEFT JOIN JWTransformationPurchaseOrder POM ON POM.Id=POD.JWTransformationPurchaseOrderId
+									LEFT JOIN  dbo.JobWorkTransformationContractChild POD ON POD.Id=POBOQMAP1.JWPODetailId
+									LEFT JOIN JWTransformationPurchaseOrder POM ON POM.Id=POD.JobWorkTransformationContractMasterId
 									WHERE POM.Id !='" + JWPOId + @"'
 									GROUP by POBOQMAP1.BOQDetailId
 								) OtherPOData ON OtherPOData.BOQDetailId=b.Id
@@ -2852,12 +2853,15 @@ LEFT JOIN (SELECT A.JobWorkTransformationContractMasterId, SUM(A.Quantity) AS Tr
             {
                 JWPurchaseOrderId = data[0]["JWTransformationPurchaseOrderId"].ToString();
             }
-
-            con.OpenDataSetThroughAdapter("select * from dbo.JobWorkTransformationContractChild where JobActivityId='" + data[0]["JobActivityId"] + "' and JobWorkItemMasterId='" + data[0]["JobWorkItemMasterId"] + "' and ArticleCodeId='" + data[0]["ArticleId"] + "' and MaterialMasterId='"+data[0]["MaterialMasterId"] + "' and JobWorkTransformationContractMasterId='" + data[0]["JWTransformationPurchaseOrderId"] + "' AND  Id<>'" + data[0]["Id"] + "' ", out dsMaster, false, "1");
-            if (dsMaster.Tables[0].Rows.Count > 0)
+            if (type != "BOQ")
             {
-                throw new Exception("Same Activity, JW Output Item, Material and Article already exist.");
+                con.OpenDataSetThroughAdapter("select * from dbo.JobWorkTransformationContractChild where JobActivityId='" + data[0]["JobActivityId"] + "' and JobWorkItemMasterId='" + data[0]["JobWorkItemMasterId"] + "' and ArticleCodeId='" + data[0]["ArticleId"] + "' and MaterialMasterId='" + data[0]["MaterialMasterId"] + "' and JobWorkTransformationContractMasterId='" + data[0]["JWTransformationPurchaseOrderId"] + "' AND  Id<>'" + data[0]["Id"] + "' ", out dsMaster, false, "1");
+                if (dsMaster.Tables[0].Rows.Count > 0)
+                {
+                    throw new Exception("Same Activity, JW Output Item, Material and Article already exist.");
+                }
             }
+            
 
             con.OpenDataSetThroughAdapter("SELECT * FROM JobWorkTransformationContractChild WHERE JobWorkTransformationContractMasterId='" + JWPurchaseOrderId + "'", out dsMaster, false, "1");
 
@@ -2916,10 +2920,10 @@ LEFT JOIN (SELECT A.JobWorkTransformationContractMasterId, SUM(A.Quantity) AS Tr
                         if (dsMaster.Tables[0].DefaultView.Count == 0)
                         {
                             bplib.clsGenID genid = new bplib.clsGenID();
-                            genid.GenID("JWTransformationPurchaseOrderDetail", out _Id);
+                            genid.GenID("JobWorkTransformationContractChild", out _Id);
                             data[i]["Id"] = "JWPD" + _Id;
                             JWPODId = data[i]["Id"].ToString();
-                            data[i]["JWTransformationPurchaseOrderId"] = JWPurchaseOrderId;
+                            data[i]["JobWorkTransformationContractMasterId"] = JWPurchaseOrderId;
                             if (OrderSpecific == "Yes")
                             {
                                 data[i]["ReferenceNo"] = data[i]["ReferenceNoM"];
@@ -2932,7 +2936,7 @@ LEFT JOIN (SELECT A.JobWorkTransformationContractMasterId, SUM(A.Quantity) AS Tr
                         else
                         {
 
-                            data[i]["JWTransformationPurchaseOrderId"] = JWPurchaseOrderId;
+                            data[i]["JobWorkTransformationContractMasterId"] = JWPurchaseOrderId;
 
                             EditRow(dsMaster.Tables[0].DefaultView[0].Row, data[i]);
                         }
