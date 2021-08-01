dd668198e448dffbd51b89c88fca67107280eab2
Dhruv Kinger <cse1785@gmail.com>
2021-08-01 18:22:21 +0530

    New Service For Attendance Process

---
+++ Aplos.HumanResource/NewAttendanceProcess/NewAttendanceProcessService.cs
@@ -0,0 +1,25 @@
+﻿using Library.Crosscutting.Security;
+using Library.Data.Sql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Library.HumanResource.NewAttendanceProcess { 
+    public class NewAttendanceProcessService
+    {
+        SqlRepository _sqlRepository;
+        ConnectionManager.clsConnectionManager ConManager;
+        
+        public NewAttendanceProcessService()
+        {
+            _sqlRepository = new SqlRepository();
+            ConManager = new ConnectionManager.clsConnectionManager();
+
+        }
+
+    }    
+}
