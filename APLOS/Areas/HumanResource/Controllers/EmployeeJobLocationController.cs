64276e199562340e0a9f80b0597e5c34fa47b300
Mizanur Rahaman <mizanur@apopinternational.com>
2021-08-03 14:32:37 +0600

    EmployeeJobLocation

---
+++ APLOS/Areas/HumanResource/Controllers/EmployeeJobLocationController.cs
@@ -0,0 +1,40 @@
+﻿using Aplos.Controllers;
+using Aplos.Properties;
+using Library.Core;
+using Library.Model.HumanResources;
+using Library.Service.HumanResources;
+using System.Web.Mvc;
+
+namespace Aplos.Areas.HumanResource.Controllers
+{
+    public class EmployeeJobLocationController : BaseController
+    {
+        #region Constructor
+
+       // private readonly IEmployeeShiftAssignService _employeeShiftAssignService;
+
+        public EmployeeJobLocationController(
+              //IEmployeeShiftAssignService employeeShiftAssignService
+            )
+        {
+           // _employeeShiftAssignService = employeeShiftAssignService;
+        }
+
+        #endregion Constructor
+
+        #region -- Pages
+
+        
+        public ActionResult Aplos()
+        {
+            return View();
+        }
+
+        #endregion -- Pages
+
+        #region -- Operations
+
+       
+        #endregion -- Operations
+    }
+}
