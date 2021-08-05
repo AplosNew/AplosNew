609a8a27940972b40eb84a30ac07ef0b4d5f1518
saad <saad@apopinternational.com>
2021-08-05 17:45:08 +0600

    Attendance raw data from app screen added

---
+++ APLOS/Areas/Attendances/Controllers/AttendanceRawDataFromAppController.cs
@@ -0,0 +1,173 @@
+﻿#region Using
+using Aplos.Controllers;
+using Library.Model.Employees;
+using Aplos.Properties;
+using Library.Service.Employees;
+using Library.Core;
+using System.Web.Mvc;
+using Library.Crosscutting.Security;
+using System.Threading;
+using Library.Data.Sql;
+using System;
+using System.Data;
+using OTSBD;
+using clsAttendance;
+using System.Collections.Generic;
+using Library.HumanResource.Attendance.Manual;
+
+#endregion
+
+namespace Aplos.Areas.Attendances.Controllers
+{
+    public class AttendanceRawDataFromAppController : BaseController
+    {
+        #region Constructor
+        private readonly ISqlRepository _sqlRepository;
+        private readonly IStoppageService _stoppageService;
+        public AttendanceRawDataFromAppController(
+              IStoppageService stoppageService,
+              ISqlRepository sqlRepository
+            )
+        {
+            _stoppageService = stoppageService;
+            _sqlRepository = sqlRepository;
+        }
+        #endregion
+
+        #region -- Pages
+        public ActionResult Aplos()
+        {
+            return View();
+        }
+        #endregion
+
+        #region -- Operations
+
+        [HttpPost]
+        public ActionResult getAttendanceData(string employeeid, string fromdate, string todate)
+        {
+            var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+            string sql = stringAttendanceData(employeeid, fromdate, todate);
+
+
+            string shiftSQL = @" SELECT * FROM ShiftDefination AS sd WHERE sd.PlantID='" + identity.PlantId + @"'";
+
+            var jsondata = Json(new { data = _sqlRepository.GetModelCollection<AttendanceProcessData>(sql), shift = _sqlRepository.GetDataCollection(shiftSQL) }, JsonRequestBehavior.AllowGet);
+            jsondata.MaxJsonLength = int.MaxValue;
+            return jsondata;
+        }
+        private string stringAttendanceData(string employeeid, string fromdate, string todate)
+        {
+            var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+
+            if (string.IsNullOrEmpty(employeeid) == false)
+                employeeid = " AND emp.SystemId='" + employeeid + @"' ";
+            else
+            {
+                todate = fromdate;
+            }
+            return @" SELECT convert(bit, 0) AS Active,
+                            kk.Id,kk.EmployeeCode,E.UserName as Entity,
+                            emp.EmployeeName,isnull(s.UserName,'') AS Section,isnull(ss.UserName,'') AS SubSection,isnull(d.UserName,'') AS Designation,isnull(dept.UserName,'') AS Department,
+                            format(KK.WorkDate,'ddd') AS DayName, 
+                            format(KK.WorkDate,'dd-MMM-yyyy') AS WorkDate, 
+
+                            KK.ShiftSystemID,kk.ShiftName,KK.ShiftSystemID AS ShiftSystemIDOriginal,
+                            format(ShiftInTime,'dd-MMM-yyyy hh:mm tt') AS ShiftInTime,
+                     	    format(CASE WHEN KK.ShiftInTime>kk.ShiftOutTime THEN DATEADD(DAY,1,kk.ShiftOutTime) ELSE kk.ShiftOutTime END ,'dd-MMM-yyyy hh:mm tt') ShiftOutTime,
+
+
+                            format(isnull(KK.InTime,ShiftInTime),'dd-MMM-yyyy') AS  InDate,format(isnull(KK.InTime,ShiftInTime),'dd-MMM-yyyy') AS  InDateOriginal,
+                            format(KK.InTime,'hh:mm tt') AS  InTime, format(KK.InTime,'hh:mm tt') AS  InTimeOriginal, 
+
+                            KK.IsManualInTime, 
+
+
+						
+                            format(isnull(KK.OutTime,format(CASE WHEN KK.ShiftInTime>kk.ShiftOutTime THEN DATEADD(DAY,1,kk.ShiftOutTime) ELSE kk.ShiftOutTime END ,'dd-MMM-yyyy hh:mm tt')),'dd-MMM-yyyy') AS  OutDate,
+                            format(isnull(KK.OutTime,format(CASE WHEN KK.ShiftInTime>kk.ShiftOutTime THEN DATEADD(DAY,1,kk.ShiftOutTime) ELSE kk.ShiftOutTime END ,'dd-MMM-yyyy hh:mm tt')),'dd-MMM-yyyy') AS  OutDateOriginal,
+                            format(KK.OutTime,'hh:mm tt') AS  OutTime, format(KK.OutTime,'hh:mm tt') AS  OutTimeOriginal, 
+
+
+                            KK.IsManualOutTime,
+
+                            format(KK.PunchInTime,'dd-MMM-yyyy hh:mm tt') AS PunchInTime,
+                            format(KK.PunchOutTime,'dd-MMM-yyyy hh:mm tt') AS PunchOutTime,
+
+                            KK.DayStatus, KK.OTHr,
+                            KK.IsOTComfirm, KK.IsOTEntitled,KK.IsManualDayStatus
+
+                             FROM (
+								
+		                            SELECT Emp.SystemID AS Id,emp.EmployeeCode,O.WorkDate, O.ShiftSystemID,sd.UserName AS ShiftName,
+								    DATEADD(minute,DATEPART(minute, isnull(stcm.InTime, sd.Intime)), DATEADD(hour,DATEPART(hour, isnull(stcm.InTime, sd.Intime)),O.WorkDate))  AS ShiftInTime,
+		                            DATEADD(minute,DATEPART(minute, isnull(stcm.OutTime, sd.OutTime)), DATEADD(hour,DATEPART(hour, isnull(stcm.OutTime, sd.OutTime)),o.WorkDate))  AS ShiftOutTime,
+		                            O.InTime, O.IsManualInTime,
+		                            O.OutTime, O.IsManualOutTime, O.IsManualDayStatus,
+       
+		                            O.PunchInTime,O.PunchOutTime,
+		                            O.DayStatus, O.OTHr, O.IsOTComfirm,
+		                            O.IsOTEntitled
+
+		                            FROM EmployeeInformation EMP
+		                            LEFT JOIN AttdnProcessData O ON EMP.SystemID=o.EmpSystemID 
+		                            LEFT OUTER JOIN ShiftDefination AS sd ON sd.SystemID=o.ShiftSystemID
+		                            LEFT OUTER JOIN ShiftTimeChgMaster AS stcm ON o.WorkDate BETWEEN stcm.FromDate AND stcm.ToDate AND sd.SystemID=stcm.ShiftDefinationID
+                       
+                            WHERE o.WorkDate BETWEEN '" + fromdate + @"' AND '" + todate + @"'" + employeeid + @"
+                        ) AS KK
+                        LEFT OUTER JOIN ShiftDefination AS sd ON sd.SystemID=kk.ShiftSystemID
+                        LEFT OUTER JOIN ShiftTimeChgMaster AS stcm ON kk.WorkDate BETWEEN stcm.FromDate AND stcm.ToDate AND sd.SystemID=stcm.ShiftDefinationID
+						    LEFT OUTER JOIN EmployeeInformation EMP ON KK.Id=EMP.SystemID
+                            LEFT JOIN MST.ManpowerBudget PMB ON EMP.BudgetCode=PMB.Id
+                            LEFT JOIN ORG.Position PR ON PMB.PositionId=PR.Id
+                            LEFT JOIN ORG.Entity E ON PMB.EntityId=E.Id
+                            LEFT JOIN ORG.Section S ON S.Id=EMP.SectionId
+                            LEFT JOIN ORG.SubSection SS ON SS.Id=EMP.SubSectionId
+                            LEFT OUTER JOIN hkp.LegalDesignation AS D ON D.Id=EMP.LegalDesignationId
+                            LEFT JOIN ORG.Department DEPT ON EMP.DepartmentId=DEPT.Id	
+                        where emp.plantid='" + identity.PlantId + @"'
+                        ORDER BY kk.EmployeeCode,CONVERT(DATE, WorkDate) ASC ";
+        }
+
+        [HttpPost, Authorize]
+        public ActionResult getAllEmployees(string fromdate, string todate)
+        {
+            var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+            TimeSpan ts = Convert.ToDateTime(todate).Subtract(Convert.ToDateTime(fromdate));
+            if (Math.Abs(ts.TotalDays) > 31)
+                return Json(new { Error = true, Message = "Timespan between from and to date cannot be greater than 31 days" }, JsonRequestBehavior.AllowGet);
+
+            string sql = @"
+                        SELECT distinct Emp.SystemID AS Id,
+                        EMP.EmployeeName
+                        ,EMP.EmployeeCode,emp.EmployeeCodePreFix,emp.EmployeeCodeNumeric
+                        ,EMP.EmpPicPath,
+                        EMP.BudgetCode,E.UserName EntityName,isnull(D.UserName,'') Designation,
+                            PR.UserName PositionName,
+                            DEPT.UserName Department,S.UserName Section,
+                            EMP.SectionId,SS.UserName SubSection
+                            ,PL.UserName Plant
+                            FROM EmployeeInformation EMP
+                            INNER JOIN AttdnProcessData O ON EMP.SystemID=o.EmpSystemID 
+                            LEFT JOIN MST.ManpowerBudget PMB ON EMP.BudgetCode=PMB.Id
+                            LEFT JOIN ORG.Position PR ON PMB.PositionId=PR.Id
+                            LEFT JOIN ORG.Entity E ON PMB.EntityId=E.Id
+                            LEFT JOIN ORG.Section S ON S.Id=EMP.SectionId
+                            LEFT JOIN ORG.SubSection SS ON SS.Id=EMP.SubSectionId
+                            LEFT OUTER JOIN hkp.LegalDesignation AS D ON D.Id=EMP.LegalDesignationId
+                            LEFT JOIN ORG.Department DEPT ON PR.DepartmentId=DEPT.Id
+                            LEFT JOIN ORG.Plant PL ON PL.Id=EMP.PlantId
+                            LEFT JOIN HKP.Designation DEG ON EMP.GivenDesignationId=DEG.Id
+    
+                        WHERE emp.PlantId='" + identity.PlantId + @"' AND o.WorkDate BETWEEN '" + fromdate + @"' AND '" + todate + @"'
+                         order by EmployeeCodePreFix,EmployeeCodeNumeric ";
+
+            var jsondata = Json(_sqlRepository.GetDataCollection(sql), JsonRequestBehavior.AllowGet);
+            jsondata.MaxJsonLength = int.MaxValue;
+            return jsondata;
+        }
+
+        #endregion
+    }
+}
