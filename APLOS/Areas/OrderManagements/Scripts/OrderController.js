46c5676968d0490c2068cb68f0583aad2137471a
Mohiuddin Akash <akash@apopinternational.com>
2021-08-14 15:16:41 +0600

    Order

--- APLOS/Areas/OrderManagements/Scripts/OrderController.js
+++ APLOS/Areas/OrderManagements/Scripts/OrderController.js
@@ -1,11 +1,11 @@
 ﻿'use strict';
 OrderController.$inject = ['cboService', 'commonMessage', '$scope', '$rootScope', 'baseService', '$routeParams', '$location', '$http', '$filter'];
 function OrderController(cboService, commonMessage, $scope, $rootScope, baseService, $routeParams, $location, $http, $filter) {
-    $rootScope.title = 'Order';
+    $rootScope.title = 'Order Status';
     $scope.path = "OrderManagements/Order/";
 
-    $scope.fromDate = null;
-    $scope.toDate = null;
+    $scope.fromDate = '';
+    $scope.toDate = '';
     $scope.exportgriddataUrl = 'GridReports/ExcelExportJson';
     $scope.downloadgriddataUrl = 'GridReports/Download';
     $scope.downloadgriddataPDFUrl = 'GridReports/DownloadPdf';
@@ -26,18 +26,9 @@ function OrderController(cboService, commonMessage, $scope, $rootScope, baseServ
         $rootScope.dateCgroup = obj.option("value");
     }
 
-    //$scope.Report = function () {
-    //    $http({
-    //        method: 'POST',
-    //        url: $scope.path + "GetOrderReport",
-    //        data: { 'parameters': $scope.parameters, 'fromDate': $scope.fromDate, 'toDate': $scope.toDate, 'dateType': $rootScope.dateCgroup },
-    //        dataType: 'JSON'
-    //    }).then(function successCallback(response) {
-    //    })
-    //};
-
     $scope.Report = function () {
         try {
+            $scope.filterComplete();
             $scope.fileName = "OrderReport.xls";
             $http({
                 method: 'POST',
@@ -58,17 +49,7 @@ function OrderController(cboService, commonMessage, $scope, $rootScope, baseServ
         } catch (e) {
         }
     }
-    //$scope.OrderReport = function () {
 
-    //    try {
-
-    //        var file_src = $scope.path + 'ProductionEfficiencyReport?PlantId=' + $scope.SelectedPlant + '&entityid=' + $scope.SelectedEntity + "&Date=" + ($filter('dateFiltering')(new Date($scope.FromDateParameter), 'dd-MM-yyyy'));
-    //        $rootScope.report(file_src);
-
-    //    } catch (e) {
-
-    //    }
-    //}
  
     //The Filters 
     $scope.filters=[];
@@ -84,9 +65,11 @@ function OrderController(cboService, commonMessage, $scope, $rootScope, baseServ
                 { field: 'Entity', width: 20, headerText: "Entity", type: "string" },
                 { field: 'Customer', width: 20, headerText: "Customer", type: "string" },
                 { field: 'Buyer', width: 20, headerText: "Buyer", type: "string" },
-                { field: 'ProductionOrderId', width: 20, headerText: "PO Id", type: "string" },
-             
+                { field: 'ResponsiblePerson', width: 20, headerText: "Responsible Person", type: "string" },
+                      
                 { field: 'ProductionStatus', width: 20, headerText: "PO Status", type: "string" },
+                { field: 'SOStatusId', width: 20, headerText: "SO Status", type: "string" },
+                { field: 'MOStatusId', width: 20, headerText: "MO Status", type: "string" },
                 
             ];
             $("#filters").ejGrid({
@@ -123,8 +106,11 @@ function OrderController(cboService, commonMessage, $scope, $rootScope, baseServ
         parameters.push({ "Key": "EntityId", "Value": getString(fl, "EntityId") });
         parameters.push({ "Key": "CustomerId", "Value": getString(fl, "CustomerId") });
         parameters.push({ "Key": "BuyerId", "Value": getString(fl, "BuyerId") });
-        parameters.push({ "Key": "ProductionOrderId", "Value": getString(fl, "ProductionOrderId") });
+        parameters.push({ "Key": "ResponsiblePersonId", "Value": getString(fl, "ResponsiblePersonId") });
+      
         parameters.push({ "Key": "ProductionStatusId", "Value": getString(fl, "ProductionStatusId") });
+        parameters.push({ "Key": "SOStatusId", "Value": getString(fl, "SOStatusId") });
+        parameters.push({ "Key": "MOStatusId", "Value": getString(fl, "MOStatusId") });
       
         $scope.parameters = parameters;
      
