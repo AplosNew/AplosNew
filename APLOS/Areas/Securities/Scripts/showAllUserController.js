53c318a637f28d2e2f292a1e52d234e8a3b19af7
Mizanur Rahaman <mizanur@apopinternational.com>
2021-08-17 19:29:52 +0600

    Password

--- APLOS/Areas/Securities/Scripts/showAllUserController.js
+++ APLOS/Areas/Securities/Scripts/showAllUserController.js
@@ -73,17 +73,18 @@ function showAllUserController(cboService, $scope, $rootScope, baseService, $rou
         baseService.pagination(pageno)
             .then(function (result) {
                 $scope.userList = result.Rows;
-                for (var i = 0; i < $scope.userList.length; i++) {
+                //for (var i = 0; i < $scope.userList.length; i++) {
 
-                    $http({
-                        method: "get"
-                        , url: "Securities/controladmin/decrypttext?decrypttxt=" + encodeURIComponent($scope.userList[i].Password)
-                        , dataType: 'JSON'
-                    }).then(function successCallback(response) {
-                        $scope.userList[i].Password = response.data;
-                    });
+                //    $http({
+                //        method: "get"
+                //        , url: "Securities/controladmin/decrypttext?decrypttxt=" + encodeURIComponent($scope.userList[i].Password)
+                //        , dataType: 'JSON'
+                //    }).then(function successCallback(response) {
+                //        $scope.userList[i].PW = response.data;
+                //    });
 
-                }
+                //}
+                //console.log($scope.userList);
             }, function () {
                 ShowResult(commonMessage.NetworkError, 'failure');
             }).finally(function () {
