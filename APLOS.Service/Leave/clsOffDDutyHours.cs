8fc3fd829bd972fc802288e20b553ef4021c1750
Optimus Saad <saad@apopinternational.com>
2021-08-23 18:24:16 +0600

    Emp. Wise Fixed OT Updated

--- APLOS.Service/Leave/clsOffDDutyHours.cs
+++ APLOS.Service/Leave/clsOffDDutyHours.cs
@@ -558,12 +558,12 @@ namespace Library.Service.Leave
         //public DateTime? EffectiveDate { get; set; }
 
         public string EmpSystemId { get; set; }
-        public int MaximumOT { get; set; }
+        //public int MaximumOT { get; set; }
         public decimal MaximumOTLimitPerWeekDay { get; set; }
         public decimal MaximumOTLimitPerHoliDay { get; set; }
         public decimal MaximumOTLimitPerWeekend { get; set; }
         public decimal MaximumOTLimitPerMonth { get; set; }
-        public bool IsExcessAllowed { get; set; }
+        //public bool IsExcessAllowed { get; set; }
         public bool IsMinimumOTLimit { get; set; }
 
 
