9ac6253b4605b5396ea65db30b1340ec8d0ecb17
shakawat hossain <shakawat@apopinternational.com>
2021-09-16 12:16:08 +0600

    issue

--- APLOS/Areas/Products/Controllers/InventoryIssueController.cs
+++ APLOS/Areas/Products/Controllers/InventoryIssueController.cs
@@ -3998,9 +3998,9 @@ namespace Aplos.Areas.Products.Controllers
 								,''ExpiryDate
 								,''BLAWBNo,''BLAWBDate
 								,''PaymentTerm,''BaseOnDueDate
-								,0NoOfDays
+								,0 NoOfDays
 							    ,''MatureDate
-								,0LCAmount
+								,0 LCAmount
 								,''ExFactoryDate
 								,''TransportAgent	
 
