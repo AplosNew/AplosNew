abb630d6206fb9ddaf6026f2153ae327a1098036
Nurul Huda Bhulyan Babu <nurulhuda@apopinternational.com>
2021-09-19 17:34:09 +0600

    EntityConfig

--- APLOS.Service/Vouchers/IVoucherReportService.cs
+++ APLOS.Service/Vouchers/IVoucherReportService.cs
@@ -26,6 +26,6 @@ namespace Library.Service.Vouchers
         IWorkbook GetIncomeStatementReportDateWise(string companyId, string PlantId, string plantName, string fromDate, string toDate, string[] parallelCurrencies);
         IWorkbook GetBalanceSheetReportDateWise(string companyId, string plantName, string fromDate, string toDate);
 
-        IWorkbook GetEntityWiseExpenseAndEarningReportDateWise(string companyId, string PlantId, string plantName, string fromDate, string toDate, string entityId, string[] parallelCurrencies);
+        IWorkbook GetEntityWiseExpenseAndEarningReportDateWise(string companyId, string PlantId, string plantName, string fromDate, string toDate, string entityId, string entity, string[] parallelCurrencies);
     }
 }
