da5d80957839514452d25ae7e6ced03374a6b1d4
Belayet Rubel Ahmed <belayet@apopinternational.com>
2021-09-21 14:44:12 +0600

    Issue Journal Modification

--- APLOS/Areas/Accounts/Controllers/VoucherParkController.cs
+++ APLOS/Areas/Accounts/Controllers/VoucherParkController.cs
@@ -179,6 +179,11 @@ namespace Aplos.Areas.Accounts.Controllers
                     rdBuilder.Append(voucherSql);
                     rdBuilder.Append(invoiceSql);
                 }
+                if (sourceType == SourceType.IssueJournal.ToString())
+                {
+                    var voucherSql = @"UPDATE [TRN].Voucher SET ISPark=1 WHERE Id='" + voucherId + "'";
+                    rdBuilder.Append(voucherSql);
+                }
                 _sqlRepository.ExecuteSqlCommand(rdBuilder.ToString());
                 _unitOfWork.SaveChanges();
                 flag = false;
