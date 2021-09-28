47ca34102b2ea471fabb8a6d4914712f9898d052
ShashAplos <shashankssharma93@gmail.com>
2021-09-28 21:26:18 +0530

    JW Val Added without Material Receive committed.

--- APLOS/Areas/JobWork/Scripts/JobWorkReceiptValueAddedController.js
+++ APLOS/Areas/JobWork/Scripts/JobWorkReceiptValueAddedController.js
@@ -2997,7 +2997,7 @@ function JobWorkReceiptValueAddedController($window, cboService, commonMessage, 
 								//		var IssuedMatInputCount = $scope.GetIssuedMatInputList.length;
 
 								for (var i = 0; i < $scope.GetMaterialInputList.length; i++) {
-									if (baseService.isUndefinedOrNull($scope.GetMaterialInputList.ArticleId)) {
+									if (baseService.isUndefinedOrNull($scope.GetMaterialInputList[i].ArticleId)) {
 										var getjwiRow = $filter("filter")($scope.GetIssuedMatInputList, { "JWTCInputId": $scope.GetMaterialInputList[i].JobWorkItemId });
 										if (getjwiRow.length == 0) {
 											x.check = false;
@@ -3062,7 +3062,7 @@ function JobWorkReceiptValueAddedController($window, cboService, commonMessage, 
 				$scope.GetMaterialInputList = [];
 				$http({
 					method: 'GET',
-					url: $scope.path + 'GetIfIssuedOrNotValAdded?JWOutputId=' + $scope.JWPOId
+					url: $scope.path + 'GetIfIssuedOrNotValAdded?JWPOId=' + $scope.JWPOId + '&JWOutputId=' + $scope.JWOutputId
 				}).then(function successCallback(response) {
 					$scope.GetMaterialInputList = response.data;
 					if ($scope.GetMaterialInputList.length > 0) {
@@ -3078,8 +3078,8 @@ function JobWorkReceiptValueAddedController($window, cboService, commonMessage, 
 								//		var IssuedMatInputCount = $scope.GetIssuedMatInputList.length;
 
 								for (var i = 0; i < $scope.GetMaterialInputList.length; i++) {
-									if (baseService.isUndefinedOrNull($scope.GetMaterialInputList.ArticleId)) {
-										var getjwiRow = $filter("filter")($scope.GetIssuedMatInputList, { "JWTCInputId": $scope.GetMaterialInputList[i].JobWorkItemMasterId });
+									if (baseService.isUndefinedOrNull($scope.GetMaterialInputList[i].ArticleId)) {
+										var getjwiRow = $filter("filter")($scope.GetIssuedMatInputList, { "JWOrderWiseId": $scope.GetMaterialInputList[i].JWOrderWiseId });
 										if (getjwiRow.length == 0) {
 											x.check = false;
 											ShowResult("This Output detail Id " + $scope.JWOutputId + " cannot be received ");
