84a3c22c942620e401c47ece0576cb5b4b7b1dad
Belayet Rubel Ahmed <belayet@apopinternational.com>
2021-10-05 15:28:42 +0600

    Accounts Invenotry Payable service

--- APLOS/Areas/Accounts/Controllers/PostInvoiceController.cs
+++ APLOS/Areas/Accounts/Controllers/PostInvoiceController.cs
@@ -255,7 +255,13 @@ namespace Aplos.Areas.Accounts.Controllers
             AccountsInventoryPayableService _accountsInventoryPayableService = new AccountsInventoryPayableService(_sqlRepository);
             return Json(_accountsInventoryPayableService.GetPostableList(id), JsonRequestBehavior.AllowGet);
         }
-
+        [HttpPost, Authorize]
+        public ActionResult GetPostableJVList(string id,string partyId)
+        {
+            var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+            AccountsInventoryPayableService _accountsInventoryPayableService = new AccountsInventoryPayableService(_sqlRepository);
+            return Json(_accountsInventoryPayableService.GetPostableJVList(identity.CompanyId, identity.PlantId, id,partyId), JsonRequestBehavior.AllowGet);
+        }
         #endregion
 
     }
