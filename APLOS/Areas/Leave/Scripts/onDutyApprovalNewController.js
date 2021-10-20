aaf46378e3c61085fe38bf5d4908779927feffa5
Dhruv Kinger <cse1785@gmail.com>
2021-10-20 12:39:57 +0530

    js file updated for new od screen

--- APLOS/Areas/Leave/Scripts/onDutyApprovalNewController.js
+++ APLOS/Areas/Leave/Scripts/onDutyApprovalNewController.js
@@ -43,7 +43,7 @@ function onDutyApprovalNewController(commonMessage, $scope, $rootScope, baseServ
                 }                             
             }
             
-            var data = ej.DataManager($scope.EmployeeListNew).executeLocal(ej.Query().select(["FromDate", "ToDate","EmpSystemId"]));
+            var data = ej.DataManager($scope.EmployeeListNew).executeLocal(ej.Query().select(["FromDate", "ToDate","EmpSystemId","MinTime","MaxTime"]));
 
 
             if ($scope.EmployeeListNew.length == 0) {
