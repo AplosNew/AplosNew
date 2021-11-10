ae893b3d500bdde9ff91986db8d0be04560474cc
Belayet Rubel Ahmed <belayet@apopinternational.com>
2021-11-10 09:56:44 +0600

    Revert "outsource save modification"
    This reverts commit 6a0cb2f9706d34690bde114c9b984b2400a8a897.

--- Aplos.MaterialManagement/Inventory/InventoryIssueService.cs
+++ Aplos.MaterialManagement/Inventory/InventoryIssueService.cs
@@ -12839,6 +12839,7 @@ namespace Library.MaterialManagement.Inventory
                                             rdBuilder.Append(builderSql);
 
                                             // End
+
                                         }
                                         issueDetail.PolicyAmount = tempPolicyAmount;
                                         issueDetail.PolicyRate = Math.Round(tempPolicyAmount / issueDetail.TransactionQty,4);
