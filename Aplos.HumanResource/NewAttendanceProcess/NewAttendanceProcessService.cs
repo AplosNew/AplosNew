723cf83f7ec5babf0f2686d446a6fa61fddc2549
Tarek Talukder <tarek@apopinternational.com>
2021-11-23 10:39:35 +0600

    Monthly Leave summary Salary process Monthly Leave Summary OT Limit Out Time Trimming Logic Change

--- Aplos.HumanResource/NewAttendanceProcess/NewAttendanceProcessService.cs
+++ Aplos.HumanResource/NewAttendanceProcess/NewAttendanceProcessService.cs
@@ -2997,7 +2997,7 @@ namespace Library.HumanResource.NewAttendanceProcess {
 									        where p.ManualFlag=1 and dt.DayType=p.DayStatus 
 											and IsOTEntitled=1 and isOTConfirmationAuto=1
 											and DayTypeOtApplicable=0
-									        and ei.PlantId='" + PlantId+@"' order by WorkDate asc";
+									        and ei.PlantId='" + PlantId+@"' --order by WorkDate asc";
                 objCon = new ConnectionManager.DAL.ConManager("1");
                 objCon.OpenDataSetThroughAdapter(sql, out ds, false, false, "", "1");
             }
