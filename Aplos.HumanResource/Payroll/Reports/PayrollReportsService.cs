c176e6698aab3c690a14f1a53ec3dfeb73b531ab
Sayanto Roy <sayanto@apopinternational.com>
2021-12-07 10:57:23 +0530

    Commit - Dynamic Reeport

--- Aplos.HumanResource/Payroll/Reports/PayrollReportsService.cs
+++ Aplos.HumanResource/Payroll/Reports/PayrollReportsService.cs
@@ -13513,7 +13513,7 @@ ELSE CONVERT(BIT,0) END  ---No
 									left join [HKP].[Bank] bb on bb.Id = SPLD.BankSystemID
 									left join [HKP].[BankBranch] bbranch on bbranch.Id = SPLD.BankBranchId
                                     left join [dbo].[EmployeeCodeType] ect on ect.Id=e.EmployeeCodeTypeId
-                                     WHERE 1=1 " + strDOJ + @" and ect.IsOutSider =0 and  e.EmployeeCodeTypeId in ( "+typeId+@")
+                                     WHERE 1=1 " + strDOJ + @"  and  e.EmployeeCodeTypeId in ( "+typeId+@")
                                             " + wcPayrollGroup + @"                                
                                      ) DD " + wcEmpStatus + @" ORDER BY ISNULL(EmployeeCodePreFix,''),ISNULL(EmployeeCodeNumeric,0)";
                 return _sqlRepository.GetDataCollection(cmdText);
