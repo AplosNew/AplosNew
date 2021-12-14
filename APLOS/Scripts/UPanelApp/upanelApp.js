1efc5bb254a70afe37a2844f04ecc09afcc1664e
Mohiuddin Akash <akash@apopinternational.com>
2021-12-14 20:17:10 +0600

    Proforma Invoice

--- APLOS/Scripts/UPanelApp/upanelApp.js
+++ APLOS/Scripts/UPanelApp/upanelApp.js
@@ -981,6 +981,8 @@ var upanelApp = angular
     .controller('JWReceiptController', JWReceiptController)
     .controller('LeavesChecklistReportNewController', LeavesChecklistReportNewController)
     .controller('WasteTransactionReportController', WasteTransactionReportController)
+    .controller('ProformaInvoiceController', ProformaInvoiceController)
+
 
     .config(AccessControllerConfig)
     .config(accountConfig)
