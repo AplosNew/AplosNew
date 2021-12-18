be18405c516ea120618b73513623e2aeee33580b
Dhruv Kinger <cse1785@gmail.com>
2021-12-18 16:59:02 +0530

    OT FInal Information New Controller

--- APLOS/Scripts/UPanelApp/upanelApp.js
+++ APLOS/Scripts/UPanelApp/upanelApp.js
@@ -289,6 +289,7 @@ var upanelApp = angular
     .controller("OTAdjustmentController", OTAdjustmentController)
     .controller("otFinalController", otFinalController)
     .controller("otFinalInformationController", otFinalInformationController)
+    .controller("otFinalInformationNewController", otFinalInformationNewController)
     .controller("OTManagementController", OTManagementController)
     .controller("ourStyleController", OurStyleController)
     .controller("packingListMasterController", packingListMasterController)
