f544a052faa1911e10e3ce25acd3e3cd80523ddd
Optimus Saad <saad@apopinternational.com>
2021-12-23 17:08:57 +0600

    Pi Wise Sales added

---
+++ APLOS/Areas/Commercial/Controllers/PIInvoiceController.cs
@@ -0,0 +1,58 @@
+﻿#region Using
+
+using Aplos.Controllers;
+using Aplos.Properties;
+using Library.Core;
+using Library.Crosscutting.Security;
+using Library.Data;
+using Library.Data.Sql;
+using Library.Data.UnitOfWorks;
+using Library.Model.Commercial;
+using Library.Model.Enums;
+using Library.Model.Taxations;
+using Library.Service.Enums;
+using Library.Service.Helpers;
+using Library.Service.Invoices;
+using Library.ViewModel.Invoices;
+using Library.ViewModel.OrderManagements;
+using Library.ViewModel.Vouchers;
+using Newtonsoft.Json;
+using OTSBD;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+#endregion
+
+namespace Aplos.Areas.Commercial.Controllers
+{
+    public class PIInvoiceController : BaseController
+    {
+        #region Constructor
+        private readonly ISqlRepository _sqlRepository;
+
+        public PIInvoiceController(ISqlRepository R)
+        {
+            _sqlRepository = R;
+        }
+        #endregion
+
+        #region -- Pages
+       
+        public ActionResult Aplos()
+        {
+            return View();
+        }
+        #endregion
+
+        #region -- Operations
+
+        #endregion
+    }
+
+
+}
---
+++ APLOS/Areas/Commercial/Scripts/PIInvoiceController.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+PIInvoiceController.$inject = ['accountService', 'commonMessage', '$scope', '$rootScope', 'baseService', '$routeParams', '$location', '$http', '$filter', '$window', 'cboService', 'bankService', '$controller'];
+function PIInvoiceController(accountService, commonMessage, $scope, $rootScope, baseService, $routeParams, $location, $http, $filter, $window, cboService, bankService, $controller) {
+    $rootScope.title = "PI wise sales";
+    $scope.Action = 'Save';
+    $scope.path = 'Commercial/PIInvoice/';
+    $scope.getListUrl = $scope.path + 'getlist';
+    $scope.saveUrl = $scope.path + 'create';
+    $scope.saveChargesUrl = $scope.path + 'CreateCharge';
+    $scope.deleteUrl = $scope.path + 'delete/';
+    
+
+}
+
+
+
+
+
+
