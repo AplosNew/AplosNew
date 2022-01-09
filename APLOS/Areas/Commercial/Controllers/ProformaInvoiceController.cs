555601a565709be54f91f75dfb84a89594f972de
Mohiuddin Akash <akash@apopinternational.com>
2022-01-09 18:06:32 +0600

    PI

--- APLOS/Areas/Commercial/Controllers/ProformaInvoiceController.cs
+++ APLOS/Areas/Commercial/Controllers/ProformaInvoiceController.cs
@@ -793,49 +793,21 @@ left outer join TermsAndConditionsPIChild TC on TC.Id=TCD.TermsAndConditionsPICh
             }
         }
 
-        //public JsonResult PoformaInvoiceReport(string PIMasterId, string PIVersionId)
-        //{
-        //    try
-        //    {
-        //        #region Validations
-        //        //if (DataList.Count == 0)
-        //        //{
-        //        //    throw new Exception("Select from Invoice list ");
-        //        //}
-        //        //for (int i = 0; i < DataList.Count; i++)
-        //        //{
-        //        //    if (DataList[i]["PartyId"].ToString() != LcData["VendorId"].ToString())
-        //        //    {
-        //        //        throw new Exception("Vendor should be matched with Purchase LC for [" + DataList[i]["PartyPlantName"].ToString() + "]");
-        //        //    }
-        //        //    if (DataList[i]["CurrencyId"].ToString() != LcData["CurrencyId"].ToString())
-        //        //    {
-        //        //        throw new Exception("Currency should be matched with Purchase LC for [" + DataList[i]["PartyPlantName"].ToString() + "]");
-        //        //    }
-        //        //}
-        //        #endregion
-        //        string PackingListMasterId = PI.Save(PIPackingListMasterData, MaterialData, DataList);
-        //        return Json(new { Error = false, Message = AplosMessage.Updated, PIPackingListMasterId = PackingListMasterId });
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return Json(new { Error = true, Message = ex.Message });
-        //    }
-        //}
+       
 
-        //[HttpGet, Authorize]
-        //public ActionResult PoformaInvoiceReport(string PIMasterId, string PIVersionId)
-        //{
-        //    try
-        //    {
-        //        PI.PoformaInvoiceReport(PIMasterId, PIVersionId);
+        [HttpGet, Authorize]
+        public ActionResult PoformaInvoiceReport(string PIMasterId, string PIVersionId)
+        {
+            try
+            {
+                PI.PoformaInvoiceReport(PIMasterId, PIVersionId);
 
-        //        return null;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        throw ex;
-        //    }
-        //}
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
