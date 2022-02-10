2ec71891452a75f97ec844223650d9145c0ae079
Optimus Saad <saad@apopinternational.com>
2022-02-10 17:33:18 +0600

    Search updated

--- APLOS/Areas/Costings/Controllers/QuickCostingMasterController.cs
+++ APLOS/Areas/Costings/Controllers/QuickCostingMasterController.cs
@@ -3386,6 +3386,39 @@ namespace Aplos.Areas.Costings.Controllers
                 return Json(new { Error = true, Message = ex.Message }, JsonRequestBehavior.AllowGet);
             }
         }
+        [HttpPost, Authorize]
+        public ActionResult GetSubMaterialSelection(string CostingMasterTemplateId, string costingComponentId, string Segment)
+        {
+
+
+            string TableName = "";
+            if (Segment == CostingSegment.DirectMaterial.ToString())
+                TableName = "PreCostingDirectMaterial";
+            else if (Segment == CostingSegment.DirectProcess.ToString())
+                TableName = "PreCostingDirectProcess";
+            else if (Segment == CostingSegment.Operation.ToString())
+                TableName = "PreCostingOperation";
+            else if (Segment == CostingSegment.Profit.ToString())
+                TableName = "PreCostingProfit";
+            else if (Segment == CostingSegment.SalesExpense.ToString())
+                TableName = "PreCostingSalesExpense";
+            else if (Segment == CostingSegment.ValueLoss.ToString())
+                TableName = "PreCostingValueLoss";
+
+
+            string sql = @"SELECT ci.ShortName,cat.UserName AS CostingCategory, CONVERT(BIT, CASE WHEN isnull(o.Id,'')<>'' THEN 1 ELSE 0 END) AS Selected, ci.CostingComponentId,ci.Id as CostingItemId,  ci.UserName,ci.Code,ci.Sequence, ci.StandardName, 
+                        o.CostingMasterTemplateId,
+                            ci.MinimumOfQuantity, ci.POIssueDeadLine,ci.UnitOfMeasurementId,cc.UserName as CostingComponent,cc.Id as CostingComponentId, 
+                            ci.POIssueDeadLine, ci.Wastage,ci.Description
+                            from hkp.CostingItem ci 
+                            left join hkp.CostingComponent cc on cc.Id = ci.CostingComponentId
+                            LEFT OUTER JOIN hkp.CostingCategory AS cat ON cat.Id=ci.CostingCategoryId
+                            LEFT join " + TableName + @" o on o.CostingItemId = ci.Id AND o.CostingMasterTemplateId='" + CostingMasterTemplateId + @"'
+                            WHERE ci.CostingComponentId='" + costingComponentId + @"'
+                            ORDER BY CONVERT(BIT, CASE WHEN isnull(o.Id,'')<>'' THEN 1 ELSE 0 END), ci.Sequence";
+
+            return Json(_sqlRepository.GetDataCollection(sql, null), JsonRequestBehavior.AllowGet);
+        }
         #endregion
     }
 
