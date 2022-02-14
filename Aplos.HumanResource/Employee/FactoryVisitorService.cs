8df4b975b523d92be8ba75acf217ace6668d22f8
Dhruv Kinger <cse1785@gmail.com>
2022-02-14 14:43:06 +0530

    API To Save Expected Visit in Future !!

--- Aplos.HumanResource/Employee/FactoryVisitorService.cs
+++ Aplos.HumanResource/Employee/FactoryVisitorService.cs
@@ -29,7 +29,7 @@ namespace Library.HumanResource.Employee
                     return "";
                 List<VisitorModel> items = DataToSave.ToList();
 
-                con.OpenDataSetThroughAdapter("select * from dbo.ItemScan where Id='" + items[0].Id + "'", out dsMaster, false, "1");
+                con.OpenDataSetThroughAdapter("select * from dbo.VisitorServiceData where 1=2", out dsMaster, false, "1");
 
                 foreach (VisitorModel item in DataToSave)
                 {
@@ -37,44 +37,26 @@ namespace Library.HumanResource.Employee
                     if (dsMaster.Tables[0].Rows.Count == 0)
                     {
                         DataRow dr = dsMaster.Tables[0].NewRow();
-
-
-                        bplib.clsGenID id = new bplib.clsGenID();
-                        id.GenIDYearly(DateTime.Now.ToShortDateString(), "Item Scan", out string NewId);
-
-
-
-                        dr["Id"] = NewId;
-                        dr["WorkDate"] = item.WorkDate;
-                        dr["Time"] = item.Time;
-                        dr["ShiftId"] = item.ShiftId;
-                        dr["Grade"] = item.Grade;
-                        dr["LocMasterId"] = item.LocMasterId;
-                        dr["PurposeId"] = item.PurposeId;
+                        clsGenID genid = new clsGenID();
+                        genid.GenID("VisitorService", out string _Id);
+                     
+                        dr["Id"] = "VSD"+ _Id;
+                        dr["CardNo"] = DBNull.Value;
+                        dr["ExpectedDate"] = item.ExpectedDate;
+                        dr["ExpectedTime"] = item.ExpectedTime;
+                        dr["VisitorCategory"] = item.VisitorCategory;
+                        dr["VisitorType"] = item.VisitorType;
+                        dr["VisitorName"] = item.VisitorName;
+                        dr["ToMeet"] = item.ToMeet;
+                        dr["Purpose"] = item.Purpose;
                         dr["Remarks"] = item.Remarks;
+                        dr["InDone"] = false;
+                        dr["OutDone"] = false;
                         dr["AddedBy"] = item.AddedBy;
                         dr["AddedDate"] = DateTime.Now.ToString();
+                        dr["AddedFromIP"] = item.AddedFromIP;
                         dsMaster.Tables[0].Rows.Add(dr);
-
-
-                    }
-                    else
-                    {
-                        DataRow dr = dsMaster.Tables[0].DefaultView[0].Row;
-                        dr.BeginEdit();
-
-                        dr["WorkDate"] = item.WorkDate;
-                        dr["Time"] = item.Time;
-                        dr["ShiftId"] = item.ShiftId;
-                        dr["Grade"] = item.Grade;
-                        dr["LocMasterId"] = item.LocMasterId;
-                        dr["PurposeId"] = item.PurposeId;
-                        dr["Remarks"] = item.Remarks;
-                        dr["UpdatedBy"] = item.UpdatedBy;
-                        dr["UpdatedDate"] = DateTime.Now.ToString();
-
-                        dr.EndEdit();
-                    }
+                    }                   
 
                 }
                 clsStaticInfo _info = new clsStaticInfo();
@@ -92,22 +74,37 @@ namespace Library.HumanResource.Employee
     }
 
     public class VisitorModel
-    {
-        public string Id { get; set; }
+    {       
+        #region Fixed Fields
         public string AddedBy { get; set; }
         public DateTime AddedDate { get; set; }
         public string UpdatedBy { get; set; }
         public DateTime? UpdatedDate { get; set; }
         public string AddedFromIP { get; set; }
         public string UpdatedFromIP { get; set; }
-        public string PurposeId { get; set; }
-        public string Remarks { get; set; }
-        public DateTime WorkDate { get; set; }
-        public DateTime Time { get; set; }
-        public string Grade { get; set; }
-        public string LocMasterId { get; set; }
-        public string ShiftId { get; set; }
+        #endregion
 
+        #region Other Fields
+        public string Id { get; set; }
+        public string CardNo { get; set; }
+        public string Purpose { get; set; }
+        public string Remarks { get; set; }
+        public DateTime ExpectedDate { get; set; }
+        public DateTime ExpectedTime { get; set; }
+        public DateTime InDate { get; set; }
+        public DateTime InTime { get; set; }
+        public DateTime OutDate { get; set; }
+        public DateTime OutTime { get; set; }
+        public string VisitorType { get; set; }
+        public string VisitorCategory { get; set; }
+        public string VisitorName { get; set; }
+        public string MobileNo { get; set; }
+        public string ToMeet { get; set; }
+        public decimal NoOfPerson { get; set; }
+        public string InDone { get; set; }
+        public string OutDone { get; set; }
+
+        #endregion
     }
 
 }
