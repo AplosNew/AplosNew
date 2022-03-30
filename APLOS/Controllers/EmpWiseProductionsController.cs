72fba86097e1215d5db13ed9517de92fc70f7c43
Dhruv Kinger <cse1785@gmail.com>
2022-03-30 11:40:28 +0530

    New Controller SetUp !!

---
+++ APLOS/Controllers/EmpWiseProductionsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Library.OrderManagement.Production;
+using APLOS;
+
+namespace Aplos.Controllers
+{
+    [BasicAuthentication]
+    public class EmpWiseProductionsController : ApiController
+    {
+        #region Constructor
+        EmployeeOperationsService _empOpt = new EmployeeOperationsService();
+
+        public EmpWiseProductionsController()
+        {
+            _empOpt = new EmployeeOperationsService();
+        }
+
+        #endregion Constructor
+
+        [HttpGet]
+        public IHttpActionResult GetPeriod()
+        {
+            try
+            {
+                var result = _empOpt.GetPeriod();
+                return Json(result);
+            }
+            catch (Exception ex)
+            {
+                var resp = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    ReasonPhrase = ex.Message
+                };
+                throw new HttpResponseException(resp);
+            }
+        }
+
+    }
+}
