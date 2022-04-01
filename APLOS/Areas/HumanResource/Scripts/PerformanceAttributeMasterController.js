2105d5567946a2a356ae07ce7e5d61e6f45f604a
Dhruv Kinger <cse1785@gmail.com>
2022-04-01 16:16:03 +0530

    Included in Project !!

--- APLOS/Areas/HumanResource/Scripts/PerformanceAttributeMasterController.js
+++ APLOS/Areas/HumanResource/Scripts/PerformanceAttributeMasterController.js
@@ -4,7 +4,7 @@ function PerformanceAttributeMasterController(cboService, commonMessage, $scope,
     $rootScope.title = 'Performance Attribute Master';
     $scope.Action = 'Save';
     $scope.path = 'HumanResource/PerformanceAttributeMaster/';
-    $scope.saveUrl = $scope.path + 'create';
+    $scope.saveUrl = $scope.path + 'Create';
     $scope.deleteUrl = $scope.path + 'delete/';
    
     $scope.ModelTemp = {
