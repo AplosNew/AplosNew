a15e678aee9389b3c5a443f947f0c99d56773aab
Radhika Goyal <rg@apopinternational.com>
2022-04-08 15:01:56 +0000

    Updated QueryController.cs

--- APLOS/Controllers/QueryController.cs
+++ APLOS/Controllers/QueryController.cs
@@ -1,6 +1,16 @@
 ﻿using Library.Data.Sql;
 using System.Text;
 using System.Web.Mvc;
+using APLOS;
+using Library.HumanResource.NewAttendanceProcess;
+using Library.Model.EmployeeServices;
+using Library.Service.EmployeeServices;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace Aplos.Controllers
 {
