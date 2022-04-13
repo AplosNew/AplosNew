a60d829907d5bf91425254b6d6f134456b5a7ea0
Mizanur Rahman <mizanur@apopinternational.com>
2022-04-13 11:07:51 +0600

    CostingBOQSalesOrder table droppd

---
+++ APLOS/Areas/Costings/Scripts/BOQCostingApprovalSettingController.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+BOQCostingApprovalSettingController.$inject = ['cboService', 'commonMessage', '$scope', '$rootScope', 'baseService', '$routeParams', '$location', '$http', '$filter', '$controller', '$window'];
+function BOQCostingApprovalSettingController(cboService, commonMessage, $scope, $rootScope, baseService, $routeParams, $location, $http, $filter, $controller, $window) {
+    $rootScope.title = 'BOQ Approval Setting';
+    $scope.path = "Costings/BOQCostingApprovalSetting/";
+    $scope.ModelBase = { Id: null, CustomerId: null, CustomerName: null, EmployeeSystemId: null, EmployeeName: null, Remarks: null, UserName: null };
+    $scope.Model = Object.assign({}, $scope.ModelBase);
+
+
+    $scope.tab = 1;
+    $scope.setTab = function (newTab) {
+        $scope.tab = newTab;
+    };
+    $scope.isSet = function (tabNum) {
+        return $scope.tab === tabNum;
+    };
+
+
+   
+}
+
