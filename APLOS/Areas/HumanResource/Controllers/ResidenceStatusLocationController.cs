733da3edac6787592c8eb6ec193eb2c6c8e86357
niteshr88 <niteshr88@gmail.com>
2022-04-30 11:05:27 +0530

    Uploading ResidenceMaster for publish

---
+++ APLOS/Areas/HumanResource/Controllers/ResidenceStatusLocationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aplos.Areas.HumanResource.Controllers
+{
+    public class ResidenceStatusLocationController : Controller
+    {
+        // GET: HumanResource/ResidenceStatusLocation
+        public ActionResult Aplos()
+        {
+            return View();
+        }
+    }
+}
---
+++ APLOS/Areas/HumanResource/Scripts/ResidenceStatusLocationController.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+ResidenceStatusLocationController.$inject = ['cboService', 'commonMessage', '$scope', '$rootScope', 'baseService', '$routeParams', '$location', '$http', '$filter'];
+function ResidenceStatusLocationController(cboService, commonMessage, $scope, $rootScope, baseService, $routeParams, $location, $http, $filter) {
+    $rootScope.title = 'Residence Status Loacation';
+    $scope.Action = 'Save';
+    $scope.ModelList = [];
+    $scope.path = 'HumanResource/ResidenceStatusLocation/';
+    $scope.getListUrl = $scope.path + 'getlist';
+    $scope.saveUrl = $scope.path + 'Save';
+    $scope.deleteUrl = $scope.path + 'delete/';
+    baseService.init($scope.getListUrl);
+}
