8b50e35432560e6648d2444d08df5a2335e75d5b
Belayet Rubel ahmed <belayet@apopinternational.com>
2022-05-01 09:40:14 +0600

    servicepo

--- APLOS/Areas/Products/Scripts/ServicePOApprovedController.js
+++ APLOS/Areas/Products/Scripts/ServicePOApprovedController.js
@@ -1,9 +1,9 @@
 ﻿'use strict';
 
-ServicePOCheckAndApprovedController.$inject = ['accountService', 'addressService', '$location', '$window',  'cboService', 'commonMessage', '$scope', '$rootScope', 'baseService', '$http', '$filter', '$controller'];
-function ServicePOCheckAndApprovedController(accountService, addressService, $location, $window, cboService, commonMessage, $scope, $rootScope, baseService, $http, $filter, $controller) {
+ServicePOApprovedController.$inject = ['accountService', 'addressService', '$location', '$window',  'cboService', 'commonMessage', '$scope', '$rootScope', 'baseService', '$http', '$filter', '$controller'];
+function ServicePOApprovedController(accountService, addressService, $location, $window, cboService, commonMessage, $scope, $rootScope, baseService, $http, $filter, $controller) {
 
-    $rootScope.title = "Service PO Checked And Approved";
+    $rootScope.title = "Service PO Approved";
     $scope.Action = 'Save';
     $scope.index = -1;
     $scope.products = [];
@@ -17,24 +17,11 @@ function ServicePOCheckAndApprovedController(accountService, addressService, $lo
         if ($scope.url === 'Service-PO-Approval') {
             $scope.tabType = 'UnApprovedList';
         }
-        else if ($scope.url === 'Service-PO-Checking') {
-            $scope.tabType = 'UnCheckedList';
-        }
-       
-
+        
     }
     $scope.uiType();
     $scope.tab = 1;
-    $scope.setTabServicePOUnChecked = function (newTab) {
-
-        $scope.tab = newTab;
-        $scope.tabType = 'UnCheckedList';
-        $scope.GetGetCheckedApprovedList();
-
-    };
-    $scope.isSetServicePOUnChecked = function (tabNum) {
-        return $scope.tab === tabNum;
-    };
+  
     $scope.setTabServicePOHold = function (newTab) {
         //debugger;
         $scope.tabType = 'HoldRejectCheckedList';
@@ -102,50 +89,6 @@ function ServicePOCheckAndApprovedController(accountService, addressService, $lo
     }
     $scope.GetGetCheckedApprovedList();
 
-   //#endregion
-    //#region Button Update function call
-    //$scope.onClickChecked = function (z) {
-    //    //debugger;
-    //    var x = "#" + z;
-    //    var gridObj = $(x).data("ejGrid");
-    //    $scope.ChkedAppDataInfo = gridObj.getSelectedRecords()[0];
-    //    $scope.url = $location.absUrl().split('!/')[1]
-    //    if ($scope.url === 'Service-PO-Approval') {
-    //        //$scope.tabType = 'UnApprovedList';
-    //        if ($scope.ChkedAppDataInfo.CheckedStatus === 'Approval') {
-    //            $scope.ChkedAppDataInfo.CheckedStatus = 'Approve';
-    //        }
-    //        $scope.POPUpStatus = $scope.ChkedAppDataInfo.CheckedStatus;
-    //    }
-    //    else if ($scope.url === 'Service-PO-Checking') {
-    //        // $scope.tabType = 'UnCheckedList';
-    //        if ($scope.ChkedAppDataInfo.CheckedStatus === 'Checked') {
-    //            $scope.ChkedAppDataInfo.CheckedStatus = 'Check';
-    //        }
-    //        $scope.POPUpStatus = $scope.ChkedAppDataInfo.CheckedStatus;
-    //    }
-    //    $scope.message = 'Are you sure to ' + $scope.POPUpStatus + '?';
-    //    angular.element(document.querySelector('#CheckApprovedAlert')).modal('show');
-    //};
-
-    //#endregion
-    //#region Update Checked and Aproved
-    $scope.onClickChecked = function (z) {
-        //debugger; 
-        var x = "#" + z;
-        var gridObj = $(x).data("ejGrid");
-        $scope.ChkedAppDataInfo = gridObj.getSelectedRecords()[0];
-        $scope.url = $location.absUrl().split('!/')[1]
-        if ($scope.url === 'Service-PO-Checking') {
-            // $scope.tabType = 'UnCheckedList';
-            if ($scope.ChkedAppDataInfo.CheckedStatus === 'Checked') {
-                $scope.ChkedAppDataInfo.CheckedStatus = 'Checked';
-            }
-            $scope.POPUpStatus = $scope.ChkedAppDataInfo.CheckedStatus;
-        }
-        $scope.message = 'Are you sure to ' + $scope.POPUpStatus + '?';
-        angular.element(document.querySelector('#CheckApprovedAlert')).modal('show');
-    };
     $scope.onClickApproved = function (z) {
         //debugger;
         var x = "#" + z;
@@ -163,65 +106,7 @@ function ServicePOCheckAndApprovedController(accountService, addressService, $lo
         angular.element(document.querySelector('#CheckApprovedAlert')).modal('show');
     };
 
-    $scope.CheckdServicePO = function () {
-        try {
-            if ($scope.url === 'Service-PO-Checking') {
-                if (baseService.isUndefinedOrNull($scope.ChkedAppDataInfo.CheckedStatus) || $scope.ChkedAppDataInfo.CheckedStatus === "Select") {
-                    ShowResult("Please Select Checked By Status", 'failure');
-                    return false;
-                }
-                else if ($scope.ChkedAppDataInfo.CheckedStatus === "Hold" || $scope.ChkedAppDataInfo.CheckedStatus === "Reject") {
-                    if (baseService.isUndefinedOrNull($scope.ChkedAppDataInfo.CheckedHoldRejectReason)) {
-                        ShowResult("Enter The Reason", 'failure');
-                        return false;
-                    }
-
-                }
-                else if ($scope.ChkedAppDataInfo.CheckedStatus === "Checked") {
-                    if (baseService.isUndefinedOrNull($scope.ChkedAppDataInfo.ApprovedBy)) {
-                        ShowResult("Please Select Approved By", 'failure');
-                        return false;
-                    }
-
-                }
-                var filteredData = $scope.ChkedAppDataInfo.Id;
-                var data = ej.DataManager(window.lst).executeLocal(ej.Query().where("ServicePOMasterId", "equal", parseInt(filteredData), true).take(1000));
-                if (data.length == 0) {
-                    throw "PO Details is reuired.";
-                }
-
-                $http({
-                    method: 'POST',
-                    url: 'Products/PurchaseOrder/ServicePOCheckedAndApproved',
-                    data: {
-                        'Id': $scope.ChkedAppDataInfo.Id,
-                        'PoValue': $scope.ChkedAppDataInfo.TotalQty,
-                        'CheckedApprovedStataus': $scope.ChkedAppDataInfo.CheckedStatus,
-                        'RejectReason': $scope.ChkedAppDataInfo.CheckedHoldRejectReason,
-                        'CheckedApprovedBy': $scope.ChkedAppDataInfo.ApprovedBy,
-                        'UIType': $scope.url
-                    },
-                    dataType: 'JSON'
-                }).then(function successCallback(response) {
-                    if (response.data.Error === true) {
-                        ShowResult(response.data.Message, 'failure');
-                    }
-                    else {
-                        ShowResult("Information Updated Successfully", 'success');
-                        $scope.GetGetCheckedApprovedList();
-                    }
-                }, function errorCallBack(response) {
-                    ShowResult(response.data.Message, 'failure');
-                });
-            }
-        }
-		catch (e) {
-                ShowResult(e, 'failure');
-            }
-        }
-        
-
-
+   
     $scope.ApprovedServicePO = function () {
          try {
          if ($scope.url === 'Service-PO-Approval') {
@@ -267,19 +152,6 @@ function ServicePOCheckAndApprovedController(accountService, addressService, $lo
     }
 
 
-
-
-
-
-    //#endregion   
-    //#region Checked And Approval Status
-
-    $scope.CheckedStatusList = function () {
-        cboService.getEnumCbo("enum/GetPOApprovalStatusCbo", function (result) {
-            $scope.checkedstatusList = result;
-        });
-    }
-    $scope.CheckedStatusList();
     $scope.LoadapprovalStatus = function () {
         cboService.getEnumCbo("enum/GetCheckedStatusCbo", function (result) {
             $scope.approvalStatusList = result;
@@ -287,34 +159,7 @@ function ServicePOCheckAndApprovedController(accountService, addressService, $lo
 
     }
     $scope.LoadapprovalStatus();
-    //#endregion
-    //#region CheckBY Approve BY 
-
-    $scope.checkedByList = [];
-    $scope.GetToBeCheckedByList = function () {
-        //debugger;
-        $http({
-            method: 'GET',
-            url: 'Products/GateentryToken/GetGatePassCheckedBy'
-        }).then(function successCallback(response) {
-            $scope.checkedByList = response.data;
-        });
-    }
-    $scope.GetToBeCheckedByList();
-
-
-    $scope.ApprovedByList = [];
-    $scope.GetToBeApprovedByByList = function () {
-        //debugger;
-        $http({
-            method: 'GET',
-            url: 'Products/PurchaseOrder/ServicePOApproveBy'
-        }).then(function successCallback(response) {
-            $scope.ApprovedByList = response.data;
-        });
-    }
-    $scope.GetToBeApprovedByByList();
-
+ 
 
     
 
