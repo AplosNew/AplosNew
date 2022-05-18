8118d767fc7a9ae3a62ce123ebfb76aea54ee7b1
OMAR <Omar@apopinternational.com>
2022-05-18 17:57:09 +0600

    PDC Module

--- APLOS/Areas/Banks/Scripts/postDateChequeController.js
+++ APLOS/Areas/Banks/Scripts/postDateChequeController.js
@@ -1,14 +1,17 @@
 ﻿"use strict";
-postDateChequeController.$inject = ["commonMessage", "$scope", "$rootScope", "baseService", "$http", "$filter","$controller"];
-function postDateChequeController(commonMessage, $scope, $rootScope, baseService, $http, $filter, $controller) {
+postDateChequeController.$inject = ["commonMessage","cboService", "$scope", "$rootScope", "baseService", "$http", "$filter","$controller"];
+function postDateChequeController(commonMessage, cboService, $scope, $rootScope, baseService, $http, $filter, $controller) {
     $rootScope.title = "PDC";
     $scope.Action = "Save";
     $scope.index = -1;
     $scope.path = "Banks/CheckManagement/";
+    $scope.saveUrl = $scope.path + 'CreatePdc';
     //$scope.getLotNumberUrl = $scope.path + "getlotnumber/";
     $controller("bankBaseController", { $scope: $scope, $http: $http });
     $scope.partyType = "Vendor";
     $controller("partyBaseController", { $scope: $scope, $http: $http });
+    $scope.employeeUrl = 'IE/MachineMasterTransaction/GetEmployeeListByWhom';
+    //$scope.employeeUrl = $scope.path + 'GetEmployeeListByWhom',
 
     $scope.pdc = {
         Id: null,
@@ -19,16 +22,21 @@ function postDateChequeController(commonMessage, $scope, $rootScope, baseService
         DocRefNo: null,
         DocDate: null,
         BaseDate: null,
-        DueDate: null,
         CurrencyId: null,
         Days: null,
         PostingDate: null,
-        EffectiveDate: null,
-        VoucherId: false,
-        IsClose: false,
-        ReminderDay: null,
-        PaymentTermId: null,
-        Amount:0
+        PaymentDate: null,
+        //VoucherId: false,
+        //IsClose: false,
+        //PaymentTermId: null,
+        Amount: 0,
+        ResponsiblePersonId: null,
+        ResponsiblePersonCode: null,
+        ResponsiblePerson: null,
+        POId: null,
+        RemainderDays: null,
+        Days:7,
+        Remarks: null
 
     };
     $scope.pdcNew = Object.assign({}, $scope.pdc);
@@ -76,6 +84,10 @@ function postDateChequeController(commonMessage, $scope, $rootScope, baseService
         $scope.hidePartyPopUp();
     };
 
+    cboService.getCboTransactionCurrencyByCompany("", function (result) {
+        $scope.tranCurrencyList = result;
+    });
+
     $scope.tab = 1;
     $scope.setTab = function (newTab) {
         $scope.tab = newTab;
@@ -88,13 +100,12 @@ function postDateChequeController(commonMessage, $scope, $rootScope, baseService
     $scope.Save = function () {
         try {
             $scope.$broadcast("show-errors-check-validity");
-            if ($scope.checkLotNewForm.$valid) {
-                angular.copy($scope.checkLotNew, $scope.CheckManagement);
-              if ($scope.Action === "Save") {
+            
+              //if ($scope.Action === "Save") {
                 $http({
                     method: "POST",
-                    url: $scope.path + "create",
-                    data: $scope.CheckManagement,
+                    url: $scope.saveUrl,
+                    data: { 'Pdc': $scope.pdcNew },
                     dataType: "JSON"
                 }).then(function successCallback(response) {
                     if (response.data.Error === true) {
@@ -103,36 +114,101 @@ function postDateChequeController(commonMessage, $scope, $rootScope, baseService
                     }
                     else {
                         ShowResult(response.data.Message, "success");
+                        $scope.Action = 'Update';
+                        $scope.getData();
                         $scope.Clear();
-                        $scope.getchequeLotList($scope.checkLotNew.BankMasterId);
+                    //    if ($rootScope.isCollapsed) {
+                    //        $rootScope.toggle();
+                    //    }
                     }
                 });
                     return true;
-              }
-                if ($scope.Action === "Update") {
-                    $http({
-                        method: "POST",
-                        url: $scope.path + "Edit",
-                        data: $scope.CheckManagement,
-                        dataType: "JSON"
-                    }).then(function successCallback(response) {
-                        if (response.data.Error === true) {
-                            ShowResult(response.
-                                data.Message, "failure");
-                        }
-                        else {
-                            ShowResult(response.data.Message, "success");
-                            $scope.Clear();
-                            $scope.getchequeLotList($scope.checkLotNew.BankMasterId);
-                        }
-                    });
-                    return true;
-                }
             }
-        } catch (e) {
+      catch (e) {
             ShowResult(e, "failure");
         }
     };
+
+    $scope.employeeParameters = {
+        limit: 10,
+        offset: 0,
+        order: 'asc',
+        sort: 'EmployeeCode, FirstName, MiddleName, LastName ',
+        searchBy: 'EmployeeCode',
+        pageSize: 10,
+        total_count: 0,
+        search: null,
+        serverPagination: true
+    };
+
+    $scope.Name = null;
+    $scope.showEmployeeListPopUp = function (name) {
+        try {
+            $scope.Name = name;
+
+            $scope.employeeParameters.searchBy = 'EmployeeCode';
+            baseService.setCurrentPage('employeeList');
+            $scope.searchEmployeeByList = [];
+            $scope.getEmployeeData = function (pageno) {
+                baseService.paginationBase($scope.employeeUrl, pageno, $scope.employeeParameters)
+                    .then(function (result) {
+                        $scope.employeeList = result.Rows;
+                        $scope.employeeParameters.total_count = result.Total;
+
+                        if (baseService.arrayLength($scope.searchEmployeeByList) === 0)
+                            baseService.getDDLSearchColumn(result.Rows, $scope.searchEmployeeByList);
+                        $scope.employeeParameters.searchBy = 'EmployeeCode';
+                    }, function () {
+                        ShowResult(commonMessage.NetworkError, 'failure');
+                    }).finally(function () {
+                    });
+            };
+            angular.element(document.querySelector('#employeePopUps')).modal('show');
+            $scope.getEmployeeData();
+        } catch (e) {
+            ShowResult(e, 'failure');
+        }
+    };
+
+    $scope.selectEmployeePopUp = function (index, data) {
+        $scope.employeeIndex = index;
+
+        $scope.pdcNew.ResponsiblePersonId = data.SystemId;
+        $scope.pdcNew.ResponsiblePerson = data.EmployeeName;
+        $scope.pdcNew.ResponsiblePersonCode = data.EmployeeCode;
+
+        angular.element(document.querySelector('#employeePopUps')).modal('hide');
+        $scope.Name = null;
+    };
+
+    $scope.hideEmployeePopUp = function () {
+        angular.element(document.querySelector('#employeePopUps')).modal('hide');
+    };
+
+    $scope.ModelList = [];
+    $scope.getData = function () {
+        $scope.ModelList = [];
+        $http.get('Banks/CheckManagement/getlist')
+            .then(function (response) {
+                $scope.ModelList = response.data;
+
+            });
+    };
+    $scope.getData();
+
+    $scope.recorddoubleclick = function (args) {
+        $scope.pdcNew = Object.assign({}, args.data);
+        try {
+            $scope.Action = 'Update';
+            if (!$rootScope.isCollapsed) {
+                $rootScope.toggle();
+            }
+        }
+        catch (e) {
+
+        }
+    };
+
 
     $scope.Clear = function () {
         ClearFields();
@@ -142,13 +218,7 @@ function postDateChequeController(commonMessage, $scope, $rootScope, baseService
 
     function ClearFields() {
         $scope.Action = "Save";
-        $scope.CheckManagement = {};
-        $scope.checkLotNew.Active = true;
-        $scope.checkLotNew.FromNo = null;
-        $scope.checkLotNew.ToNo = null;
-        $scope.checkLotNew.LotNumber = null;
-       // $scope.checkLotNew.LotNumber = lotNumber;
-        $scope.checkLotNew.IsNonSequential = false;
+        $scope.pdcNew = Object.assign({}, $scope.pdc);
     }
    
     $scope.getBank = function () {
