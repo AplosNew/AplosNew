102933336ad7fbbe38735d813a64cee1a31b3226
Mizanur Rahman <mizanur@apopinternational.com>
2022-05-21 17:31:15 +0600

    formula fixing

--- APLOS/Areas/OrderManagements/Controllers/MasterOrderController.cs
+++ APLOS/Areas/OrderManagements/Controllers/MasterOrderController.cs
@@ -144,6 +144,15 @@ namespace Aplos.Areas.OrderManagements.Controllers
 
                     dtValue.Rows.Add(dtValueRow);
                 }
+                else if (i > 0 && string.IsNullOrEmpty(dsOpenHead.Tables[0].Rows[i]["FormulaId"].ToString()))
+                {
+                    DataRow dtValueRow = dtValue.NewRow();
+
+                    dtValueRow["OrderLineCostingItemID"] = dsOpenHead.Tables[0].Rows[i]["OrderLineCostingItemId"].ToString().Trim();
+                    dtValueRow["Amount"] = dsOpenHead.Tables[0].Rows[i]["Value"].ToString().Trim();
+
+                    dtValue.Rows.Add(dtValueRow);
+                }
 
                 if (!string.IsNullOrEmpty(dsOpenHead.Tables[0].Rows[i]["FormulaId"].ToString()))
                 {
@@ -171,6 +180,8 @@ namespace Aplos.Areas.OrderManagements.Controllers
 
 
                 }
+                
+                
             }
 
 
