cd17e77f0a9809409a78ae6c09931299ba648883
Akanksha Sharma <akanksha@AGL.onmicrosoft.com>
2022-06-02 17:55:19 +0530

    remove validation from user name and standard name in zone-master

--- APLOS/Scripts/UPanelApp/upanelApp.js
+++ APLOS/Scripts/UPanelApp/upanelApp.js
@@ -1041,7 +1041,7 @@ upanelApp.controller("vendorPaymentApprovalController", vendorPaymentApprovalCon
 upanelApp.controller("ManpowerControlReportsController", ManpowerControlReportsController);
 upanelApp.controller("SalesOrderStatusReportController", SalesOrderStatusReportController);
 upanelApp.controller("ZoneMasterController", ZoneMasterController);
-upanelApp.controller("FuguaiTransactionController", FuguaiTransactionController);
+//upanelApp.controller("FuguaiTransactionController", FuguaiTransactionController);
 
 
 
