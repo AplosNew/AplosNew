12d7f7b226a31e4baae8a8e0895b66eb00864ca8
Mizanur Rahman <mizanur@apopinternational.com>
2022-06-11 17:09:48 +0600

    GetTaskAccordingToRresponsiblePersonList

--- APLOS/Areas/Products/Scripts/MaterialIssueSlipController.js
+++ APLOS/Areas/Products/Scripts/MaterialIssueSlipController.js
@@ -427,7 +427,6 @@ function MaterialIssueSlipController(addressService, $window, cboService, common
             }
             else if (issueStatus == 'Checked') {
                 $scope.IssueSlipCheckedList = response.data;
-               console.log($scope.IssueSlipCheckedList);
             }
             else if (issueStatus == 'HoldReject') {
                 $scope.IssueSlipHoldRejectList = response.data;
