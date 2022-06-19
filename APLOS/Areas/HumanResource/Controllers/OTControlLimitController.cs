f302f2b960d4a3a68ac52358254cb0427f65a515
Mizanur Rahman <mizanur@apopinternational.com>
2022-06-19 12:13:34 +0600

    OT Control limit

--- APLOS/Areas/HumanResource/Controllers/OTControlLimitController.cs
+++ APLOS/Areas/HumanResource/Controllers/OTControlLimitController.cs
@@ -3,9 +3,20 @@
 using Aplos.Controllers;
 using Aplos.Properties;
 using Library.Core;
+using Library.Crosscutting.Security;
+using Library.Data;
 using Library.Data.Sql;
 using Library.Model.Payrolls;
+using Library.Security.Core;
+using Library.Service.Helpers;
+using Library.Service.HumanResources.Profile;
 using Library.Service.Setups;
+using Syncfusion.XlsIO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Threading;
 using System.Web.Mvc;
 
 #endregion Using
@@ -33,10 +44,288 @@ namespace Aplos.Areas.HumanResource.Controllers
 
         #endregion -- Pages
 
+
+        [HttpPost, Authorize]
+        public JsonResult ImportData()
+        {
+            string path;
+            clsTemplateReadProfile objR = null;
+            try
+            {
+                objR = new clsTemplateReadProfile();
+                var file = Request.Files["file"];
+                var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+                SaveFiles(out path);
+                var data = ReadData(identity.PlantId, path);
+                JsonResult json = Json(data, JsonRequestBehavior.AllowGet);
+                json.MaxJsonLength = int.MaxValue;
+                return json;
+            }
+            catch (Exception ex)
+            {
+                return Json(new { Error = true, Message = ex.Message });
+            }
+        }
+        public void SaveFiles(out string path)
+        {
+            path = "";
+            try
+            {
+               
+                var file = Request.Files["file"];
+                if (file != null)
+                {
+                    var extension = Path.GetExtension(file.FileName);
+                    if (extension.ToLower() == ".xlsx" || extension.ToLower() == ".xls")
+                    {
+                    }
+                    else
+                        throw new CustomException(Resources.ExcelUploadError);
+                }
+                if (file != null)
+                {
+                    path = Path.Combine(ResourcesPathReader.GetAttendanceRawData(), file.FileName);
+                    if (System.IO.File.Exists(path))
+                    {
+                        System.IO.File.Delete(path);
+                        file.SaveAs(path);
+                    }
+                    else
+                    {
+                        file.SaveAs(path);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public List<OTControlLimitDetail> ReadData(string plantid, string path)
+        {
+            List<OTControlLimitDetail> data = null;
+            //string path = "";
+            DataSet dsExcel = null;
+            try
+            {
+                data = new List<OTControlLimitDetail>();
+                ReadFile(path, out dsExcel);
+                Validation(dsExcel, plantid);
+                data = dsExcel.Tables[0].ToList<OTControlLimitDetail>();
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Validation(DataSet dsExcel, string plantid)
+        {
+
+            try
+            {
+
+                if (dsExcel.Tables[0].Rows.Count > 0)
+                {
+                    if (false)
+                    {
+                        for (int i = 0; i < dsExcel.Tables[0].Rows.Count; i++)
+                        {
+                            string strTempPDate = "";
+                            string strTempPTimee = "";
+                            string strTempPType = "";
+
+                            strTempPDate = dsExcel.Tables[0].Rows[i][1].ToString().Trim();
+                            strTempPTimee = dsExcel.Tables[0].Rows[i][2].ToString().Trim();
+                            strTempPType = dsExcel.Tables[0].Rows[i][3].ToString().Trim().ToUpper();
+
+                        }//for
+
+                    }
+
+                }
+                else
+                {
+                    throw new Exception("Please Select File");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public void ReadFile(string path, out DataSet dsExcel)
+        {
+            FileInfo docFile;
+            dsExcel = null;
+            try
+            {
+                ExcelEngine excelEngine = null;
+                IApplication application = null;
+                IWorkbook workbook = null;
+                excelEngine = new ExcelEngine();
+                application = excelEngine.Excel;
+                workbook = excelEngine.Excel.Workbooks.Open(path);
+                DataTable dt = workbook.Worksheets[0].ExportDataTable(6, 1, 5000, 18, ExcelExportDataTableOptions.ColumnNames);
+                dt.DefaultView.RowFilter = "isnull(Sequence,'')<>''";
+                dt = dt.DefaultView.ToTable();
+                dsExcel = new DataSet();
+                dsExcel.Tables.Add(dt);
+                docFile = new FileInfo(path);
+                if (docFile.Exists)
+                {
+                    docFile.Delete();
+                }
+            }
+            catch (Exception ex)
+            {
+                docFile = new FileInfo(path);
+                if (docFile.Exists)
+                {
+                    docFile.Delete();
+                }
+                throw (ex);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Create(Dictionary<string, object> data, List<Dictionary<string, object>> detailList)
+        {
+            SaveData(data, detailList);
+            return Json(new { Data = data, Message = AplosMessage.Insert });
+        }
+
+        private void SaveData(Dictionary<string, object> data, List<Dictionary<string, object>> detailList)
+        {
+            var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+
+            try
+            {
+                DataSet dsMaster, dsDetail;
+                ConnectionManager.DAL.ConManager con = new ConnectionManager.DAL.ConManager("1");
+                con.OpenDataSetThroughAdapter("SELECT * FROM dbo.OTControlLimit WHERE Id='" + data["Id"] + "'", out dsMaster, false, "1");
+
+                string _Id= "";
+                string masterId = "";
+
+                if (dsMaster.Tables[0].Rows.Count == 0)
+                {
+                    bplib.clsGenID genid = new bplib.clsGenID();
+                    genid.GenerateIDYearly(DateTime.Now.ToShortDateString().ToString(), "OTControlLimit", out _Id);
+
+                    data["Id"] = _Id;
+                    AddNewRow(dsMaster.Tables[0], data);
+                }
+                else
+                {
+                    _Id = data["Id"].ToString();
+                    EditRow(dsMaster.Tables[0].Rows[0], data);
+                }
+
+                masterId = dsMaster.Tables[0].Rows[0]["Id"].ToString();
+
+                con.OpenDataSetThroughAdapter("SELECT * FROM dbo.OTControlLimitDetail WHERE OTControlLimitId ='" + masterId + "'", out dsDetail, false, "1");
+
+                int count = 0;
+                foreach (var item in detailList)
+                {
+                    count++;
+                    DataView dv = new DataView(dsDetail.Tables[0]);
+                    dv.RowFilter = "Id='" + item["Id"] + "'";
+
+                    if (dv.Count == 0)
+                    {
+                        item["Id"] = masterId + "-" + count;
+                        item["OTControlLimitId"] = masterId;
+
+                        AddNewRow(dsDetail.Tables[0], item);
+                    }
+                    else
+                    {
+                        DataRow drmo = dv[0].Row;
+                        EditRow(drmo, item);
+                    }
+                }
+
+
+                clsStaticInfo obj = new clsStaticInfo();
+                obj.SaveDataSets(dsMaster, dsDetail);
+
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        private void AddNewRow(DataTable dt, Dictionary<string, object> sourceData)
+        {
+            var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+            DataRow dr = dt.NewRow();
+
+            foreach (var item in sourceData.Keys)
+            {
+                try
+                {
+                    dr[item] = sourceData[item];
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            dr["AddedBy"] = identity.Name;
+            dr["AddedDate"] = System.DateTime.Now.ToString();
+            dr["AddedFromIP"] = identity.IPAddress;
+            dr["UpdatedBy"] = identity.Name;
+            dr["UpdatedDate"] = System.DateTime.Now.ToString();
+            dr["UpdatedFromIP"] = identity.IPAddress;
+
+            dt.Rows.Add(dr);
+        }
+        private void EditRow(DataRow dr, Dictionary<string, object> sourceData)
+        {
+            var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+            dr.BeginEdit();
+
+            foreach (var item in sourceData.Keys)
+            {
+                try
+                {
+                    dr[item] = sourceData[item];
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            dr["UpdatedBy"] = identity.Name;
+            dr["UpdatedDate"] = System.DateTime.Now.ToString();
+            dr["UpdatedFromIP"] = identity.IPAddress;
+            dr.EndEdit();
+        }
+
         #region -- Operations
 
 
 
         #endregion -- Operations
+    }
+
+    public class OTControlLimitDetail
+    {
+
+        public string Id { get; set; }
+        public string OTControlLimitId { get; set; }
+        public string BudgetCode { get; set; }
+        public string BudgetCodeId { get; set; }
+        public string DailyOTLimit { get; set; }
+        public string WeeklyOTLimit { get; set; }
+        public string WeekOffOTLimit { get; set; }
+        public string MonthlyOTLimit { get; set; }
+        public string Remarks { get; set; }
+
     }
 }
