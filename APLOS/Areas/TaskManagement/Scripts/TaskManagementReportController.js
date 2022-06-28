7de72dcdf075819e8ed97772efad05d0273d1b49
Mizanur Rahman <mizanur@apopinternational.com>
2022-06-28 10:10:34 +0600

    Task report

--- APLOS/Areas/TaskManagement/Scripts/TaskManagementReportController.js
+++ APLOS/Areas/TaskManagement/Scripts/TaskManagementReportController.js
@@ -22,35 +22,53 @@ function TaskManagementReportController(commonMessage, $scope, $rootScope, baseS
 
     $scope.filters = [];
     $scope.getFiltersData = function () {
-        $http({
-            method: 'GET',
-            url: 'TaskManagement/TaskManagementReport/getFiltersData?fromDate=' + $scope.FromDate + '&todate=' + $scope.ToDate,
-            dataType: 'JSON'
-        }).then(function successCallback(response) {
-            $scope.filters = response.data;
-            var columnList = [
-                { field: 'DesignationGroup', width: 20, headerText: "Designation Group", type: "string" },
-                { field: 'TaskCreatedBy', width: 20, headerText: "Task CreatedBy", type: "string" },
-                { field: 'Department', width: 20, headerText: "Department", type: "string" },
-                { field: 'Entity', width: 20, headerText: "Entity", type: "string" },
-                { field: 'UserReportGroup', width: 20, headerText: "User Group2", type: "string" }
+        try {
+            if (baseService.isUndefinedOrNull($scope.FromDate)) {
+                throw "From Date is required.";
+            }
+            else if (baseService.isUndefinedOrNull($scope.ToDate)) {
+                throw "To Date is required.";
+            }
+            else if (new Date($scope.FromDate) > new Date($scope.ToDate)) {
+                throw "From date must be below or equal to To Date";
+            }
+            else if (new Date($scope.ToDate) < new Date($scope.FromDate)) {
+                throw "To date must be above or equal to From Date.";
+            }
 
-            ];
-            $("#filters").ejGrid({
-                dataSource: $scope.filters,
-                minWidth: 450, minHeight: 400,
-                allowFiltering: true, allowPaging: true, enableTouch: true, responsive: true, allowTextWrap: true, allowScrolling: true,
-                filterSettings: { filterType: "excel" },
-                columns: columnList
+
+            $http({
+                method: 'GET',
+                url: 'TaskManagement/TaskManagementReport/getFiltersData?fromDate=' + $scope.FromDate + '&todate=' + $scope.ToDate,
+                dataType: 'JSON'
+            }).then(function successCallback(response) {
+                $scope.filters = response.data;
+                var columnList = [
+                    { field: 'DesignationGroup', width: 20, headerText: "Designation Group", type: "string" },
+                    { field: 'TaskCreatedBy', width: 20, headerText: "Task CreatedBy", type: "string" },
+                    { field: 'Department', width: 20, headerText: "Department", type: "string" },
+                    { field: 'Entity', width: 20, headerText: "Entity", type: "string" },
+                    { field: 'UserReportGroup', width: 20, headerText: "User Group2", type: "string" }
+
+                ];
+                $("#filters").ejGrid({
+                    dataSource: $scope.filters,
+                    minWidth: 450, minHeight: 400,
+                    allowFiltering: true, allowPaging: true, enableTouch: true, responsive: true, allowTextWrap: true, allowScrolling: true,
+                    filterSettings: { filterType: "excel" },
+                    columns: columnList
+                });
+
+                var gridObj = $("#filters").data("ejGrid");
+                gridObj.refreshContent(true);
+                gridObj.refreshTemplate();
+                $("#filters").children('.e-pager.e-js.e-pager').hide();
+                $("#filters").children('.e-gridcontent.e-droppable.e-js').hide();
+                $("#filters").children('.e-gridcontent').hide();
             });
-
-            var gridObj = $("#filters").data("ejGrid");
-            gridObj.refreshContent(true);
-            gridObj.refreshTemplate();
-            $("#filters").children('.e-pager.e-js.e-pager').hide();
-            $("#filters").children('.e-gridcontent.e-droppable.e-js').hide();
-            $("#filters").children('.e-gridcontent').hide();
-        });
+        } catch (e) {
+            ShowResult(e, 'failure');
+        }
     }
 
     $scope.parameters = [];
@@ -87,73 +105,107 @@ function TaskManagementReportController(commonMessage, $scope, $rootScope, baseS
     }
 
     $scope.GetTaskManagementReport = function () {
-        $scope.filterComplete();
-        $scope.fileName = "TaskManagementReport.xlsx";
-        if ($scope.model.State == "EmployeeWise") {
-            $scope.fileName = "TaskManagementReportEmployeeWise.xlsx";
-        }
-        else if ($scope.model.State == "DepartmentWise") {
-            $scope.fileName = "TaskManagementReportDepartmentWise.xlsx";
-        }
-        else {
-            $scope.fileName = "TaskManagementReportDesignationGroupWise.xlsx";
-        }
-        $http({
-            method: 'POST',
-            url: $scope.path + "GetTaskManagementReport",
-            data: { 'parameters': $scope.parameters, 'fromDate': $scope.FromDate, 'todate': $scope.ToDate, 'model': $scope.model },
-            dataType: 'JSON'
-        }).then(function successCallback(response) {
-            if (response.data.Error == true) {
-                ShowResult(response.data.Message, 'failure');
+        try {
+            if (baseService.isUndefinedOrNull($scope.FromDate)) {
+                throw "From Date is required.";
+            }
+            else if (baseService.isUndefinedOrNull($scope.ToDate)) {
+                throw "To Date is required.";
+            }
+            else if (new Date($scope.FromDate) > new Date($scope.ToDate)) {
+                throw "From date must be below or equal to To Date";
+            }
+            else if (new Date($scope.ToDate) < new Date($scope.FromDate)) {
+                throw "To date must be above or equal to From Date.";
+            }
+
+            $scope.filterComplete();
+            $scope.fileName = "TaskManagementReport.xlsx";
+            if ($scope.model.State == "EmployeeWise") {
+                $scope.fileName = "TaskManagementReportEmployeeWise.xlsx";
+            }
+            else if ($scope.model.State == "DepartmentWise") {
+                $scope.fileName = "TaskManagementReportDepartmentWise.xlsx";
             }
             else {
-                $window.open($scope.downloadgriddataUrlPath + "?FullPath=" + response.data.FileName + "&fileName=" + $scope.fileName);
+                $scope.fileName = "TaskManagementReportDesignationGroupWise.xlsx";
             }
-        }, function errorCallback(response) {
-            ShowResult(response.data.Message, 'failure');
-        });
+            $http({
+                method: 'POST',
+                url: $scope.path + "GetTaskManagementReport",
+                data: { 'parameters': $scope.parameters, 'fromDate': $scope.FromDate, 'todate': $scope.ToDate, 'model': $scope.model },
+                dataType: 'JSON'
+            }).then(function successCallback(response) {
+                if (response.data.Error == true) {
+                    ShowResult(response.data.Message, 'failure');
+                }
+                else {
+                    $window.open($scope.downloadgriddataUrlPath + "?FullPath=" + response.data.FileName + "&fileName=" + $scope.fileName);
+                }
+            }, function errorCallback(response) {
+                ShowResult(response.data.Message, 'failure');
+            });
+        } catch (e) {
+            ShowResult(e, 'failure');
+        }
     }
 
     $scope.GetTaskManagementData = function () {
-        $scope.TaskManagementDataList = [];
-        $scope.filterComplete();
+        try {
+            if (baseService.isUndefinedOrNull($scope.FromDate)) {
+                throw "From Date is required.";
+            }
+            else if (baseService.isUndefinedOrNull($scope.ToDate)) {
+                throw "To Date is required.";
+            }
+            else if (new Date($scope.FromDate) > new Date($scope.ToDate)) {
+                throw "From date must be below or equal to To Date";
+            }
+            else if (new Date($scope.ToDate) < new Date($scope.FromDate)) {
+                throw "To date must be above or equal to From Date.";
+            }
 
-        $http({
-            method: 'POST',
-            url: $scope.path + "GetTaskManagementData",
-            data: { 'parameters': $scope.parameters, 'fromDate': $scope.FromDate, 'todate': $scope.ToDate, 'model': $scope.model },
-            dataType: 'JSON'
-        }).then(function successCallback(response) {
-            if (response.data.Error == true) {
-                ShowResult(response.data.Message, 'failure');
-            }
-            else {
-                $scope.TaskManagementDataList = response.data;
-                var totalTask = $filter("sumByKey")($filter("filter")($scope.TaskManagementDataList), "CreatedTask");
-                var totalTaskDue = $filter("sumByKey")($filter("filter")($scope.TaskManagementDataList), "TaskDue");
-                for (var i = 0; i < $scope.TaskManagementDataList.length; i++) {
-                    if ($scope.TaskManagementDataList[i].CreatedTask == 0) {
-                        $scope.TaskManagementDataList[i].TotalStoryPoint = 0;
-                    }
-                    $scope.TaskManagementDataList[i].OfTotalTask = Math.ceil(($scope.TaskManagementDataList[i].CreatedTask / totalTask) * 100);
-                    if (isNaN($scope.TaskManagementDataList[i].OfTotalTask)) {
-                        $scope.TaskManagementDataList[i].OfTotalTask = 0;
-                    }
-                    $scope.TaskManagementDataList[i].PerTaskDue = Math.ceil(($scope.TaskManagementDataList[i].TaskDue / totalTaskDue) * 100);
-                    $scope.TaskManagementDataList[i].OverdueTask = $scope.TaskManagementDataList[i].TaskDue - $scope.TaskManagementDataList[i].OnTimeTask - $scope.TaskManagementDataList[i].LateTask;
-                    if (isNaN($scope.TaskManagementDataList[i].OverdueTask)) {
-                        $scope.TaskManagementDataList[i].OverdueTask = 0;
-                    }
-                    if (isNaN($scope.TaskManagementDataList[i].PerTaskDue)) {
-                        $scope.TaskManagementDataList[i].PerTaskDue = 0;
-                    }
-                    $scope.TaskManagementDataList[i].Performance = ((($scope.TaskManagementDataList[i].OnTimeTask * 2) + $scope.TaskManagementDataList[i].LateTask * 1) * $scope.TaskManagementDataList[i].PerTaskDue) - $scope.TaskManagementDataList[i].UnRead;//formula
+            $scope.TaskManagementDataList = [];
+            $scope.filterComplete();
+
+            $http({
+                method: 'POST',
+                url: $scope.path + "GetTaskManagementData",
+                data: { 'parameters': $scope.parameters, 'fromDate': $scope.FromDate, 'todate': $scope.ToDate, 'model': $scope.model },
+                dataType: 'JSON'
+            }).then(function successCallback(response) {
+                if (response.data.Error == true) {
+                    ShowResult(response.data.Message, 'failure');
                 }
-            }
-        }, function errorCallback(response) {
-            ShowResult(response.data.Message, 'failure');
-        });
+                else {
+                    $scope.TaskManagementDataList = response.data;
+                    var totalTask = $filter("sumByKey")($filter("filter")($scope.TaskManagementDataList), "CreatedTask");
+                    var totalTaskDue = $filter("sumByKey")($filter("filter")($scope.TaskManagementDataList), "TaskDue");
+                    for (var i = 0; i < $scope.TaskManagementDataList.length; i++) {
+                        if ($scope.TaskManagementDataList[i].CreatedTask == 0) {
+                            $scope.TaskManagementDataList[i].TotalStoryPoint = 0;
+                        }
+                        $scope.TaskManagementDataList[i].OfTotalTask = Math.ceil(($scope.TaskManagementDataList[i].CreatedTask / totalTask) * 100);
+                        if (isNaN($scope.TaskManagementDataList[i].OfTotalTask)) {
+                            $scope.TaskManagementDataList[i].OfTotalTask = 0;
+                        }
+                        $scope.TaskManagementDataList[i].PerTaskDue = Math.ceil(($scope.TaskManagementDataList[i].TaskDue / totalTaskDue) * 100);
+                        $scope.TaskManagementDataList[i].OverdueTask = $scope.TaskManagementDataList[i].TaskDue - $scope.TaskManagementDataList[i].OnTimeTask - $scope.TaskManagementDataList[i].LateTask;
+                        if (isNaN($scope.TaskManagementDataList[i].OverdueTask)) {
+                            $scope.TaskManagementDataList[i].OverdueTask = 0;
+                        }
+                        if (isNaN($scope.TaskManagementDataList[i].PerTaskDue)) {
+                            $scope.TaskManagementDataList[i].PerTaskDue = 0;
+                        }
+                        $scope.TaskManagementDataList[i].Performance = ((($scope.TaskManagementDataList[i].OnTimeTask * 2) + $scope.TaskManagementDataList[i].LateTask * 1) * $scope.TaskManagementDataList[i].PerTaskDue) - $scope.TaskManagementDataList[i].UnRead;//formula
+                    }
+                }
+            }, function errorCallback(response) {
+                ShowResult(response.data.Message, 'failure');
+            });
+        } catch (e) {
+            ShowResult(e, 'failure');
+        }
     }
 
 }
