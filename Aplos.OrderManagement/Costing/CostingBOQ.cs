f5d7abc33056f1faf03f3948320ec40d9b82f6ae
Om@r <omar@apopinternational.com>
2022-09-18 18:25:38 +0600

    Boq report

--- Aplos.OrderManagement/Costing/CostingBOQ.cs
+++ Aplos.OrderManagement/Costing/CostingBOQ.cs
@@ -1393,6 +1393,7 @@ namespace Library.OrderManagement.Costing
                 //Instantiate the Excel application object
                 IApplication application = excelEngine.Excel;
 
+                var report = new ReportUtility();
                 //Set the default application version
                 application.DefaultVersion = ExcelVersion.Excel2013;
                 IWorkbook workbook = application.Workbooks.Create(1);
@@ -1400,16 +1401,80 @@ namespace Library.OrderManagement.Costing
 
                 sheet.Name = "BOQ Report";
 
+                var headerData = getBOQStatusReportHeaderSql();
                 DataTable dtEmployeeData = BOQReportQuery(CostingBOQMasterId);
 
                 int ROW = 6;
                 int COL = 1;
+
+
+                #region Header
+                report.SetMasterHeaderText(ref sheet, ROW, 1, "Master Order");
+                sheet[ROW, 1].ColumnWidth = 20;
+                sheet.Range[ROW, 1].VerticalAlignment = ExcelVAlign.VAlignTop;
+                sheet.Range[ROW, 1].HorizontalAlignment = ExcelHAlign.HAlignLeft;
+                report.SetText(ref sheet, ROW, 2, headerData["MasterOrderId"].ToString());
+                sheet[report.GetColumnNameForXls(2) + ROW + ":" + report.GetColumnNameForXls(3) + ROW].Merge();
+                //sheet[ROW, 2].ColumnWidth = 20;
+                sheet.Range[ROW, 2].VerticalAlignment = ExcelVAlign.VAlignTop;
+
+                report.SetMasterHeaderText(ref sheet, ROW, 4, "Customer");
+                sheet[ROW, 4].ColumnWidth = 25;
+                sheet.Range[ROW, 4].VerticalAlignment = ExcelVAlign.VAlignTop;
+                sheet.Range[ROW, 4].HorizontalAlignment = ExcelHAlign.HAlignLeft;
+                report.SetText(ref sheet, ROW, 5, headerData["PartyName"].ToString());
+                sheet[report.GetColumnNameForXls(5) + ROW + ":" + report.GetColumnNameForXls(6) + ROW].Merge();
+                //sheet[ROW, 5].ColumnWidth = 30;
+                sheet.Range[ROW, 5].VerticalAlignment = ExcelVAlign.VAlignTop;
+                ROW++;
+
+                report.SetMasterHeaderText(ref sheet, ROW, 1, "Buyer Ref No");
+                sheet[ROW, 1].ColumnWidth = 20;
+                sheet.Range[ROW, 1].VerticalAlignment = ExcelVAlign.VAlignTop;
+                sheet.Range[ROW, 1].HorizontalAlignment = ExcelHAlign.HAlignLeft;
+                report.SetText(ref sheet, ROW, 2, headerData["BuyerReferenceNo"].ToString());
+                sheet[report.GetColumnNameForXls(2) + ROW + ":" + report.GetColumnNameForXls(3) + ROW].Merge();
+                sheet[ROW, 2].ColumnWidth = 20;
+                sheet.Range[ROW, 2].VerticalAlignment = ExcelVAlign.VAlignTop;
+
+                report.SetMasterHeaderText(ref sheet, ROW, 4, "Buyer");
+                sheet[ROW, 4].ColumnWidth = 25;
+                sheet.Range[ROW, 4].VerticalAlignment = ExcelVAlign.VAlignTop;
+                sheet.Range[ROW, 4].HorizontalAlignment = ExcelHAlign.HAlignLeft;
+                report.SetText(ref sheet, ROW, 5, headerData["BuyerName"].ToString());
+                sheet[report.GetColumnNameForXls(5) + ROW + ":" + report.GetColumnNameForXls(6) + ROW].Merge();
+                sheet[ROW, 5].ColumnWidth = 30;
+                sheet.Range[ROW, 5].VerticalAlignment = ExcelVAlign.VAlignTop;
+                ROW++;
+
+                report.SetMasterHeaderText(ref sheet, ROW, 1, "Contract No");
+                sheet[ROW, 1].ColumnWidth = 20;
+                sheet.Range[ROW, 1].VerticalAlignment = ExcelVAlign.VAlignTop;
+                sheet.Range[ROW, 1].HorizontalAlignment = ExcelHAlign.HAlignLeft;
+                report.SetText(ref sheet, ROW, 2, headerData["OwnReferenceNo"].ToString());
+                sheet[report.GetColumnNameForXls(2) + ROW + ":" + report.GetColumnNameForXls(3) + ROW].Merge();
+                sheet[ROW, 2].ColumnWidth = 20;
+                sheet.Range[ROW, 2].VerticalAlignment = ExcelVAlign.VAlignTop;
+
+                report.SetMasterHeaderText(ref sheet, ROW, 4, "Product");
+                sheet[ROW, 4].ColumnWidth = 25;
+                sheet.Range[ROW, 4].VerticalAlignment = ExcelVAlign.VAlignTop;
+                sheet.Range[ROW, 4].HorizontalAlignment = ExcelHAlign.HAlignLeft;
+                report.SetText(ref sheet, ROW, 5, headerData["Product"].ToString());
+                sheet[report.GetColumnNameForXls(5) + ROW + ":" + report.GetColumnNameForXls(6) + ROW].Merge();
+                //sheet[ROW, 5].ColumnWidth = 25;
+                sheet.Range[ROW, 5].VerticalAlignment = ExcelVAlign.VAlignTop;
+
+                ROW++;
+                ROW++;
+                #endregion
+
                 sheet[ROW, COL].Text = "Responsible Person:" + dtEmployeeData.Rows[0]["ResponsiblePerson"].ToString();
                 sheet.Range[ROW, 1, ROW, 10].Merge();
                 ROW++;
 
                 sheet[ROW, COL].Text = "Sl.No";
-                sheet[ROW, COL].ColumnWidth = 6;
+                sheet[ROW, COL].ColumnWidth = 13;
                 int colSlNo = COL;
                 COL++;
 
@@ -1422,7 +1487,7 @@ namespace Library.OrderManagement.Costing
                 int colItemDesc = COL;
                 COL++;
                 sheet[ROW, COL].Text = "Criteria";
-                sheet[ROW, COL].ColumnWidth = 8;
+                //sheet[ROW, COL].ColumnWidth = 8;
                 int colBOQCriteria = COL;
                 COL++;
                 sheet[ROW, COL].Text = "Material Code";
@@ -1549,6 +1614,32 @@ namespace Library.OrderManagement.Costing
                 sheet.UsedRange.VerticalAlignment = ExcelVAlign.VAlignTop;
                 sheet.Range[StartRow, 1, ROW, endCol].CellStyle.Font.Size = 8f;
 
+                //Total Start
+                var endRow = ROW++;
+
+                sheet.Range[endRow, 1].Text = "Total";
+                sheet.Range[endRow, 1, endRow, 18].Merge();
+                sheet.Range[endRow, 1].CellStyle.Font.Bold = true;
+                sheet.Range[endRow, 19].Number = clsStaticInfo.dbl(dtEmployeeData.Compute("SUM(BOMAmount)", null));
+                sheet.Range[endRow, 19].NumberFormat = clsStaticInfo.NumberFormat(2);
+                sheet.Range[endRow, 19].CellStyle.Font.Bold = true;
+                sheet.Range[endRow, 19, endRow, 19].VerticalAlignment = ExcelVAlign.VAlignCenter;
+                sheet.Range[endRow, 19, endRow, 19].HorizontalAlignment = ExcelHAlign.HAlignCenter;
+
+                sheet.Range[endRow, 20].Number = clsStaticInfo.dbl(dtEmployeeData.Compute("SUM(PlanAmount)", null));
+                sheet.Range[endRow, 20].NumberFormat = clsStaticInfo.NumberFormat(2); 
+                sheet.Range[endRow, 20].CellStyle.Font.Bold = true;
+                sheet.Range[endRow, 20, endRow, 20].VerticalAlignment = ExcelVAlign.VAlignCenter;
+                sheet.Range[endRow, 20, endRow, 20].HorizontalAlignment = ExcelHAlign.HAlignCenter;
+                sheet.Range[endRow, 20].CellStyle.Font.Bold = true;
+
+                sheet.Range[endRow, 1, endRow, endCol].BorderAround(ExcelLineStyle.Hair);
+                sheet.Range[endRow, 1, endRow, endCol].BorderInside(ExcelLineStyle.Hair);
+
+                endRow++;
+                endRow++;
+
+                //Total End
                 sheet["A" + StartRow.ToString()].FreezePanes();
 
                 sheet.Range[StartRow, colSlNo, ROW, colSlNo].NumberFormat = clsStaticInfo.NumberFormat();
@@ -1646,6 +1737,34 @@ namespace Library.OrderManagement.Costing
                 throw;
             }
 
+        }
+
+        public Dictionary<string, object> getBOQStatusReportHeaderSql()
+        {
+            try
+            {
+                var sql = @"SELECT boq.Id RowId,boq.[Sequence],boq.ItemRefNo,ci.UserName AS CostingItem,boq.BOQCriteria,mma.StandardName AS Article
+										,boq.SKUDesc,boq.POCriteria,boq.Consumption
+										,boq.BOMQty,boq.BOMQtyBase,boq.RequiredQty
+										,MO.Id MasterOrderId
+										,moi.BuyerReferenceNo,moi.OwnReferenceNo,moi.Id LineItemId
+										,PAR.UserName PartyName,B.UserName BuyerName,mma.StandardName Product
+										FROM BOQ  boq
+										LEFT JOIN hkp.CostingItem AS ci ON ci.Id=boq.CostingItemId
+										left join costingboqmaster BOM on BOM.Id=boq.CostingBOQMasterId
+										left join TRN.SalesOrder SO on SO.Id=boq.SalesOrderId
+									    left join TRN.MasterOrder MO on MO.PartyId=BOM.CustomerId
+										left join HKP.Party PAR on PAR.Id=MO.PartyId
+										left join HKP.Buyer B on B.Id=MO.BuyerId
+										left join TRN.MasterOrderItem AS moi on MO.Id=moi.MasterOrderId 
+										LEFT JOIN mst.MaterialMasterArticle AS mma ON mma.Id=moi.ArticleId ";
+
+                return _sqlRepository.GetData(sql);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         private string UpdateString(object FieldValue)
