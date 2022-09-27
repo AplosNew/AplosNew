9edba31983197aa22d98ecabe780451f4864656e
Nitesh Kumar <nitesh@apopinternational.com>
2022-09-27 12:02:45 +0530

    error msg appeard

--- APLOS/Areas/Materials/Scripts/DetentionLogController.js
+++ APLOS/Areas/Materials/Scripts/DetentionLogController.js
@@ -21,7 +21,8 @@ function DetentionLogController(cboService, commonMessage, $scope, $rootScope, b
         IssueByNo: null,
         Remarks: null,
         LoginTime: LogTime,
-        
+        isClose: 0,
+        isUpdate: 0
     };
     $scope.ModalNew = Object.assign({}, $scope.ModalTemp);
 
@@ -198,6 +199,7 @@ function DetentionLogController(cboService, commonMessage, $scope, $rootScope, b
     $scope.getIssueByNo();
 
     $scope.Save = function () {
+        $scope.$broadcast('show-errors-check-validity');
         $http({
             method: 'POST',
             url: 'Materials/DetentionLog/Save',
