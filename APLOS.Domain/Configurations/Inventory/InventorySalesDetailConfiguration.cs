b85062be1271a9440d43a3b3bb45e750d9cb2aa4
Belayet Rubel ahmed <belayet@apopinternational.com>
2022-10-01 11:04:40 +0600

    InventorySalesDetail

--- APLOS.Domain/Configurations/Inventory/InventorySalesDetailConfiguration.cs
+++ APLOS.Domain/Configurations/Inventory/InventorySalesDetailConfiguration.cs
@@ -10,9 +10,9 @@ namespace Library.Model.Configurations.Inventory
         {
             HasKey(t => t.Id);
             Property(t => t.AvgRate).HasPrecision(18, 10);
-            Property(t => t.AvgAmount).HasPrecision(18, 10);
+            Property(t => t.AvgAmount).HasPrecision(20, 10);
             Property(t => t.PolicyRate).HasPrecision(18, 10);
-            Property(t => t.PolicyAmount).HasPrecision(18, 10);
+            Property(t => t.PolicyAmount).HasPrecision(20, 10);
             Property(t => t.SalesRate).HasPrecision(18, 10);
             ToTable(nameof(InventorySalesDetail), DbSchema.Transaction);
             Ignore(r => r.ModelState);
