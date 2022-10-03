e8b0c7e32cef31f4bc0a5b0086254f8c5f8ca45c
Nitesh Kumar <nitesh@apopinternational.com>
2022-10-03 10:17:26 +0530

    New screen Medincine Master added

---
+++ Aplos.HumanResource/Employee/SicknessTypeService.cs
@@ -0,0 +1,27 @@
+﻿#region LIB
+using Library.Core;
+using Library.Crosscutting.Security;
+using Library.Data.Sql;
+using OTSBD;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+#endregion LIB
+
+namespace Library.HumanResource.Employee
+{
+   public class SicknessTypeService
+    {
+        private readonly SqlRepository _sqlRepository;
+        #region const
+        public SicknessTypeService()
+        {
+            _sqlRepository = new SqlRepository();
+        }
+        #endregion const
+    }
+}
