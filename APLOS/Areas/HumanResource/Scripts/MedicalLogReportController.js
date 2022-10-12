a893571318a4815189b4f7a09ef57581faa1c8dc
Nitesh Kumar <nitesh@apopinternational.com>
2022-10-12 18:39:23 +0530

    Medical log report fixd

--- APLOS/Areas/HumanResource/Scripts/MedicalLogReportController.js
+++ APLOS/Areas/HumanResource/Scripts/MedicalLogReportController.js
@@ -4,6 +4,7 @@ function MedicalLogReportController(cboService, commonMessage, $scope, $rootScop
     $rootScope.title = 'Medical Log Report';
     $scope.ModelList = [];
     $scope.path = 'HumanResource/MedicalLogReport/';
+    $scope.EmployeeUrl = 'HumanResource/MedicalLog/getEmployee';
     $scope.getListUrl = $scope.path + 'getlist';
     baseService.init($scope.getListUrl);
     $scope.downloadgriddataUrl = 'GridReports/Download';
@@ -18,4 +19,73 @@ function MedicalLogReportController(cboService, commonMessage, $scope, $rootScop
         angular.element(document.querySelector('#empPopUpId')).modal('hide');
 
     }
+
+    $scope.ModelTemp = {
+        FromDate: null,
+        ToDate: null,
+        EmployeeSysId:null
+    };
+    $scope.ModalNew = Object.assign({}, $scope.ModelTemp);
+
+    $scope.MedicalLogGridList = [];
+    $scope.medicallogGridView = function () {
+        $http({
+            method: 'POST',
+            url: $scope.path + 'medicallogGridView',
+            data: {
+                'from': $scope.ModalNew.FromDate,
+                'to': $scope.ModalNew.ToDate,
+                'empSystemId': $scope.ModalNew.EmployeeSysId
+            },
+            dataType: 'JSON'
+        }).then(function successCallback(response) {
+            $scope.MedicalLogGridList = response.data;
+        })
+    }
+
+    // #region Get All Employee and select by double click
+    $scope.EmployeeList = [];
+    $scope.getEmployee = function () {
+        $http({
+            method: 'POST',
+            url: $scope.EmployeeUrl,
+            dataType: 'JSON'
+        }).then(function succ(resp) {
+            $scope.EmployeeList = resp.data;
+        });
+
+    }
+    $scope.getEmployee();
+
+    $scope.doubleEmployee = function (e) {
+        $scope.ModalNew.EmployeeSysId = e.data.SystemId;
+        $scope.ModalNew.EmployeeName = e.data.EmployeeName;
+        $scope.closeEmpPopUp();
+
+    }
+    // #endregion Get All Employee and select by double click
+
+    $scope.XlsMedicalLogReport = function () {
+        $http({
+            method: 'POST',
+            url: $scope.path + "XlsMedicalLogReport",
+            data: {
+                'from': $scope.ModalNew.FromDate,
+                'to': $scope.ModalNew.ToDate,
+                'empSystemId': $scope.ModalNew.EmployeeSysId
+            },
+            dataType: 'JSON'
+        }).then(function successCallback(response) {
+            if (response.data.Error === true) {
+                ShowResult(response.data.Message, 'failure');
+            }
+            else {
+
+                $rootScope.report($scope.downloadgriddataUrl + "?FileName=" + response.data.FileName);
+            }
+        }, function errorCallback(response) {
+            ShowResult(response.data.Message, 'failure');
+        });
+
+    };
 }
