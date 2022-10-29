5ac31ece0fbeb133bccfaedb0262ba89502df6fe
mizanur <mizanur@apopinternational.com>
2022-10-29 12:10:31 +0600

    GetFinishedGoodsPackingReportData

--- APLOS/Areas/Productions/Scripts/FinishedGoodsPackingReportController.js
+++ APLOS/Areas/Productions/Scripts/FinishedGoodsPackingReportController.js
@@ -5,39 +5,39 @@ function FinishedGoodsPackingReportController(cboService, commonMessage, $scope,
     $scope.path = 'Productions/FinishedGoodsPackingReport/';
     $scope.downloadgriddataUrl = 'GridReports/Download';
 
-    //$scope.PurposeList = [];
-    //$scope.getAllEntities = function () {
-    //    $http({
-    //        method: 'POST',
-    //        url: "Productions/MaterialMovementPurpose/GetCbo"
-    //    }).then(function successCallback(response) {
-    //        $scope.PurposeList = response.data;
-    //        var index = 0;
-    //        $('#PurposeList').ejDropDownList(
-    //            {
-    //                dataSource: $scope.entityList,
-    //                fields: { text: "UserName", value: "Id" },
-    //                selectedIndex: index, showCheckBox: true, multiSelectMode: ej.MultiSelectMode.VisualMode
-    //                , width: 250
-    //            });
-    //        var DropDownEntityListObj = $("#PurposeList").data("ejDropDownList");
-    //        var PurposeId = DropDownEntityListObj.getSelectedValue();
-    //    });
-    //}
-    //$scope.getAllEntities();
+    $scope.PurposeList = [];
+    $scope.getAllEntities = function () {
+        $http({
+            method: 'Get',
+            url: "Productions/MaterialMovementPurpose/GetCbo"
+        }).then(function successCallback(response) {
+            $scope.PurposeList = response.data;
+            var index = 0;
+            $('#PurposeList').ejDropDownList(
+                {
+                    dataSource: $scope.PurposeList,
+                    fields: { text: "Text", value: "Value" },
+                    selectedIndex: index, showCheckBox: true, multiSelectMode: ej.MultiSelectMode.VisualMode
+                    , width: 250
+                });
+            var DropDownEntityListObj = $("#PurposeList").data("ejDropDownList");
+            var PurposeId = DropDownEntityListObj.getSelectedValue();
+        });
+    }
+    $scope.getAllEntities();
 
     $scope.downloadgriddataUrlPath = 'GridReports/DownloadUsingFullPath';//DownloadUsingPath
     $scope.fileName="GetFinishedGoodsPackingReport.xlsx";
     $scope.getFinishedStocksReport = function () {
-        //var DropDownEntityListObj = $("#PurposeList").data("ejDropDownList");
-        //var Purpose = DropDownEntityListObj.getSelectedValue();
+        var DropDownEntityListObj = $("#PurposeList").data("ejDropDownList");
+        var PurposeId = DropDownEntityListObj.getSelectedValue();
 
         $http({
             method: 'POST',
             url: $scope.path + "GetReport",
             
             data: {
-                'ToDate': $scope.ToDate, 'FromDate': $scope.FromDate
+                'ToDate': $scope.ToDate, 'FromDate': $scope.FromDate, 'PurposeId': PurposeId
             },
             dataType: 'JSON'
         }).then(function successCallback(response) {
