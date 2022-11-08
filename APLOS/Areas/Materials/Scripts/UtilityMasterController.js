aca83733ac304b1df723d07e71b673747c225bcd
mizanur <mizanur@apopinternational.com>
2022-11-08 12:46:31 +0600

    GetUtilityMasterData

--- APLOS/Areas/Materials/Scripts/UtilityMasterController.js
+++ APLOS/Areas/Materials/Scripts/UtilityMasterController.js
@@ -15,7 +15,7 @@ function UtilityMasterController(cboService, commonMessage, $scope, $rootScope, 
     baseService.init($scope.getListUrl);
     $scope.searchBy = "UserName"; $scope.search = "";
     $scope.searchByList = [{ value: 'Id', name: "Id" }, { value: 'Code', name: "Code" }, { value: 'ShortName', name: "Short Name" }, { value: 'StandardName', name: "Standard Name" }, { value: 'UserName', name: "User Name" }, { value: 'Description', name: "Description" }, { value: 'Remarks', name: "Remarks" }];
-
+    $scope.searchByUMList = [{ value: 'Id', name: "Id" }, { value: 'Code', name: "Code" }, { value: 'ShortName', name: "Short Name" }, { value: 'StandardName', name: "Standard Name" }, { value: 'UserName', name: "User Name" }, { value: 'Description', name: "Description" }, { value: 'Remarks', name: "Remarks" }];
 
     $scope.getData = function () {
         $http({
@@ -29,6 +29,27 @@ function UtilityMasterController(cboService, commonMessage, $scope, $rootScope, 
         });
     }
     $scope.getData();
+
+    $scope.UtilityMasterList = [];
+    $scope.GetUtilityMasterData = function () {
+        $http({
+            method: 'POST',
+            url: $scope.path + "GetUtilityMasterData?UtilityMasterId=" + $scope.ModelNew.Id,
+            data: { column: $scope.searchBy, value: $scope.search },
+            dataType: 'JSON'
+        }).then(function successCallback(response) {
+            $scope.UtilityMasterList = response.data;
+            angular.element(document.querySelector('#UtilityMasterPopUp')).modal('show');
+        });
+    }
+
+    $scope.GetUtilityMaster = function (obj) {
+        $scope.ModelNew.InPutSource = obj.data.UserName;
+        $scope.ModelNew.InPutSourceId = obj.data.Id;
+        angular.element(document.querySelector('#UtilityMasterPopUp')).modal('hide');
+        $scope.searchByUMList = '';
+    }
+
 
     $scope.tab = 1;
     $scope.setTab = function (newTab) {
@@ -370,30 +391,26 @@ function UtilityMasterController(cboService, commonMessage, $scope, $rootScope, 
         $scope.ModelNew = Object.assign({}, $scope.ModelTemp);
         $scope.ModelNew.Sequence = seq;
         $scope.utilityDetails = [];
-        $scope.SelectedassetpDataList = [];
+        $scope.SelectedAssetDataList = [];
     }
 
     $scope.ClearUtilityDetail = function () {
         $scope.ModelChildNew = Object.assign({}, $scope.ModelChild);
     }
 
-    $scope.GetAssetpDataList = function () {
-
-    }
-
     // #region Asset
 
-    $scope.assetpDataList = [];
-    $scope.GetAssetpDataList = function () {
+    $scope.assetDataList = [];
+    $scope.GetAssetDataList = function () {
         $http({
             method: 'GET',
             url: 'IE/MachineMasterUI/GetAssetData'
         }).then(function successCallback(response) {
-            $scope.assetpDataList = response.data;
-            for (var i = 0; i < $scope.assetpDataList.length; i++) {
-                for (var j = 0; j < $scope.SelectedassetpDataList.length; j++) {
-                    if ($scope.assetpDataList[i].MachineMasterAssetId == $scope.SelectedassetpDataList[j].MachineMasterAssetId) {
-                        $scope.assetpDataList.splice(i, 1);
+            $scope.assetDataList = response.data;
+            for (var i = 0; i < $scope.assetDataList.length; i++) {
+                for (var j = 0; j < $scope.SelectedAssetDataList.length; j++) {
+                    if ($scope.assetDataList[i].MachineMasterAssetId == $scope.SelectedAssetDataList[j].MachineMasterAssetId) {
+                        $scope.assetDataList.splice(i, 1);
                     }
                 }
             }
@@ -421,8 +438,8 @@ function UtilityMasterController(cboService, commonMessage, $scope, $rootScope, 
 
         var filtered = $("#GridAsset").data("ejGrid").getFilteredRecords();
         if (angular.isUndefinedOrNull(filtered) || filtered.length == 0) {
-            for (var i = 0; i < $scope.assetpDataList.length; i++) {
-                $scope.assetpDataList[i].Active = ChkOrUnchk;
+            for (var i = 0; i < $scope.assetDataList.length; i++) {
+                $scope.assetDataList[i].Active = ChkOrUnchk;
             }
         }
         else {
@@ -441,30 +458,30 @@ function UtilityMasterController(cboService, commonMessage, $scope, $rootScope, 
             method: 'GET',
             url: 'Materials/UtilityMaster/GetUtilityMasterAssetData?UtilityMasterId=' + $scope.ModelNew.Id
         }).then(function successCallback(response) {
-            $scope.SelectedassetpDataList = response.data;
+            $scope.SelectedAssetDataList = response.data;
         });
     }
 
-    $scope.SelectedassetpDataList = [];
+    $scope.SelectedAssetDataList = [];
     function MakeData() {
-        for (var i = 0; i < $scope.assetpDataList.length; i++) {
-            if ($scope.assetpDataList[i].Active == true) {
-                if (checkExists($scope.SelectedassetpDataList, $scope.assetpDataList[i].MachineMasterAssetId) === false) {
+        for (var i = 0; i < $scope.assetDataList.length; i++) {
+            if ($scope.assetDataList[i].Active == true) {
+                if (checkExists($scope.SelectedAssetDataList, $scope.assetDataList[i].MachineMasterAssetId) === false) {
                     var ob = {};
                     ob.Id = null;
                     ob.UtilityMasterId = $scope.ModelNew.Id;
-                    ob.MachineMasterAssetId = $scope.assetpDataList[i].MachineMasterAssetId;
-                    ob.Entity = $scope.assetpDataList[i].Entity;
-                    ob.AssetCode = $scope.assetpDataList[i].AssetCode;
-                    ob.AssetName = $scope.assetpDataList[i].AssetName;
-                    ob.AssetDetail = $scope.assetpDataList[i].AssetDetail;
-                    ob.AssetReference = $scope.assetpDataList[i].AssetReference;
-                    ob.WorkCenterMaster = $scope.assetpDataList[i].WorkCenterMaster;
+                    ob.MachineMasterAssetId = $scope.assetDataList[i].MachineMasterAssetId;
+                    ob.Entity = $scope.assetDataList[i].Entity;
+                    ob.AssetCode = $scope.assetDataList[i].AssetCode;
+                    ob.AssetName = $scope.assetDataList[i].AssetName;
+                    ob.AssetDetail = $scope.assetDataList[i].AssetDetail;
+                    ob.AssetReference = $scope.assetDataList[i].AssetReference;
+                    ob.WorkCenterMaster = $scope.assetDataList[i].WorkCenterMaster;
 
-                    $scope.SelectedassetpDataList.push(ob);
+                    $scope.SelectedAssetDataList.push(ob);
                 }
                 else {
-                    throw "This Asset " + $scope.assetpDataList[i].AssetName + " is already taken.";
+                    throw "This Asset " + $scope.assetDataList[i].AssetName + " is already taken.";
                 }
             }
         }
@@ -482,14 +499,14 @@ function UtilityMasterController(cboService, commonMessage, $scope, $rootScope, 
 
     $scope.SaveAssets = function () {
         try {
-            if (baseService.arrayLength($scope.SelectedassetpDataList) < 0) {
+            if (baseService.arrayLength($scope.SelectedAssetDataList) < 0) {
                 throw "Select Asset.";
             }
 
             $http({
                 method: 'POST',
                 url: 'Materials/UtilityMaster/CreateAsset',
-                data: { 'assets': $scope.SelectedassetpDataList },
+                data: { 'assets': $scope.SelectedAssetDataList },
                 dataType: 'JSON'
             }).then(function successCallback(response) {
                 if (response.data.Error === true) {
