3f919ca4afa80fda1292946e60691b2254234ff5
Nitesh Kumar <nitesh@apopinternational.com>
2022-11-12 10:01:03 +0530

    Two new module (Employer Master, Experience Master) added

---
+++ APLOS/Areas/HumanResource/Scripts/ExperienceMasterController.js
@@ -0,0 +1,35 @@
+﻿'use strict';
+ExperienceMasterController.$inject = ['cboService', 'commonMessage', '$scope', '$rootScope', '$window', 'baseService', '$routeParams', '$location', '$http', '$controller', '$filter'];
+function ExperienceMasterController(cboService, commonMessage, $scope, $rootScope, $window, baseService, $routeParams, $location, $http, $controller, $filter) {
+    $rootScope.title = 'Medicine Receipt';
+    $scope.Action = 'Save';
+    $scope.ModelList = [];
+    $scope.path = 'HumanResource/ExperienceMaster/';
+    $scope.getSeqUrl = $scope.path + 'getautosequence';
+    $scope.getListUrl = $scope.path + 'getlist';
+    $scope.saveUrl = $scope.path + 'Save';
+    $scope.saveUrlP = $scope.path + 'SavePurpose';
+    $scope.deleteUrl = $scope.path + 'Delete/';
+    baseService.init($scope.getListUrl);
+
+    $scope.DepartmentList = [];
+    $scope.GetDepartment = function () {
+        $http.get('Materials/DetentionLogReport/GetDepartment')
+            .then(
+                function successCallback(response) {
+                    $scope.DepartmentList = response.data;
+
+                }
+            )
+    }
+    $scope.GetDepartment();
+
+    $scope.ModelTemp = {
+        Id: null,
+        Sequence: 0,
+        AreaOfExperience: null,
+        Department: null,
+        IsActive:true
+    };
+    $scope.ModalNew = Object.assign({}, $scope.ModelTemp);
+}
