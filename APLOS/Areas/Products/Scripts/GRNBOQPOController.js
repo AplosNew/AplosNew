af9072b904528a8a67f571775701fd0b9ffdfb24
om@r <omar@apopinternational.com>
2022-12-20 12:12:08 +0600

    GRN BOQ PO

--- APLOS/Areas/Products/Scripts/GRNBOQPOController.js
+++ APLOS/Areas/Products/Scripts/GRNBOQPOController.js
@@ -492,11 +492,15 @@ function GRNBOQPOController(addressService, $window, factoryService, cboService,
             url: $scope.path + 'GetReceiveTaxList?receiveDetailId=' + grnId
         }).then(function (response) {
             $scope.POMaterialTaxList = response.data;
+            $scope.receiveTaxList = response.data;
 
             for (var i = 0; i < $scope.MasterList.length; i++) {
                 var linepk = $scope.MasterList[i].InventoryReceiveDetailId;
                 var list = getPOMaterialtaxlist(linepk);
                 $scope.MasterList[i].POMaterialTaxList = list;
+            }
+            for (var j = 0; j < length; j++) {
+
             }
         });
     };
@@ -600,6 +604,30 @@ function GRNBOQPOController(addressService, $window, factoryService, cboService,
         }
         angular.element(document.querySelector('#receiveTaxPopUp')).modal('show');
     };
+
+    $scope.getReceiveTaxListPOAfterSave = function (data, flag, index, Id) {
+        $scope.receiveTaxindex = index;
+        $scope.taxAbleAmnt = data.TrnAmount;
+        $scope.percentageColumn = flag;
+        $scope.Currency = $("#currency option:selected").text();
+        $scope.currentMaterialRow = index;
+        $scope.currentInventoryReceiveDetailIdRow = Id;
+        $scope.taxAbleAmnt = data.TrnAmount;
+        $scope.percentageColumn = flag;
+        $scope.currentMaterialRow = index;
+        $scope.receiveTaxList = [];
+        if ($scope.POMaterialTaxList.length > 0) {
+            $scope.HSNCode = $scope.POMaterialTaxList[0].HSNCode;
+            $scope.receiveTaxList = $scope.POMaterialTaxList;
+        }
+        $scope.total = 0;
+        for (var j = 0; j < $scope.receiveTaxList.length; j++) {
+            $scope.total = $scope.total + $scope.receiveTaxList[j].TaxAmount;
+        }
+        angular.element(document.querySelector('#receiveTaxPopUp')).modal('show');
+    };
+
+
     $scope.Clear = function () {
         ClearFields();
         return true;
@@ -748,7 +776,7 @@ function GRNBOQPOController(addressService, $window, factoryService, cboService,
                                     $scope.productId = response.data.Id;
                                     $scope.productNew.msgForAllocationNeed = response.data.entity.msgForAllocationNeed;
                                     $scope.Action = 'Update';
-                                    //$scope.$scope.getReceiveTaxListPO(data.PODetailId);
+                                    $scope.GetGRNMaterialTaxData($scope.productNew.Id);
                                 }
                             }), function (response) {
                                 ShowResult(response.data.Message, 'failure');
