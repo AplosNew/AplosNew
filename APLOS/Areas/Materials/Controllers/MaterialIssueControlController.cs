8b5c32afba23ba9ca5d97c29dc8e680c4dd01fe4
om@r <omar@apopinternational.com>
2023-01-25 17:05:53 +0600

    issue control report

--- APLOS/Areas/Materials/Controllers/MaterialIssueControlController.cs
+++ APLOS/Areas/Materials/Controllers/MaterialIssueControlController.cs
@@ -1163,7 +1163,7 @@ Where SO.Id " + soId + "";
 						else ''
 						END
 						,p.UserName ProcessName,po.SalesOrderId,FGColor=isnull(po.FGColor1,'')+','+isnull(po.FGColor2,'')+','+isnull(po.FGColor3,'')
-						,po.ProductionOrder,PreparedBy.EmployeeName AddedBy
+						,'PONo: ' + ISNULL(IsR.ProductionOrderId,po.ProductionOrder) ProductionOrder,PreparedBy.EmployeeName AddedBy
 						FROM TRN.IssueRequestMaster As IsR
 						                LEFT JOIN dbo.EmployeeInformation eI ON eI.SystemId=IsR.CheckedBy
                                          LEFT JOIN dbo.EmployeeInformation eI1 ON eI1.SystemId=IsR.AuthorizedBy
