3a7a45f6a21bb442dd1706f68073e153adb25280
Mizanur Rahaman <mizanur@apopinternational.com>
2023-02-07 16:07:43 +0600

    OS1xls report modify

--- APLOS/Areas/OrderManagements/Controllers/ProductionOrderReportsController.cs
+++ APLOS/Areas/OrderManagements/Controllers/ProductionOrderReportsController.cs
@@ -4811,7 +4811,7 @@ SUM(CASE WHEN SAME.FromCurrencyId=mo.CurrencyId THEN SO.CM* so.Qty ELSE  so.CM* 
                 if (Convert.ToDateTime(fromDate) > Convert.ToDateTime(toDate))
                     throw new Exception("To date cannot be earlier than from date");
                 DataTable dtOrderMaster;
-                getOS1(entityid, out dtOrderMaster);
+                getOS1(entityid, fromDate, toDate, productionStatusList,out dtOrderMaster);
 
                 if (dtOrderMaster.Rows.Count == 0)
                     throw new Exception("No data found");
@@ -6013,9 +6013,9 @@ group by pbt.productionOrderId,pbtm.MaxNoOfWS, pbt.Id ) Btn on Btn.ProductionOrd
 
 
         }
-        private void getOS1(string entityid, out DataTable dtOrderMaster)
+        private void getOS1(string entityid, string fromDate, string toDate, string productionStatusList, out DataTable dtOrderMaster)
         {
-
+            string orderStatusIds = "'" + productionStatusList.Replace(",", "','") + "'";//replaced with ""
 
             string sql = @"	SELECT so.Id AS SalesOrderId,btn.BulletinId,btn.NoOfWS,btn.TotalSPT,
 	con.Id ContractId,PA.UserName ContractName,M.LCRef LCNo,format(XCOM.ExpectedCompletionDate,'dd-MMM-yyyy') AS ExpectedCompletionDate,XCOM.Quantity SODistributedQty,
@@ -6122,8 +6122,8 @@ left outer join trn.ProductionBulletinTemplateDetail pbtd on pbtd.ProductionBull
 AND  pbtm.ProcessId=(select top 1 sx.ProcessId from trn.ProductionOrderProcessSet SX where SX.ProductionOrderId=pbt.productionOrderId and isnull(SX.IsBaseProcess,0)=1)
 group by pbt.productionOrderId,pbtm.MaxNoOfWS, pbt.Id ) Btn on Btn.ProductionOrderId=po.Id
 --left outer join BOQ on boq.SalesOrderId=so.Id and boq.SalesOrderId=(select top 1 SalesOrderId from boq where SalesOrderId=so.Id)
-                            WHERE os.Id='" + Library.Model.Enums.OrderStatusEnum.Active.ToString() + @"' AND mo.EntityId IN (" + entityid + @")
-            ORDER BY	trkp.UserName,trke.UserName,PAG.UserName DESC, p.UserName, b.UserName,convert(date,so.DeliveryDate),SO.ID";
+                            WHERE os.Id IN("+ orderStatusIds +@") AND mo.EntityId IN (" + entityid + @") AND mo.AddedDate between '"+fromDate+@"' AND '"+toDate+@"'
+            ORDER BY trkp.UserName,trke.UserName,PAG.UserName DESC, p.UserName, b.UserName,convert(date,so.DeliveryDate),SO.ID";
 
             dtOrderMaster = _sqlRepository.GetDataTable(sql);
 
