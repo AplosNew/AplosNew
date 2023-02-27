9e2a1f162e8a1dd0cac622a94ddb8b808b1e0e40
Mizanur Rahaman <mizanur@apopinternational.com>
2023-02-27 15:49:47 +0600

    DailyAttendancesController

--- APLOS/App_Start/BundleConfig.cs
+++ APLOS/App_Start/BundleConfig.cs
@@ -261,6 +261,19 @@ namespace Aplos
 
                  "~/Areas/DailyAttendance/Scripts/dailyattendanceApp.js"));
 
+
+            bundles.Add(new ScriptBundle("~/bundles/dapanelLogin").Include(
+                   "~/Scripts/DailyAttendances/dapanelLoginController.js",
+                  "~/Scripts/DailyAttendances/ppanelLogoutController.js",
+                   "~/Scripts/DailyAttendances/dapanelLoginApp.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dapanel").Include(
+
+                  "~/Scripts/DailyAttendances/ppanelLogoutController.js",
+                  "~/Scripts/DailyAttendances/daPasswordChangeController.js",
+                  "~/Scripts/DailyAttendances/dapanelApp.js"
+                  ));
+
             #endregion js
 
 #if DEBUG
