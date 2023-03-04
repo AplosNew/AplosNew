4ec423d2c3e8ab46a218987bcc424852f63470b0
Mohammed Asif <Asif@apopinternational.com>
2023-03-04 13:29:57 +0530

    Updated the changes in production summary screen.

--- Aplos.OrderManagement/Production/ProductionSummaryData.cs
+++ Aplos.OrderManagement/Production/ProductionSummaryData.cs
@@ -644,30 +644,30 @@ namespace Library.OrderManagement.Production
         {
             string CmdText = @"SELECT distinct PO.Id POId,PS.UserName ProductionStatus, PO.RequiredTimeUnit, PD.Product, PD.ProductCategory,PD.Buyer,PD.Customer 
                                    ,PD.BuyerOrder,PD.OwnOrder,PD.BuyerItem,PD.OwnItem,PD.Description,PD.PONumber,PO.EntityId,E.UserName Entity
-								  -- ,PlannedQty=CASE WHEN PQ.Qty=0 THEN PO.PlannedQty ELSE PO.PlannedQty END
-         --                   --,((CASE WHEN PQ.Qty=0 THEN PO.PlannedQty ELSE PO.PlannedQty END)-ISNULL(CEILING(PRS.TotalProductionQty),0)) RemainingQty
-         --                    ,ISNULL((CASE WHEN ISNULL(PPS.Qty,0)=0 THEN ISNULL(PQ.Qty,PO.PlannedQty) ELSE PO.PlannedQty*PPS.Qty/100 END)-ISNULL(CEILING(PRS.TotalProductionQty), 0),0) RemainingQty
-         --                   , ISNULL(CEILING(PRS.TotalProductionQty),0)TotalProductionQty
-									--,SONo=STUFF((select distinct ','+XSO.Id from 
-         --                                                        trn.SalesOrder XSO 
-         --                                                        JOIN trn.ProductionOrderDetail AS Xpod ON Xpod.SalesOrderId=Xso.Id
-						   --                                      LEFT JOIN [TRN].[CustomerPO] CPO ON CPO.Id = XSO.CustomerPOId
-         --                                                        WHERE po.Id=Xpod.ProductionOrderId	for xml path(''),TYPE).value('.', 'VARCHAR(MAX)'), 1, 1, '')
-         --                       ,Material=STUFF((select distinct ','+MM.UserName from 
-         --                                                        trn.SalesOrder XSO 
-         --                                                        JOIN trn.ProductionOrderDetail AS Xpod ON Xpod.SalesOrderId=Xso.Id
-						   --                                      LEFT JOIN trn.MasterOrderItem moi ON moi.Id = XSO.MasterOrderItemId
-						   --                                      LEFT JOIN MST.MaterialMaster mm on mm.id=MOI.MaterialMasterId
-         --                                                        WHERE po.Id=Xpod.ProductionOrderId	for xml path(''),TYPE).value('.', 'VARCHAR(MAX)'), 1, 1, '')
-         --                    ,Article=STUFF((select distinct ','+MMA.StandardName  from 
-         --                                                        trn.SalesOrder XSO 
-         --                                                        JOIN trn.ProductionOrderDetail AS Xpod ON Xpod.SalesOrderId=Xso.Id
-						   --                                      LEFT JOIN trn.MasterOrderItem moi ON moi.Id = XSO.MasterOrderItemId
-						   --                                      LEFT JOIN MST.MaterialMaster mm on mm.id=MOI.MaterialMasterId
-									--							 LEFT JOIN MST.MaterialMasterArticle AS mma on mma.MaterialMasterId=MM.Id
-         --                                                        WHERE po.Id=Xpod.ProductionOrderId	for xml path(''),TYPE).value('.', 'VARCHAR(MAX)'), 1, 1, ''),
-         --                          PRS.LotNumber
-         --                          --,PRS.ResponsiblePerson
+								  								   ,PlannedQty=CASE WHEN PQ.Qty=0 THEN PO.PlannedQty ELSE PO.PlannedQty END
+                            --,((CASE WHEN PQ.Qty=0 THEN PO.PlannedQty ELSE PO.PlannedQty END)-ISNULL(CEILING(PRS.TotalProductionQty),0)) RemainingQty
+                             ,ISNULL((CASE WHEN ISNULL(PPS.Qty,0)=0 THEN ISNULL(PQ.Qty,PO.PlannedQty) ELSE PO.PlannedQty*PPS.Qty/100 END)-ISNULL(CEILING(PRS.TotalProductionQty), 0),0) RemainingQty
+                            , ISNULL(CEILING(PRS.TotalProductionQty),0)TotalProductionQty
+									,SONo=STUFF((select distinct ','+XSO.Id from 
+                                                                 trn.SalesOrder XSO 
+                                                                 JOIN trn.ProductionOrderDetail AS Xpod ON Xpod.SalesOrderId=Xso.Id
+						                                         LEFT JOIN [TRN].[CustomerPO] CPO ON CPO.Id = XSO.CustomerPOId
+                                                                 WHERE po.Id=Xpod.ProductionOrderId	for xml path(''),TYPE).value('.', 'VARCHAR(MAX)'), 1, 1, '')
+                                ,Material=STUFF((select distinct ','+MM.UserName from 
+                                                                 trn.SalesOrder XSO 
+                                                                 JOIN trn.ProductionOrderDetail AS Xpod ON Xpod.SalesOrderId=Xso.Id
+						                                         LEFT JOIN trn.MasterOrderItem moi ON moi.Id = XSO.MasterOrderItemId
+						                                         LEFT JOIN MST.MaterialMaster mm on mm.id=MOI.MaterialMasterId
+                                                                 WHERE po.Id=Xpod.ProductionOrderId	for xml path(''),TYPE).value('.', 'VARCHAR(MAX)'), 1, 1, '')
+                             ,Article=STUFF((select distinct ','+MMA.StandardName  from 
+                                                                 trn.SalesOrder XSO 
+                                                                 JOIN trn.ProductionOrderDetail AS Xpod ON Xpod.SalesOrderId=Xso.Id
+						                                         LEFT JOIN trn.MasterOrderItem moi ON moi.Id = XSO.MasterOrderItemId
+						                                         LEFT JOIN MST.MaterialMaster mm on mm.id=MOI.MaterialMasterId
+																 LEFT JOIN MST.MaterialMasterArticle AS mma on mma.MaterialMasterId=MM.Id
+                                                                 WHERE po.Id=Xpod.ProductionOrderId	for xml path(''),TYPE).value('.', 'VARCHAR(MAX)'), 1, 1, ''),
+                                   PRS.LotNumber
+                                   --,PRS.ResponsiblePerson
 								   FROM TRN.ProductionOrder PO 
                                    LEFT JOIN TRN.ProductionOrderProcessSet PPS ON PPS.ProductionOrderID = PO.Id AND PPS.ProcessId = '" + processId + @"'
 								   LEFT JOIN [HKP].[ProductionStatus] PS ON PS.Id=PO.ProductionStatusId
