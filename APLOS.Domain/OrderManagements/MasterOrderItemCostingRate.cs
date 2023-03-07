dbd111400a6f2e688b91c9bf12d3433eece2a168
Mizanur Rahaman <mizanur@apopinternational.com>
2023-03-07 15:27:05 +0600

    Update logic change in moi

---
+++ APLOS.Domain/OrderManagements/MasterOrderItemCostingRate.cs
@@ -0,0 +1,23 @@
+﻿using Library.Core;
+using System;
+using System.Xml.Serialization;
+
+namespace Library.Model.OrderManagements
+{
+    public class MasterOrderItemCostingRate:BaseModel
+    {
+        public string Id { get; set; }
+        public string OrderLineCostingItemId { get; set; }
+        [XmlIgnore]
+        public MasterOrderItem MasterOrderItem { get; set; }
+        public string MasterOrderItemId { get; set; }
+        public decimal Value { get; set; }
+        public string UserName { get; set; }
+        public string AddedBy { get; set; }
+        public DateTime AddedDate { get; set; }
+        public string AddedFromIP { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public string UpdatedFromIP { get; set; }
+    }
+}
