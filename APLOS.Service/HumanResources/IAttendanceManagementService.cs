6c6adb55f4a1f141cd0764605c8162f92d313ed4
Mohammed Asif <Asif@apopinternational.com>
2023-03-13 08:32:27 +0530

    Updated the changes in production control screen.

--- APLOS.Service/HumanResources/IAttendanceManagementService.cs
+++ APLOS.Service/HumanResources/IAttendanceManagementService.cs
@@ -60,5 +60,6 @@ namespace Library.Service.HumanResources
 
         IWorkbook GetTiffinBillReport(string plantId, string FromDate, string ToDate, string ShiftId, string Hr, string Min);
         IWorkbook GetSkillManagementJobCardReports(string name, string companyGroupId, string companyId, string plantId, string plantName, string plannedId);
+        IWorkbook GetProductionJobCardReports(string name, string companyGroupId, string companyId, string plantId, string plantName, string workCenterId);
     }
 }
