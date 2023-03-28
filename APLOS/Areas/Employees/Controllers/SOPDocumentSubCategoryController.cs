1b8c6e34b73fe2d34bc93e887b4d439fb0b4929a
Nitesh Kumar <nitesh@apopinternational.com>
2023-03-28 16:21:58 +0530

    Document setup

--- APLOS/Areas/Employees/Controllers/SOPDocumentSubCategoryController.cs
+++ APLOS/Areas/Employees/Controllers/SOPDocumentSubCategoryController.cs
@@ -5,6 +5,13 @@ using Aplos.Properties;
 using Library.Service.Employees;
 using Library.Core;
 using System.Web.Mvc;
+using Library.Crosscutting.Security;
+using System.Threading;
+using Library.Data.Sql;
+using System.Data;
+using System.Collections.Generic;
+using Library.Security.Core;
+using System;
 
 #endregion
 
@@ -12,6 +19,8 @@ namespace Aplos.Areas.Employees.Controllers
 {
     public class SOPDocumentSubCategoryController : BaseController
     {
+        private readonly ISqlRepository _sqlRepository;
+        string TableName = "hkp.SOPDocumentSubCategory";
         #region Constructor
         /// <summary>   The SOPDocumentSubCategoryService service. </summary>
         private readonly ISOPDocumentSubCategoryService _SOPDocumentSubCategoryService;
@@ -20,10 +29,12 @@ namespace Aplos.Areas.Employees.Controllers
         public SOPDocumentSubCategoryController(
               ISOPDocumentSubCategoryService SOPDocumentSubCategoryService
             , ICompanyGroupSOPDocumentSubCategoryService companyGroupSOPDocumentSubCategoryService
+            , ISqlRepository R
             )
         {
             _SOPDocumentSubCategoryService = SOPDocumentSubCategoryService;
             _companyGroupSOPDocumentSubCategoryService = companyGroupSOPDocumentSubCategoryService;
+            _sqlRepository = R;
         }
         #endregion
 
@@ -42,10 +53,19 @@ namespace Aplos.Areas.Employees.Controllers
             return Json(new SelectList(_companyGroupSOPDocumentSubCategoryService.GetCbo(), "Value", "Text"), JsonRequestBehavior.AllowGet);
         }
 
-        [HttpGet, Authorize]
-        public ActionResult GetList(GridParameter parameters)
+        [HttpPost]
+        public ActionResult GetList(string column, string value)
         {
-            return Json(_companyGroupSOPDocumentSubCategoryService.Query(parameters), JsonRequestBehavior.AllowGet);
+            string strkey = "1=1";
+            if (string.IsNullOrEmpty(column) == false && string.IsNullOrEmpty(value) == false)
+                strkey = column + " like '%" + value + "%'";
+
+            var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+            string sql = @"select top 100 * from (SELECT * FROM " + TableName + ") AS TEMP WHERE " + strkey + " order by sequence";
+
+
+
+            return Json(_sqlRepository.GetDataCollection(sql, null), JsonRequestBehavior.AllowGet);
         }
 
         [HttpGet, Authorize]
@@ -54,11 +74,115 @@ namespace Aplos.Areas.Employees.Controllers
             return Json(_SOPDocumentSubCategoryService.GetAutoSequence(), JsonRequestBehavior.AllowGet);
         }
 
+        //[HttpPost]
+        //public JsonResult Create(SOPDocumentSubCategory SOPDocumentSubCategory)
+        //{
+        //    _SOPDocumentSubCategoryService.Insert(SOPDocumentSubCategory);
+        //    return Json(new { SOPDocumentSubCategory, Sequence = _SOPDocumentSubCategoryService.GetAutoSequence(), Message = AplosMessage.Success });
+        //}
+
         [HttpPost]
-        public JsonResult Create(SOPDocumentSubCategory SOPDocumentSubCategory)
+        public JsonResult Create(Dictionary<string, object> data)
         {
-            _SOPDocumentSubCategoryService.Insert(SOPDocumentSubCategory);
-            return Json(new { SOPDocumentSubCategory, Sequence = _SOPDocumentSubCategoryService.GetAutoSequence(), Message = AplosMessage.Success });
+            try
+            {
+                DataSet dsMaster;
+                ConnectionManager.DAL.ConManager con = new ConnectionManager.DAL.ConManager("1");
+                con.OpenDataSetThroughAdapter("select * from " + TableName + " where Code='" + data["Code"] + "' AND  Id<>'" + data["Id"] + "'", out dsMaster, false, "1");
+                if (dsMaster.Tables[0].Rows.Count > 0)
+                    throw new Exception("Same Code already exists!!!");
+
+                con.OpenDataSetThroughAdapter("select * from " + TableName + " where UserName='" + data["UserName"] + "' AND  Id<>'" + data["Id"] + "'", out dsMaster, false, "1");
+                if (dsMaster.Tables[0].Rows.Count > 0)
+                    throw new Exception("Same User Name already exists!!!");
+
+
+                con.OpenDataSetThroughAdapter("select * from " + TableName + " where Id='" + data["Id"] + "'", out dsMaster, false, "1");
+
+                string _Id = "";
+
+                #region data update
+                if (dsMaster.Tables[0].Rows.Count == 0)
+                {
+                    bplib.clsGenID genid = new bplib.clsGenID();
+                    genid.GenID(TableName, out _Id);
+
+                    data["Id"] =  _Id;
+                    AddNewRow(dsMaster.Tables[0], data);
+                }
+                else
+                {
+                    _Id = data["Id"].ToString();
+                    EditRow(dsMaster.Tables[0].Rows[0], data);
+                }
+                #endregion data update
+
+                clsStaticInfo _info = new clsStaticInfo();
+                _info.SaveDataSets(dsMaster);
+
+                return Json(new { Error = false, Data = data, Sequence = GetSequence(), Message = AplosMessage.Updated });
+
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new { Error = true, Message = ex.Message });
+
+            }
+        }
+
+        private double GetSequence()
+        {
+            DataTable dt = _sqlRepository.GetDataTable("SELECT  isnull(Max(Sequence),0) AS Sequence FROM " + TableName + "");
+            if (dt.Rows.Count > 0)
+                return clsStaticInfo.dbl(dt.Rows[0]["Sequence"].ToString()) + 1;
+
+            return 1;
+        }
+
+        private void AddNewRow(DataTable dt, Dictionary<string, object> sourceData)
+        {
+            var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+            DataRow dr = dt.NewRow();
+
+            foreach (var item in sourceData.Keys)
+            {
+                try
+                {
+                    dr[item] = sourceData[item];
+                }
+                catch (Exception)
+                {
+                }
+            }
+            dr["AddedBy"] = identity.Name;
+            dr["AddedDate"] = System.DateTime.Now.ToString();
+            dr["AddedFromIP"] = identity.IPAddress;
+            dr["UpdatedBy"] = identity.Name;
+            dr["UpdatedDate"] = System.DateTime.Now.ToString();
+            dr["UpdatedFromIP"] = identity.IPAddress;
+
+            dt.Rows.Add(dr);
+        }
+        private void EditRow(DataRow dr, Dictionary<string, object> sourceData)
+        {
+            var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+            dr.BeginEdit();
+
+            foreach (var item in sourceData.Keys)
+            {
+                try
+                {
+                    dr[item] = sourceData[item];
+                }
+                catch (Exception)
+                {
+                }
+            }
+            dr["UpdatedBy"] = identity.Name;
+            dr["UpdatedDate"] = System.DateTime.Now.ToString();
+            dr["UpdatedFromIP"] = identity.IPAddress;
+            dr.EndEdit();
         }
 
         [HttpPost]
