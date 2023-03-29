f33a8ec14a54fb6e098ca4323d071573b86a8c3e
om@r <omar@apopinternational.com>
2023-03-29 17:28:43 +0600

    leave report

--- APLOS.Service/HumanResources/AttendanceManagementService.cs
+++ APLOS.Service/HumanResources/AttendanceManagementService.cs
@@ -24955,6 +24955,7 @@ private void CreateFinalSettlement(string companyGroupId, string companyId, stri
         DataTable dtFinalSettlementDedutionData = null;
         DataTable dtFinalSettlementEarningData = null;
         decimal TotalEarning = 0;
+        decimal TotalEarningAmount = 0;
         decimal SeparationTypeAmount = 0;
         decimal LvEncashmentAmount = 0;
         decimal LeaveEncash = 0;
@@ -24989,11 +24990,14 @@ private void CreateFinalSettlement(string companyGroupId, string companyId, stri
             LeaveEncash = Convert.ToDecimal(dtFinalSettlementData.Rows[0]["LeaveEncash"].ToString());
             LastMonthNetPayAmount = Convert.ToDecimal(dtFinalSettlementData.Rows[0]["LastMonthNetPayAmount"].ToString());
             FinalSettlementEarningData = dtFinalSettlementEarningData.Rows.Count > 0 ? Convert.ToDecimal(dtFinalSettlementEarningData.Compute("SUM(Amount)", string.Empty)) : 0;
+            
             TotalEarning = SeparationTypeAmount + LeaveEncash + LastMonthNetPayAmount + FinalSettlementEarningData;
+            TotalEarningAmount = SeparationTypeAmount + LvEncashmentAmount + LastMonthNetPayAmount + FinalSettlementEarningData;
 
             decimal TotalDeduction = dtFinalSettlementDedutionData.Rows.Count > 0 ? Convert.ToDecimal(dtFinalSettlementDedutionData.Compute("SUM(Amount)", string.Empty)) : 0;
             decimal Payable = TotalEarning - TotalDeduction;
             document.Replace("{TotalEarning}", cnDgt(TotalEarning.ToString("N0"), UserName), false, true);
+            document.Replace("{TotalEarningAmount}", cnDgt(TotalEarning.ToString("N0"), UserName), false, true);
             document.Replace("{TotalDeduction}", cnDgt(TotalDeduction.ToString("N0"), UserName), false, true);
             document.Replace("{Payable}", cnDgt(Payable.ToString("N0"), UserName), false, true);
 
