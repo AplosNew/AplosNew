68a39375a78bd8e5adb680fa054f7b0cefc84184
Nitesh Kumar <nitesh@apopinternational.com>
2023-03-31 10:33:26 +0530

    chart account setup

---
+++ APLOS/Areas/Accounts/Controllers/ChartAccountSetupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aplos.Areas.Accounts.Controllers
+{
+    public class ChartAccountSetupController : Controller
+    {
+        // GET: Accounts/ChartAccountSetup
+        public ActionResult Aplos()
+        {
+            return View();
+        }
+    }
+}
