a73d6bd8ff2f5b0912da4f04659eb31d8a7ed36a
Nitesh Kumar <nitesh@apopinternational.com>
2023-06-29 15:18:02 +0530

    id logic changed for vehicle req

--- APLOS/Areas/HumanResource/Controllers/PlantInOutControllReportController.cs
+++ APLOS/Areas/HumanResource/Controllers/PlantInOutControllReportController.cs
@@ -23,26 +23,33 @@ namespace Aplos.Areas.HumanResource.Controllers
         public ActionResult GetPlantInOutGridData()
         {
             string sql = @"select FORMAT(PIO.AddedDate, 'dd-MMM-yyyy')[Date], PIO.InandOut
-,InTime = Case when InandOut = 'IN' THEN PIO.[Time] else  NULL end
-,OutTime = Case when InandOut = 'OUT' THEN PIO.[Time] else  NULL end
-, DATEDIFF(MINUTE,Case when InandOut = 'IN' THEN PIO.[Time] end, Case when InandOut = 'OUT' THEN PIO.[Time] end) OutDuration
-,EI.EmployeeName, D.UserName GivenDesignation
+,InTime = Case when PIO.InandOut = 'IN' THEN PIO.[Time] else  NULL end
+,OutTime = Case when PIO.InandOut = 'OUT' THEN PIO.[Time] else  NULL end
+, (DATEDIFF (
+SECOND
+,(select  MAX([PI].[Time]) from PlantInOutControl [PI] where [PI].EmployeeCode = EI.SystemId and [PI].InandOut = 'OUT' group by [PI].InandOut)
+,(select  MIN([PI].[Time]) from PlantInOutControl [PI] where [PI].EmployeeCode = EI.SystemId and [PI].InandOut = 'IN' group by [PI].InandOut)
+)) InOutDuration
+,EI.EmployeeCode,EI.EmployeeName, D.UserName GivenDesignation
 , EI.EmployeeCurrentStatus 
 ,A.UserName Activity, SS.UserName SubSection,S.UserName Section,DEP.UserName Department,EI.EmployeeStatus 
 ,DEP.UserName Department
-from PlantInOutControl PIO
-left join EmployeeInformation EI on EI.SystemId = PIO.EmployeeCode
+
+from PlantInOutControl PIO 
+left join EmployeeInformation EI  on EI.SystemId = PIO.EmployeeCode 
+
 left join MST.ManpowerBudget BGT on BGT.Id = EI.BudgetCode
 left join MST.BudgetMasterActivity BMA on BGT.ROBudgetCode = BMA.BudgetMasterId
 left join HKP.Activity A on BMA.ActivityId = A.Id
 left join ORG.Position P on BGT.PositionId = P.Id
---left join ORG.Division D on P.DivisionId  = D.Id
 left join ORG.Department DT on P.DepartmentId = DT.Id
 left join ORG.Section S on P.SectionId = S.Id
 left join ORG.SubSection SS on P.SubSectionId = SS.Id
 left join ORG.Entity E on E.Id = BGT.EntityId
 LEFT JOIN ORG.Department DEP on DEP.Id = EI.DepartmentId
-left join HKP.Designation D on D.Id = EI.GivenDesignationId";
+left join HKP.Designation D on D.Id = EI.GivenDesignationId
+--where EI.SystemId = 2014991 
+order by EI.EmployeeCode, PIO.AddedDate";
             return Json(_sqlRepository.GetDataCollection(sql), JsonRequestBehavior.AllowGet);
         }
     }
