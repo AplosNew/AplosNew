fe9b3471d8bde7063cac35de82e7fc06ee943523
Nitesh Kumar <nitesh@apopinternational.com>
2023-07-05 16:30:37 +0530

    bug fixd in packingcontroller

--- APLOS/Areas/Productions/Scripts/PackingController.js
+++ APLOS/Areas/Productions/Scripts/PackingController.js
@@ -608,14 +608,7 @@ function PackingController(cboService, commonMessage, $scope, $rootScope, baseSe
         $scope.Percent = kk.toFixed(2);
     }
 
-    $scop.CalculatePORemainingQty = function () {
-        for (var i = 0; i < $scope.PoLotRefGrid.length; i++) {
-            if ($scope.PoLotRefGrid[i]["checked"] == true) {
-                $scope.PoLotRefGrid[i]["quant"] = $scope.PoLotRefGrid[i]["POQty"] - $scope.PoLotRefGrid[i]["quant"];
-            }
-        }
-    }
-
+   
     //Double Click on The POLotRefGrid
     $scope.cartonDetail = [];
     $scope.inactiveCartons = [];
