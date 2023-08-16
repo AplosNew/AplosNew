c11d183ad3f1b6ba670b29c07f1e8ecb9d57df8a
Nitesh Kumar <nitesh@apopinternational.com>
2023-08-16 16:31:27 +0530

    ProcessManagementMaster and SalesOrderStatus

--- APLOS/Areas/Processes/Scripts/ProcessManagementController.js
+++ APLOS/Areas/Processes/Scripts/ProcessManagementController.js
@@ -25,6 +25,21 @@ function ProcessManagementController(commonMessage, $scope, $rootScope, baseServ
     }
     // #endregion TAB CHANGE
 
+    $scope.ProcessManagementTemp = {
+        Id: null,
+        StandardName: null,
+        UserName: null,
+        Process: null,
+        SubProcess: null,
+        MinSPTTime: null,
+        MaxSPTTime: null,
+        StandardSPTTime: null,
+        ResponsiblePerson: null,
+        EmployeeName: null,
+        IsWorkCenterApplicable: null
+    }
+    $scope.ProcessManagementNew = Object.assign({}, $scope.ProcessManagementTemp)
+
     $scope.DataList = [];
     $scope.GetProcessManagementDataList = function () {
         $http({
@@ -32,7 +47,7 @@ function ProcessManagementController(commonMessage, $scope, $rootScope, baseServ
             url: 'Processes/ProcessManagement/GetProcessManagementDataList'
         }).then(function successCallback(response) {
             $scope.DataList = response.data;
-            
+
         }
         )
     }
@@ -43,6 +58,7 @@ function ProcessManagementController(commonMessage, $scope, $rootScope, baseServ
         $scope.ProcessManagementNew = Object.assign({}, args.data)
         $scope.Action = 'Update';
         $scope.LoadEntityDetails();
+        $scope.LoadUtilityGrid();
         if (!$rootScope.isCollapsed) {
             $rootScope.toggle();
         }
@@ -53,7 +69,7 @@ function ProcessManagementController(commonMessage, $scope, $rootScope, baseServ
         $http({
             method: 'POST',
             url: 'Processes/ProcessManagement/LoadEntityDetails',
-            data: { 'headerId': $scope.ProcessManagementNew.Id}
+            data: { 'headerId': $scope.ProcessManagementNew.Id }
         }).then(function successCallback(response) {
             $scope.EntityList = response.data;
         }
@@ -101,25 +117,27 @@ function ProcessManagementController(commonMessage, $scope, $rootScope, baseServ
     $scope.LoadUtilityGrid = function () {
         $http({
             method: 'POST',
-            url: 'Processes/ProcessManagement/LoadUtilityGrid'
+            url: 'Processes/ProcessManagement/LoadUtilityGrid',
+            data: { 'headerId': $scope.ProcessManagementNew.Id }
         }).then(function successCallback(response) {
             $scope.UtilityList = response.data;
         }
         )
     }
-    $scope.LoadUtilityGrid();
+
 
     $scope.ResponsiblePersonList = [];
     $scope.LoadResponsiblePopupData = function () {
         $http({
             method: 'POST',
             url: 'Processes/ProcessManagement/LoadResponsiblePopupData'
+
         }).then(function successCallback(response) {
             $scope.ResponsiblePersonList = response.data;
             angular.element(document.querySelector('#employeePopUps')).modal('show');
         }
         )
-       
+
     }
 
     $scope.UOMList = [];
@@ -127,7 +145,7 @@ function ProcessManagementController(commonMessage, $scope, $rootScope, baseServ
         $http({
             method: 'POST',
             url: $scope.path + 'GetUOM',
-            dataType:'JSON'
+            dataType: 'JSON'
         })
             .then(function successCallback(response) {
                 $scope.UOMList = response.data;
@@ -140,6 +158,7 @@ function ProcessManagementController(commonMessage, $scope, $rootScope, baseServ
         $http({
             method: 'POST',
             url: $scope.path + 'LoadWorkCenterDetails',
+            data: { 'processId': $scope.ProcessManagementNew.Process },
             dataType: 'JSON'
         })
             .then(function successCallback(response) {
@@ -147,21 +166,6 @@ function ProcessManagementController(commonMessage, $scope, $rootScope, baseServ
             })
     }
     $scope.GetWorkcenter();
-
-    $scope.ProcessManagementTemp = {
-        Id: null,
-        StandardName: null,
-        UserName:null,
-        Process: null,
-        SubProcess:null,
-        MinSPTTime: null,
-        MaxSPTTime:null,
-        StandardSPTTime: null,
-        ResponsiblePerson: null,
-        EmployeeName: null,
-        IsWorkCenterApplicable:null
-    }
-    $scope.ProcessManagementNew = Object.assign({}, $scope.ProcessManagementTemp)
 
     $scope.Save = function () {
         $http({
@@ -184,7 +188,15 @@ function ProcessManagementController(commonMessage, $scope, $rootScope, baseServ
     $scope.SelectedEntityList = [];
     $scope.SaveProcessEntity = function () {
         for (var i = 0; i < $scope.EntityList.length; i++) {
-            if ($scope.EntityList[i].Flag) {
+            //if ($scope.EntityList[i].Flag) {
+            //    $scope.SelectedEntityList.push($scope.EntityList[i]);
+            //}
+
+            if ($scope.EntityList[i].Flag == true && ($scope.EntityList[i].IsActive == null || $scope.EntityList[i].IsActive == false)) {
+                $scope.SelectedEntityList.push($scope.EntityList[i]);
+            }
+            else if ($scope.EntityList[i].Flag == false && $scope.EntityList[i].Id != null) {
+
                 $scope.SelectedEntityList.push($scope.EntityList[i]);
             }
 
@@ -213,57 +225,69 @@ function ProcessManagementController(commonMessage, $scope, $rootScope, baseServ
     $scope.SaveProcessMaterial = function () {
         for (var i = 0; i < $scope.MaterialList.length; i++) {
             if ($scope.MaterialList[i].Flag) {
-                $scope.ChkdMaterialList.push($scope.MaterialList[i]); 
+                $scope.ChkdMaterialList.push($scope.MaterialList[i]);
             }
-
-            $http({
-                method: 'POST',
-                url: $scope.path + 'SaveProcessMaterial',
-                data: {
-                    'datalist': $scope.ChkdMaterialList,
-                    'headerid': $scope.ProcessManagementNew.Id
-                },
-                dataType: 'JSON'
-            })
-                .then(function successCallback(response) {
-                    if (response.data.Error === true) {
-                        ShowResult(response.data.Message, 'failure');
-                    }
-                    else {
-                        ShowResult(response.data.Message, 'success');
-
-                    }
-                })
         }
+
+        $http({
+            method: 'POST',
+            url: $scope.path + 'SaveProcessMaterial',
+            data: {
+                'datalist': $scope.ChkdMaterialList,
+                'headerid': $scope.ProcessManagementNew.Id
+            },
+            dataType: 'JSON'
+        })
+            .then(function successCallback(response) {
+                if (response.data.Error === true) {
+                    ShowResult(response.data.Message, 'failure');
+                }
+                else {
+                    ShowResult(response.data.Message, 'success');
+
+                }
+            })
+
     }
 
     $scope.checkUtilityList = [];
     $scope.SaveProcessUtility = function () {
         for (var i = 0; i < $scope.UtilityList.length; i++) {
-            if ($scope.UtilityList[i].Flag) {
+            //if ($scope.UtilityList[i].Flag) {
+            //    $scope.checkUtilityList.push($scope.UtilityList[i]);
+            //}
+
+            if ($scope.UtilityList[i].Flag == true && ($scope.UtilityList[i].IsActive == null || $scope.UtilityList[i].IsActive == false)) {
                 $scope.checkUtilityList.push($scope.UtilityList[i]);
             }
+            else if ($scope.UtilityList[i].Flag == false && $scope.UtilityList[i].Id != null) {
 
-            $http({
-                method: 'POST',
-                url: $scope.path + 'SaveProcessUtility',
-                data: { 'datalist': $scope.checkUtilityList },
-                dataType: 'JSON'
-            })
-                .then(function successCallback(response) {
-                    if (response.data.Error === true) {
-                        ShowResult(response.data.Message, 'failure');
-                    }
-                    else {
-                        ShowResult(response.data.Message, 'success');
-
-                    }
-                })
+                $scope.checkUtilityList.push($scope.UtilityList[i]);
+            }
         }
+        $http({
+            method: 'POST',
+            url: $scope.path + 'SaveProcessUtility',
+            data: {
+                'datalist': $scope.checkUtilityList,
+                'headerid': $scope.ProcessManagementNew.Id
+            },
+            dataType: 'JSON'
+        })
+            .then(function successCallback(response) {
+                if (response.data.Error === true) {
+                    ShowResult(response.data.Message, 'failure');
+                }
+                else {
+                    ShowResult(response.data.Message, 'success');
+
+                }
+            })
+
     }
 
     $scope.chkdWorkcenterList = [];
-    $scope.SaveWorkcenter = function(){
+    $scope.SaveWorkcenter = function () {
         for (var i = 0; i < $scope.QualityManagementMasterWorkCenterList.length; i++) {
             if ($scope.QualityManagementMasterWorkCenterList[i].Flag) {
                 $scope.chkdWorkcenterList.push($scope.QualityManagementMasterWorkCenterList[i]);
@@ -276,7 +300,7 @@ function ProcessManagementController(commonMessage, $scope, $rootScope, baseServ
             url: $scope.path + 'SaveWorkcenter',
             data: {
                 'datalist': $scope.chkdWorkcenterList
-                
+
             },
             dataType: 'JSON'
         })
@@ -308,7 +332,7 @@ function ProcessManagementController(commonMessage, $scope, $rootScope, baseServ
 
     $scope.GetProcessParameter = function (args) {
         $scope.PMPModelNew = Object.assign({}, args.data)
-        $scope.ActionPP  = 'Update';
+        $scope.ActionPP = 'Update';
         if (!$rootScope.isCollapsed) {
             $rootScope.toggle();
         }
@@ -328,14 +352,14 @@ function ProcessManagementController(commonMessage, $scope, $rootScope, baseServ
     $scope.LoadProcessParameterData();
 
     $scope.PMTemp = {
-        Id:null,
+        Id: null,
         Sequence: null,
         ItemName: null,
         UOMId: null,
         Min: null,
         Max: null,
         Remarks: null,
-        IsUtilityApplicable:null
+        IsUtilityApplicable: null
     }
     $scope.PMPModelNew = Object.assign({}, $scope.PMTemp);
 
@@ -374,7 +398,248 @@ function ProcessManagementController(commonMessage, $scope, $rootScope, baseServ
     }
 
     $scope.DeleteProcessParameter = function () {
-
+        
     }
     // #endregion Process Parameter
+
+    //------------------------------------------------------------------------------------------------------------------------------
+    // #region OWM
+
+    $scope.ModelTemp = {
+        Id: null,
+        Sequence: null,
+        Code: null,
+        ShortName: null,
+        UserName: null,
+        StandardName: null,
+        Active:true,
+        Remarks:null
+    }
+    $scope.ModelNew = Object.assign({}, $scope.ModelTemp);
+
+    $scope.GetSequenceOWM = function () {
+        cboService.getSequence($scope.path + 'GetSequenceOWM', function (data) {
+            $scope.ModelTemp.Sequence = data;
+            $scope.ModelNew.Sequence = data;
+        });
+    };
+    $scope.GetSequenceOWM();
+
+    $scope.EditMode = function (args) {
+        $scope.ModelNew = Object.assign({}, args.data)
+       
+        if (!$rootScope.isCollapsed) {
+            $rootScope.toggle();
+        }
+    }
+
+    $scope.OWMDataList = [];
+    $scope.GetOWMData = function () {
+        $http({
+            method: 'POST',
+            url: $scope.path + 'GetOWMData',
+            dataType:'JSON'
+        })
+            .then(function successCallback(response) {
+                $scope.OWMDataList = response.data;
+            })
+    }
+    $scope.GetOWMData();
+
+
+    $scope.OWMSave = function () {
+        $http({
+            method: 'POST',
+            url: $scope.path + 'OWMSave',
+            data: { 'data': $scope.ModelNew },
+            dataType:'JSON'
+        })
+            .then(function (response) {
+                if (response.data.Error === true) {
+                    ShowResult(response.data.Message, 'failure');
+                }
+                else {
+                    ShowResult(response.data.Message, 'success');
+                    $scope.GetOWMData();
+                    ClearFields($scope.GetSequenceOWM());
+                }
+            })
+    }
+
+    $scope.Clear = function () {
+        ClearFields($scope.GetSequence());
+        return true;
+    };
+
+    function ClearFields(seq) {
+        $scope.Action = 'Save';
+
+        $scope.ModelNew = {
+            Id: null,
+            Sequence: 0,
+            StandardName: null,
+            UserName: null,
+            ShortName: null,
+            Code: null,
+            Active: true,
+            Remarks: null,
+        };
+        $scope.ModelNewGPL = Object.assign({}, $scope.ModelTemp);
+        $scope.ModelNewGPL.Sequence = seq;
+
+    }
+
+    $scope.message_confirmation = "";
+    $scope.removeProcessManagementRowModal = function (tempId) {
+        try {
+            $scope.tempId = tempId;
+            $scope.message_confirmation = "Are you sure want to permanent delete ?";
+            angular.element(document.querySelector('#confirmDetailPopUp')).modal('show');
+        }
+        catch (e) {
+            ShowResult(e, 'Error');
+        }
+    };
+
+    $scope.removeProcessManagementRow = function () {
+        $http({
+            method: 'POST',
+            url: $scope.path + 'DeleteProcessManagementOWM?id=' + $scope.ModelNew.Id,
+           
+            dataType: 'JSON'
+        }).then(function successCallback(response) {
+            if (response.data.Error === true) {
+                ShowResult(response.data.Message, 'failure');
+            }
+            else {
+                ShowResult(response.data.Message, 'success');
+                $scope.getUtilityGridData($scope.ModelNew.Id);
+            }
+            function errorCallBack(response) {
+                ShowResult(response.data.Message, 'failure');
+            }
+        });
+    };
+    // #endregion OWM
+   //------------------------------------------------------------------------------------------------------------------------------
+
+
+   //------------------------------------------------------------------------------------------------------------------------------
+    $scope.ModelTempGPL = {
+        Id: null,
+        Sequence: null,
+        Code: null,
+        ShortName: null,
+        UserName: null,
+        StandardName: null,
+        Active: true,
+        Remarks: null
+    }
+    $scope.ModelNewGPL = Object.assign({}, $scope.ModelTempGPL);
+
+    $scope.GetSequenceGPL = function () {
+        cboService.getSequence($scope.path + 'GetSequenceGPL', function (data) {
+            $scope.ModelTempGPL.Sequence = data;
+            $scope.ModelNewGPL.Sequence = data;
+        });
+    };
+    $scope.GetSequenceGPL();
+
+    $scope.GPLEditMode = function (args) {
+        $scope.ModelNewGPL = Object.assign({}, args.data)
+
+        if (!$rootScope.isCollapsed) {
+            $rootScope.toggle();
+        }
+    }
+
+    $scope.GPLDataList = [];
+    $scope.GetGPLData = function () {
+        $http({
+            method: 'POST',
+            url: $scope.path + 'GetGPLData',
+            dataType: 'JSON'
+        })
+            .then(function successCallback(response) {
+                $scope.GPLDataList = response.data;
+
+            });
+    }
+    $scope.GetGPLData();
+
+    $scope.GPLSave = function () {
+        $http({
+            method: 'POST',
+            url: $scope.path + 'GPLSave',
+            data: { 'data': $scope.ModelNew },
+            dataType: 'JSON'
+        })
+            .then(function (response) {
+                if (response.data.Error === true) {
+                    ShowResult(response.data.Message, 'failure');
+                }
+                else {
+                    ShowResult(response.data.Message, 'success');
+                    $scope.GetGPLData();
+                    ClearFieldsGPL($scope.GetSequenceGPL());
+                }
+            })
+    }
+
+    $scope.ClearGPL = function () {
+        ClearFieldsGPL($scope.GetSequenceGPL());
+        return true;
+    };
+
+    function ClearFieldsGPL(seq) {
+        $scope.Action = 'Save';
+
+        $scope.ModelNewGPL = {
+            Id: null,
+            Sequence: 0,
+            StandardName: null,
+            UserName: null,
+            ShortName: null,
+            Code: null,
+            Active: true,
+            Remarks: null,
+        };
+        $scope.ModelNewGPL = Object.assign({}, $scope.ModelTemp);
+        $scope.ModelNewGPL.Sequence = seq;
+
+    }
+    $scope.message_confirmation = "Are you sure want to permanent delete ?";
+    $scope.removeProcessManagementGPLRowModal = function (tempId) {
+        try {
+            $scope.tempId = tempId;
+            $scope.message_confirmation = "Are you sure want to permanent delete ?";
+            angular.element(document.querySelector('#confirmDetailGPLPopUp')).modal('show');
+        }
+        catch (e) {
+            ShowResult(e, 'Error');
+        }
+    };
+
+    $scope.removeProcessManagementGPLRow = function () {
+        $http({
+            method: 'POST',
+            url: $scope.path + 'DeleteProcessManagementGPL?id=' + $scope.ModelNewGPL.Id,
+
+            dataType: 'JSON'
+        }).then(function successCallback(response) {
+            if (response.data.Error === true) {
+                ShowResult(response.data.Message, 'failure');
+            }
+            else {
+                ShowResult(response.data.Message, 'success');
+                $scope.getUtilityGridData($scope.ModelNew.Id);
+            }
+            function errorCallBack(response) {
+                ShowResult(response.data.Message, 'failure');
+            }
+        });
+    };
+   //------------------------------------------------------------------------------------------------------------------------------
+
+
 }
