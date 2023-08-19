2a45796be4c043fa4cda79c94b9bbd638550df26
Mizanur Rahaman <mizanur@apopinternational.com>
2023-08-19 11:03:26 +0600

    SelectingSales order in PO

--- APLOS/Areas/OrderManagements/Scripts/productionOrderController.js
+++ APLOS/Areas/OrderManagements/Scripts/productionOrderController.js
@@ -595,8 +595,7 @@ function ProductionOrderController(cboService, commonMessage, $scope, $rootScope
                 }
                 isBaseProcess = false;
             }
-            if (!isBaseProcess)
-            {
+            if (!isBaseProcess) {
                 $scope.btndisable = false;
                 throw 'Please select base process';
             }
@@ -889,15 +888,19 @@ function ProductionOrderController(cboService, commonMessage, $scope, $rootScope
 
 
 
-                    if ($scope.recipeMaterialList[i].ProductionGrouping != groupid)
-                        throw "Selecting different group materials are not allowed";
+                    if (!baseService.isUndefinedOrNull($scope.recipeMaterialList[i].ProductionGrouping)){
+                        if ($scope.recipeMaterialList[i].ProductionGrouping != groupid)
+                            throw "Selecting different group materials are not allowed";
 
+                    } else {
+                        if ($scope.recipeMaterialList[i].ArticleId != id)
+                            throw "Selecting different articles are not allowed";
+
+                    }
                     //if ($scope.recipeMaterialList[i].ProductID != productid)
                     //    throw "Selecting different products are not allowed";
 
-                   
-                     if ($scope.recipeMaterialList[i].ArticleId != id)
-                        throw "Selecting different articles are not allowed";
+
 
 
                     //if ($scope.recipeMaterialList[i].MaterialMasterId != id)
@@ -1750,7 +1753,7 @@ function ProductionOrderController(cboService, commonMessage, $scope, $rootScope
 
     $scope.SaveFP = function () {
         try {
-            if (baseService.arrayLength($scope.productionFPWorkCenterList)>0) {
+            if (baseService.arrayLength($scope.productionFPWorkCenterList) > 0) {
                 $http({
                     method: 'POST',
                     url: 'OrderManagements/ProductionOrder/SaveWCFPData',
