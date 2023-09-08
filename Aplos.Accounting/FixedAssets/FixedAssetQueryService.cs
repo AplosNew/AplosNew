484a05491f7a0f61d29a4c2e3b66e6482b899bca
Mizanur Rahaman <mizanur@apopinternational.com>
2023-09-08 12:32:40 +0600

    ApproveCapitalize

--- Aplos.Accounting/FixedAssets/FixedAssetQueryService.cs
+++ Aplos.Accounting/FixedAssets/FixedAssetQueryService.cs
@@ -2488,6 +2488,50 @@ Where CM.IsApproved=1 AND CM.ApprovedById='" + EmployeeId + "'";
                 throw (ex);
             }
         }
+
+        public void ApproveCapitalizeData(Dictionary<string, object> data, out string masterId)
+        {
+            var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+            ConnectionManager.DAL.ConManager objCon;
+            DataSet dsMaster, dsChild = null;
+            string _Id = string.Empty;
+            string _CId = string.Empty;
+            try
+            {
+                bplib.clsGenID genid = new bplib.clsGenID();
+
+                string sql = "SELECT * FROM [TRN].[CapitalizationMaster] WHERE Id='" + data["Id"] + "'";
+                objCon = new ConnectionManager.DAL.ConManager("1");
+                objCon.OpenDataSetThroughAdapter(sql, out dsMaster, false, "1");
+
+                if (dsMaster.Tables[0].Rows.Count == 0)
+                {
+
+                    genid.GenerateIDYearly(DateTime.Now.ToShortDateString().ToString(), "CapitalizationMaster", out _Id);
+
+                    data["Id"] = _Id;
+                    //data["Type"] = "New";
+
+                    AddNewRow(dsMaster.Tables[0], data);
+                }
+                else
+                {
+                    _Id = data["Id"].ToString();
+                    EditRow(dsMaster.Tables[0].Rows[0], data);
+                }
+                masterId = dsMaster.Tables[0].Rows[0]["Id"].ToString();
+                
+
+                clsStaticInfo obj = new clsStaticInfo();
+                obj.SaveDataSets(dsMaster);
+
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
         private void AddNewRowAssetRegister<T>(DataTable dt, T Data)
         {
             Dictionary<string, object> sourceData = Data.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public).ToDictionary(prop => prop.Name, prop => prop.GetValue(Data, null));
