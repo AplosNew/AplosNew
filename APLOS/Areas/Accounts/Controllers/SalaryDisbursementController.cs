7c57530729efb3dc4f7d0dc8a1351872862b96ca
azmal <azmal@apopinternational.com>
2023-12-11 12:54:59 +0600

    bonuspost01

--- APLOS/Areas/Accounts/Controllers/SalaryDisbursementController.cs
+++ APLOS/Areas/Accounts/Controllers/SalaryDisbursementController.cs
@@ -85,6 +85,10 @@ namespace Aplos.Areas.Accounts.Controllers
         {
             return View("~/Areas/Accounts/Views/SalaryDisbursement/BonusDisbursement.cshtml");
         }
+        public ActionResult BonusDisbursementPost()
+        {
+            return View("~/Areas/Accounts/Views/SalaryDisbursement/BonusDisbursementPost.cshtml");
+        }
 
         [Authorize, HttpGet]
         public JsonResult GetSalaryLockDataList(string yearNo, string monthNo, string employeeId, bool isActive, bool isSeperated, bool isMaternity)
