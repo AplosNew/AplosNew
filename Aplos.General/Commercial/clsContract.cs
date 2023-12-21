96f32865b6ac7bdd01688214e3c09537530518bc
Aman Kumar Rai <aman@apopinternational.com>
2023-12-21 12:08:34 +0530

    Contract Templete

--- Aplos.General/Commercial/clsContract.cs
+++ Aplos.General/Commercial/clsContract.cs
@@ -381,7 +381,7 @@ GROUP BY A.UserName,A.StandardValue,A.Sequence,A.FundUtilization,A.Id,A.Remarks,
                                 c.InvoicingByAddress as ConsigneeBillToAddress,c.DeliveryByAddress as ConsigneeShipToAddress,cu.Code as CurrencyName,cu.Id CurrencyId,
                                 p.UserName as MarketingCommissioningAgent,c.ContractNo,FORMAT(c.AddedDate,'dd-MMM-yyyy') AddedDate,PT.UserName PaymentTerm
                                 ,SO.Id SONo,CONVERT(varchar,SO.DeliveryDate,5) DeliveryDate,DS.UserName Destination,moi.BuyerReferenceNo,C.AddedBy CreatedBy
-                                ,B.UserName Bank,BM.AccountNumber,BB.UserName AS BankBranch,BB.IFSCCode
+                                ,B.UserName Bank,BM.AccountNumber,BB.UserName AS BankBranch,BB.IFSCCode , BB.SWIFTCode , AM.Address1 Addresss
                                 from dbo.[Contract] C
                                 left join  TRN.SalesOrder as so on C.Id=SO.ContractId
                                 left join TRN.MasterOrderItem as moi on moi.Id=SO.MasterOrderItemId
@@ -400,6 +400,7 @@ GROUP BY A.UserName,A.StandardValue,A.Sequence,A.FundUtilization,A.Id,A.Remarks,
                                 LEFT JOIN HKP.Bank B ON B.Id=C.BankId
 								LEFT JOIN MST.BankMaster BM ON BM.BankId=B.Id
 								LEFT JOIN HKP.BankBranch BB ON BB.BankId = BM.BankId AND BB.Id = BM.BankBranchId
+								left join mst.AddressMaster AM on AM.Id = BB.AddressMasterId 
                                 where c.Id='" + ContractId + "'";
 
                 return _sqlRepository.GetDataTable(strSQL);
