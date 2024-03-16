ea28b2f59a3c321120b9f925d36248c0efb13986
Mizanur Rahman <mizanur@apopinternational.com>
2024-03-16 10:19:11 +0600

    MasterOrderItem id creation

--- APLOS.Service/OrderManagements/MasterOrderService.cs
+++ APLOS.Service/OrderManagements/MasterOrderService.cs
@@ -1759,8 +1759,8 @@ WHERE MOI.MasterOrderId='" + id + "'";
                     var thirdCharDbList = _thirdCharacteristicsRepository.Query(t => salesOrderIds.Contains(t.SalesOrderId)).Select().ToList();
                     var SOCostingConfirmationDbList = _SOCostingConfirmationRepository.Query(t => salesOrderIds.Contains(t.SalesOrderId)).Select().ToList();
 
-                   // var count = _itemRepository.SqlQuery<int>($"SELECT ISNULL(MAX(CAST(RIGHT(Id, 2) AS INT)), 0) Id FROM [TRN].[MasterOrderItem] WHERE MasterOrderId='{masterId}'").First();
-                    var count = _itemRepository.SqlQuery<int>($"SELECT CAST((RIGHT(ISNULL(MAX(CAST(Id AS INT)), 0),3)) AS INT) Id FROM [TRN].[MasterOrderItem] WHERE MasterOrderId='{masterId}'").First();
+                    var count = _itemRepository.SqlQuery<int>($"SELECT ISNULL(MAX(CAST(RIGHT(Id, 2) AS INT)), 0) Id FROM [TRN].[MasterOrderItem] WHERE MasterOrderId='{masterId}'").First();
+                    //var count = _itemRepository.SqlQuery<int>($"SELECT CAST((RIGHT(ISNULL(MAX(CAST(Id AS INT)), 0),3)) AS INT) Id FROM [TRN].[MasterOrderItem] WHERE MasterOrderId='{masterId}'").First();
                     foreach (var item in itemList)
                     {
                         if (item.TotalQty == 0) throw new CustomException("Add Qty");
@@ -1768,7 +1768,7 @@ WHERE MOI.MasterOrderId='" + id + "'";
                         {
                             count++;
                             item.ProductionGrouping = RemoveSpace(item.ProductionGrouping);
-                            item.Id = MakePK(masterId, count, 2);
+                            item.Id = MakePK(masterId, count, 3);
                             item.MasterOrderId = masterId;
                             AuditService.AddedLog(item);
                             _itemRepository.Insert(item);
