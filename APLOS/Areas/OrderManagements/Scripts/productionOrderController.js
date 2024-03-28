887bea8f8e3230994c36225b94e9d78da9b64a59
Mizanur Rahman <mizanur@apopinternational.com>
2024-03-28 17:33:22 +0600

    show error in Lot No in PO

--- APLOS/Areas/OrderManagements/Scripts/productionOrderController.js
+++ APLOS/Areas/OrderManagements/Scripts/productionOrderController.js
@@ -4328,21 +4328,28 @@ function ProductionOrderController(cboService, commonMessage, $scope, $rootScope
                 url: 'OrderManagements/ProductionOrder/GetPOLotControlSettingsData?poId=' + $scope.model.Id + '&entityId=' + $scope.model.EntityId + '&userLotNo=' + $scope.model.UserDefineLotNo
             }).then(function (response) {
 
-                if (baseService.arrayLength($scope.lotControlList) > 0) {
-                    for (var i = 0; i < $scope.lotControlList.length; i++) {
-                        for (var j = 0; j < response.data.length; j++) {
-                            if (!baseService.isUndefinedOrNull($scope.lotControlList[i].Id) && $scope.lotControlList[i].ProcessId == response.data[j].ProcessId && $scope.lotControlList[i].SeqNo == response.data[j].SeqNo) {
-                                $scope.lotControlList[i].LotNo = response.data[j].LotNo;
-                                $scope.lotControlList[i].UserLotNo = response.data[j].UserLotNo;
+                if (response.data.Error === true) {
+                    ShowResult(response.data.Message, 'failure');
+                }
+                else {
+                    ShowResult(response.data.Message, 'success');
+                    if (baseService.arrayLength($scope.lotControlList) > 0) {
+                        for (var i = 0; i < $scope.lotControlList.length; i++) {
+                            for (var j = 0; j < response.data.length; j++) {
+                                if (!baseService.isUndefinedOrNull($scope.lotControlList[i].Id) && $scope.lotControlList[i].ProcessId == response.data[j].ProcessId && $scope.lotControlList[i].SeqNo == response.data[j].SeqNo) {
+                                    $scope.lotControlList[i].LotNo = response.data[j].LotNo;
+                                    $scope.lotControlList[i].UserLotNo = response.data[j].UserLotNo;
+                                }
                             }
                         }
+                    } else {
+                        $scope.lotControlList = response.data;
                     }
-                } else {
-                    $scope.lotControlList = response.data;
+                    var gridObj = $("#GridLC").data("ejGrid");
+                    gridObj.refreshContent();
+                    gridObj.refreshTemplate();
                 }
-                var gridObj = $("#GridLC").data("ejGrid");
-                gridObj.refreshContent();
-                gridObj.refreshTemplate();
+                
             });
         } catch (e) {
             ShowResult(e, 'failure');
