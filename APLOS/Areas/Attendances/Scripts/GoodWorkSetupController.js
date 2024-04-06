e886c91703f7c22f3ce6e4d09c1b1774a6d25643
Mizanur Rahman <mizanur@apopinternational.com>
2024-04-06 13:15:22 +0600

    MedicalLog

--- APLOS/Areas/Attendances/Scripts/GoodWorkSetupController.js
+++ APLOS/Areas/Attendances/Scripts/GoodWorkSetupController.js
@@ -84,7 +84,6 @@ function GoodWorkSetupController(cboService, commonMessage, $scope, $rootScope, 
         $scope.closePopUp2();
     }
 
-
     $scope.clearEmp = function () {
         $scope.ModelNew.ResponsiblePersonId = null;
         $scope.ModelNew.ResponsiblePerson = null;
@@ -94,8 +93,6 @@ function GoodWorkSetupController(cboService, commonMessage, $scope, $rootScope, 
     $scope.closePopUp2 = function () {
         angular.element(document.querySelector('#popUp')).modal('hide');
     }
-
-
 
     $scope.Save = function () {
         $scope.$broadcast('show-errors-check-validity');
@@ -143,7 +140,6 @@ function GoodWorkSetupController(cboService, commonMessage, $scope, $rootScope, 
             });
         }
     };
-
 
     //#region Entity
 
