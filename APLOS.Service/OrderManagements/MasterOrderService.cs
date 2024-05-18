bfa9a3de81ecffc10b9414fe3b990048e344deda
Mizanur Rahman <mizanur@apopinternational.com>
2024-05-18 11:47:18 +0600

    UserRemarksControl

--- APLOS.Service/OrderManagements/MasterOrderService.cs
+++ APLOS.Service/OrderManagements/MasterOrderService.cs
@@ -1928,7 +1928,7 @@ WHERE MOI.MasterOrderId='" + id + "'";
                 if (userRemarksControl.RemarkControlId != null)
                 {
                     var data = _UserRemarksControlRepository.Find(userRemarksControl.MasterOrderId);
-                    if (data == null)
+                    if (data.Id == null)
                     {
                         userRemarksControl.Id = entity.Id;
                         userRemarksControl.MasterOrderId = entity.Id;
