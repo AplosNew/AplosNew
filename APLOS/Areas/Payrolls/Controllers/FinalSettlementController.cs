c279e51999b0555e24416c2638987f5608922be6
mizanur <mizanur@apopinternational.com>
2024-08-10 20:46:15 +0600

    EmployeeSeperationItem

--- APLOS/Areas/Payrolls/Controllers/FinalSettlementController.cs
+++ APLOS/Areas/Payrolls/Controllers/FinalSettlementController.cs
@@ -2055,7 +2055,7 @@ Left join SalaryLock sl on sl.EmpSystemId=spc.EmpInfoSystemID AND sl.YearNo=SPM.
 LEFT JOIN TRN.Voucher  V ON V.Id=sl.PayableVoucherId 
 left join trn.VoucherDetail vd on vd.VoucherId=v.Id and vd.TrnNature ='Annual Bonus' and vd.SalaryHeadId=SPC.SalaryHeadID and vd.CrAmount>0 
 Where HeadCategory IN('Annual Bonus Retain') AND ISNULL(SPC.DisbusmentAmount,0)!=0
-AND ISNULL(sl.PayableVoucherId,'')<>'' and sl.islocked=1 AND sl.BonusDisbursementVoucherId IS NULL 
+AND ISNULL(sl.PayableVoucherId,'')<>'' and sl.islocked=1 AND sl.BonusDisbursementVoucherId IS NULL AND ISNULL(sl.IsBonusDisbursed,0)=0 AND sl.BonusDisbursementAdviceId IS NULL
 AND SPC.EmpInfoSystemID='" + empId + @"')A
 			 ) AS varchar(100))
            
@@ -2282,7 +2282,7 @@ ORDER BY OL.Sequence";
                 }
 
 
-                elockBNsql = @"Select * from  dbo.SalaryLock where EmpSystemId IN(" + empIds + ") AND PayableVoucherId<>'' AND BonusDisbursementVoucherId IS NULL";
+                elockBNsql = @"Select * from  dbo.SalaryLock where EmpSystemId IN(" + empIds + ") AND PayableVoucherId<>'' AND BonusDisbursementVoucherId IS NULL AND ISNULL(IsBonusDisbursed,0)=0 AND BonusDisbursementAdviceId IS NULL";
                 con.OpenDataSetThroughAdapter(elockBNsql, out dsEmpBN, false, "1");
 
 
