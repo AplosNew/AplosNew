11f9a2749f044c492c3022addff12672223961c1
Belayet Rubel ahmed <belayet@apopinternational.com>
2024-11-25 13:56:59 +0600

    Service Pocurment Service Master

--- APLOS/Areas/Products/Controllers/PurchaseOrderController.cs
+++ APLOS/Areas/Products/Controllers/PurchaseOrderController.cs
@@ -3221,17 +3221,12 @@ LEFT JOIN dbo.EmployeeInformation EI2 ON EI2.SystemId=IR.ApprovedBy
             try
             {
                 var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
-                var sql = @"Select 
-                a.Id ServicePODetailId,a.ServicePOMasterId
-                ,b.Id ServiceMasterId
-                ,b.UserName ServiceMasterName
-                , a.Amount 
-                ,c.TaxAmount TotalTaxAmount
-                ,0 [check]
-                ,d.IsNonCreditable
+                var sql = @"Select  a.Id ServicePODetailId,a.ServicePOMasterId
+                ,b.Id ServiceMasterId ,b.UserName ServiceMasterName
+                , a.Amount  ,c.TaxAmount TotalTaxAmount
+                ,0 [check] ,d.IsNonCreditable
                 ,TotalAmount=CASE WHEN d.IsNonCreditable=1 then (a.Amount + c.TaxAmount) Else a.Amount  END
-				,a.Qty
-				,a.Rate
+				,a.Qty ,a.Rate,a.BudgetMasterId,a.ActivityId
 				,UOM.Username UoM,null CurrentQty,A.TransactionUoMId,Mapdata.Qty OtherReceived,Balance=Isnull(a.Qty,0)-ISNULL(Mapdata.Qty,0)
                 FROM trn. ServicePODetail a
                 LEFT join trn.ServicePOMaster d on d.id=a.ServicePOMasterId
