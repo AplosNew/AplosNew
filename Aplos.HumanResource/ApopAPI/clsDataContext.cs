ff60fa49b69225b1ee427a4d87a3b350eaa7594c
Aman Kumar Rai <aman@apopinternational.com>
2025-01-11 17:55:01 +0530

    Publish

--- Aplos.HumanResource/ApopAPI/clsDataContext.cs
+++ Aplos.HumanResource/ApopAPI/clsDataContext.cs
@@ -13255,7 +13255,7 @@ OUTER APPLY(Select * from [dbo].[SalesAdditionalInfo] Where AdditionalInfoId=A.I
                     LotNo += ",'" + item.LotNo + "'";
                 }
 
-                con.OpenDataSetThroughAdapter("select * from dbo.ScanRawData where 1 = 1", out dsMaster, false, "1");
+                con.OpenDataSetThroughAdapter("select * from dbo.ScanRawData where LotNo = '" + items[0].LotNo + "'", out dsMaster, false, "1");
 
 
                 foreach (PacketScanData item in DataToSave)
