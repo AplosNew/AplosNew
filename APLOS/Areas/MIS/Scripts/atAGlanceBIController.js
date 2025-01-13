0155fd3f9474f0d916f11827857286238f852445
Belayet Rubel ahmed <belayet@apopinternational.com>
2025-01-13 12:29:59 +0600

    BOQ UPdate unprocessed

--- APLOS/Areas/MIS/Scripts/atAGlanceBIController.js
+++ APLOS/Areas/MIS/Scripts/atAGlanceBIController.js
@@ -1,5 +1,13 @@
 ﻿'use strict';
-atAGlanceBIController.$inject = ['commonMessage', '$scope', '$rootScope', 'baseService', '$routeParams', '$location', '$http', '$filter'];
-function atAGlanceBIController(commonMessage, $scope, $rootScope, baseService, $routeParams, $location, $http, $filter) {
+atAGlanceBIController.$inject = ['commonMessage', '$scope', 'cboService','$rootScope', 'baseService', '$routeParams', '$location', '$http', '$filter'];
+function atAGlanceBIController(commonMessage, $scope, cboService, $rootScope, baseService, $routeParams, $location, $http, $filter) {
     $rootScope.title = "At A Glance";
+
+    $scope.comInfo = {};
+    cboService.getCboInterCompany(null, function (result) {
+        $scope.companyList = result;
+        $scope.comInfo.CompanyId = $scope.companyList[0].CompanyId;
+        $scope.comInfo.CompanyName = $scope.companyList[0].CompanyName;
+    });
+
 }
--- APLOS/Areas/MIS/Scripts/manpowerStatusBIController.js
+++ APLOS/Areas/MIS/Scripts/manpowerStatusBIController.js
@@ -1,5 +1,11 @@
 ﻿'use strict';
-manpowerStatusBIController.$inject = ['commonMessage', '$scope', '$rootScope', 'baseService', '$routeParams', '$location', '$http', '$filter'];
-function manpowerStatusBIController(commonMessage, $scope, $rootScope, baseService, $routeParams, $location, $http, $filter) {
+manpowerStatusBIController.$inject = ['commonMessage', '$scope', '$rootScope', 'cboService', 'baseService', '$routeParams', '$location', '$http', '$filter'];
+function manpowerStatusBIController(commonMessage, $scope, $rootScope, cboService, baseService, $routeParams, $location, $http, $filter) {
     $rootScope.title = "Manpower Status";
+    $scope.comInfo = {};
+    cboService.getCboInterCompany(null, function (result) {
+        $scope.companyList = result;
+        $scope.comInfo.CompanyId = $scope.companyList[0].CompanyId;
+        $scope.comInfo.CompanyName = $scope.companyList[0].CompanyName;
+    });
 }
