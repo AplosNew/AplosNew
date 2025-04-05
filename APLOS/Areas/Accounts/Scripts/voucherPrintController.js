9051ccfbbb65b469ff4363d0b1a58da91686981b
azmal <azmal@apopinternational.com>
2025-04-05 17:41:21 +0600

    vp05

---
+++ APLOS/Areas/Accounts/Scripts/voucherPrintController.js
@@ -0,0 +1,64 @@
+﻿'use strict';
+voucherPrintController.$inject = ['cboService', 'commonMessage', '$scope', '$rootScope', 'baseService', '$routeParams', '$location', '$http', '$filter', '$window'];
+function voucherPrintController(cboService, commonMessage, $scope, $rootScope, baseService, $routeParams, $location, $http, $filter, $window) {
+    $rootScope.title = 'VoucherPark';
+    $scope.Action = 'Save';
+    $scope.path = 'Accounts/VoucherPark/';
+    $scope.url = "Accounts/VoucherPark";
+    
+    $scope.voucher = {
+        Id: null,
+        VoucherNo: null
+    };
+
+
+    $scope.VoucherDataList = [];
+    $scope.getVoucherData = function () {
+        try {
+            $http({
+                method: 'POST',
+                url: $scope.path + "GetVoucherDataListforPrint",
+                data: { voucherNo: $scope.voucher.VoucherNo},
+                dataType: 'JSON'
+
+            }).then(function successCallback(response) {
+                $scope.VoucherDataList = response.data.DATA;
+
+            }),
+                function errorCallBack(response) {
+                    ShowResult(response.data.Message, 'failure');
+                }
+        }
+
+        catch (e) {
+
+        }
+    }
+
+    $scope.PrintReport = function (reportFormat, SourceType, voucherId, beneficiaryType) {
+        if (SourceType == 'VendorInvoice') {
+            if (beneficiaryType == 'Vendor') {
+                $window.open('Accounts/Invoice/ReportVendorInvoice?reportFormat=' + reportFormat + '&voucherId=' + voucherId, '_blank');
+            }
+            else {
+                $window.open('Employees/EmployeeReport/GetEmployeePayableExpenseReport?reportFormat=' + reportFormat + '&voucherId=' + voucherId, '_blank');
+            }
+        }
+        else if (SourceType == 'BankJournal') {
+            $window.open('Banks/BankReport/GetBankJournalReport?reportFormat=' + reportFormat + '&voucherId=' + voucherId, '_blank');
+        }
+        else if (SourceType == 'CashJournal') {
+            $window.open('Banks/CashReport/GetCashJournalReport?reportFormat=' + reportFormat + '&voucherId=' + voucherId, '_blank');
+        }
+        else {
+            $window.open('Accounts/Invoice/ReportVendorInvoice?reportFormat=' + reportFormat + '&voucherId=' + voucherId, '_blank');
+        }
+    }
+  
+};
+
+
+
+
+
+
