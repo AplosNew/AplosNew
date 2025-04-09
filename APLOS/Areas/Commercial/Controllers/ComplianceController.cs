4cf30071dd505a74c8aadaa8d9e6f378258d7ca4
Mizanur Rahaman <mizanur@apopinternational.com>
2025-04-09 19:44:49 +0600

    ComplianceMaster

---
+++ APLOS/Areas/Commercial/Controllers/ComplianceController.cs
@@ -0,0 +1,200 @@
+﻿#region Using
+
+using Aplos.Controllers;
+using Aplos.Properties;
+using Library.Core;
+using Library.Crosscutting.Security;
+using Library.Data.Sql;
+using Library.Model.Setups;
+using Library.Service.Enums;
+using Library.Service.Setups;
+using OTSBD;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading;
+using System.Web.Mvc;
+
+#endregion Using
+
+namespace Aplos.Areas.Commercial.Controllers
+{
+    public class ComplianceController : BaseController
+    {
+        string TableName = "hkp.ComplianceMaster";
+        //authentication for
+        //GetList Create Delete
+
+
+        #region Constructor
+
+        private readonly ISqlRepository _sqlRepository;
+        public ComplianceController(ISqlRepository R)
+        {
+            _sqlRepository = R;
+        }
+
+        #endregion Constructor
+
+
+        [Authorize]
+        public ActionResult Aplos()
+        {
+            return View();
+        }
+
+       
+
+        [HttpPost, Authorize]
+        public ActionResult GetList(string column, string value)
+        {
+            string strkey = "1=1";
+            if (string.IsNullOrEmpty(column) == false && string.IsNullOrEmpty(value) == false)
+                strkey = column + " like '%" + value + "%'";
+
+            var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+            string sql = @"select top 100 * from (SELECT * FROM " + TableName + ") AS TEMP WHERE " + strkey + "";
+
+
+
+            return Json(_sqlRepository.GetDataCollection(sql, null), JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public JsonResult Create(Dictionary<string, object> data)
+        {
+            try
+            {
+                DataSet dsMaster;
+                ConnectionManager.DAL.ConManager con = new ConnectionManager.DAL.ConManager("1");
+                con.OpenDataSetThroughAdapter("select * from " + TableName + " where ItemName='" + data["UserName"] + "'  AND  Id<>'" + data["Id"] + "'", out dsMaster, false, "1");
+                if (dsMaster.Tables[0].Rows.Count > 0)
+                    throw new Exception("ItemName already exists!!!");
+
+                con.OpenDataSetThroughAdapter("select * from " + TableName + " where   Code='" + data["Code"] + "' AND  Id<>'" + data["Id"] + "'", out dsMaster, false, "1");
+                if (dsMaster.Tables[0].Rows.Count > 0)
+                    throw new Exception("Code already exists!!!");
+
+
+
+                con.OpenDataSetThroughAdapter("select * from " + TableName + " where Id='" + data["Id"] + "'", out dsMaster, false, "1");
+
+                string _Id = "";
+
+
+
+
+                #region data update
+                if (dsMaster.Tables[0].Rows.Count == 0)
+                {
+                    bplib.clsGenID genid = new bplib.clsGenID();
+                    genid.GenerateIDYearly(DateTime.Now.ToShortDateString().ToString(), nameof(TableName), out _Id);
+
+                    data["Id"] =  _Id;
+                    AddNewRow(dsMaster.Tables[0], data);
+                }
+                else
+                {
+                    _Id = data["Id"].ToString();
+                    EditRow(dsMaster.Tables[0].Rows[0], data);
+                }
+                #endregion data update
+
+
+                clsStaticInfo _info = new clsStaticInfo();
+                _info.SaveDataSets(dsMaster);
+
+
+                return Json(new { Error = false,Message = AplosMessage.Updated });
+
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new { Error = true, Message = ex.Message });
+
+            }
+        }
+
+        public ActionResult Delete(string id)
+        {
+            string sql = @"select * from [HKP].[LCOpenChargesTypeGL] where LCOpenChargesTypeId = '"+ id + "'";
+                try
+                {
+                    if (string.IsNullOrEmpty(id))
+                        throw new Exception("Select entry first");
+
+                    ConnectionManager.clsConnection con = new ConnectionManager.clsConnection();
+                    con.BeginTransaction();
+                    con.executeQuery("delete from " + TableName + " where id='" + id + "'");
+                    con.CommitTransaction();
+
+                    return Json(new { Error = false, Message = AplosMessage.Deleted }, JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception ex)
+                {
+                    return Json(new { Error = true, Message = ex.Message }, JsonRequestBehavior.AllowGet);
+                }
+            
+
+        }
+
+
+        private void AddNewRow(DataTable dt, Dictionary<string, object> sourceData)
+        {
+            var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+            DataRow dr = dt.NewRow();
+
+            foreach (var item in sourceData.Keys)
+            {
+                try
+                {
+                    dr[item] = sourceData[item];
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+
+
+           
+            dr["AddedBy"] = identity.Name;
+            dr["AddedDate"] = System.DateTime.Now.ToString();
+            dr["AddedFromIP"] = identity.IPAddress;
+            //dr["UpdatedBy"] = identity.Name;
+            //dr["UpdatedDate"] = System.DateTime.Now.ToString();
+            //dr["UpdatedFromIP"] = identity.IPAddress;
+
+            dt.Rows.Add(dr);
+        }
+        private void EditRow(DataRow dr, Dictionary<string, object> sourceData)
+        {
+            var identity = (CustomIdentity)Thread.CurrentPrincipal.Identity;
+            dr.BeginEdit();
+
+            foreach (var item in sourceData.Keys)
+            {
+                try
+                {
+                    dr[item] = sourceData[item];
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            
+            dr["UpdatedBy"] = identity.Name;
+            dr["UpdatedDate"] = System.DateTime.Now.ToString();
+            dr["UpdatedFromIP"] = identity.IPAddress;
+
+            dr.EndEdit();
+        }
+       
+
+        
+
+    }
+}
---
+++ APLOS/Areas/Commercial/Scripts/ComplianceController.js
@@ -0,0 +1,124 @@
+﻿'use strict';
+ComplianceController.$inject = ['cboService', 'commonMessage', '$scope', '$rootScope', 'baseService', '$routeParams', '$location', '$http', '$filter'];
+function ComplianceController(cboService, commonMessage, $scope, $rootScope, baseService, $routeParams, $location, $http, $filter) {
+    $rootScope.title = 'Compliance';
+    $scope.Action = 'Save';
+    $scope.ModelList = [];
+    $scope.path = 'Commercial/Compliance/';
+    $scope.getListUrl = $scope.path + 'getlist';
+    $scope.saveUrl = $scope.path + 'create';
+    $scope.deleteUrl = $scope.path + 'delete/';
+    $scope.Action = 'Save';
+    $scope.searchBy = "Code"; $scope.search = "";
+    $scope.searchByList = [{ value: 'Id', name: "Id" }, { value: 'Code', name: "Code" },  { value: 'Remarks', name: "Remarks" }];
+
+    $scope.CriticalityLevelList = [
+        { 'Value': "Normal", 'Text': "Normal" },
+        { 'Value': "Critical", 'Text': "Critical" },
+        { 'Value': "Important", 'Text': "Important" }
+    ];
+
+    $scope.ComplianceValueList = [
+        { 'Value': "0", 'Text': "0" },
+        { 'Value': "1", 'Text': "1" },
+        { 'Value': "2", 'Text': "2" },
+        { 'Value': "3", 'Text': "3" }
+    ];
+
+
+    $scope.getData = function () {
+        $http({
+            method: 'POST',
+            url: $scope.path + "GetList",
+            data: { column: $scope.searchBy, value: $scope.search },
+            dataType: 'JSON'
+        }).then(function successCallback(response) {
+            $scope.ModelList = response.data;
+        });
+    }
+    $scope.getData();
+
+
+
+    $scope.ModelTemp = {
+        Id: null,
+        Sequence: 0,
+        Code: null,
+        ShortName: null,
+        StandardName: null,
+        UserName: null,
+        Description: null,
+        Remarks: null,
+        Active: true,
+        Type: null
+    };
+    $scope.ModelNew = Object.assign({}, $scope.ModelTemp);
+
+
+
+    $scope.Get = function (args) {
+
+        $scope.ModelNew = Object.assign({}, args.data);
+        $scope.Action = 'Update';
+        if (!$rootScope.isCollapsed) {
+            $rootScope.toggle();
+        }
+    };
+
+    $scope.Save = function () {
+        $scope.$broadcast('show-errors-check-validity');
+        if ($scope.ModelNewForm.$valid) {
+            $http({
+                method: 'POST',
+                url: $scope.saveUrl,
+                data: { 'data': $scope.ModelNew },
+                dataType: 'JSON'
+            }).then(function successCallback(response) {
+                if (response.data.Error === true) {
+                    ShowResult(response.data.Message, 'failure');
+                }
+                else {
+                    ShowResult(response.data.Message, 'success');
+                    ClearFields();
+                    $scope.getData();
+
+                }
+            }), function errorCallBack(response) {
+                ShowResult(response.data.Message, 'failure');
+            }
+
+        }
+    };
+
+    $scope.Delete = function () {
+        if (!baseService.isUndefinedOrNull($scope.ModelNew.Id)) {
+            $http({
+                method: 'POST',
+                url: $scope.deleteUrl + $scope.ModelNew.Id,
+                dataType: 'JSON'
+            }).then(function successCallback(response) {
+                if (response.data.Error === true) {
+                    ShowResult(response.data.Message, 'failure');
+                }
+                else {
+                    ShowResult(response.data.Message, 'success');
+                    ClearFields();
+                    $scope.getData();
+                }
+                function errorCallBack(response) {
+                    ShowResult(response.data.Message, 'failure');
+                }
+            });
+        }
+    };
+
+    $scope.Clear = function () {
+        ClearFields();
+        return true;
+    };
+
+    function ClearFields() {
+        $scope.Action = 'Save';
+        $scope.ModelNew = Object.assign({}, $scope.ModelTemp);
+    }
+}
