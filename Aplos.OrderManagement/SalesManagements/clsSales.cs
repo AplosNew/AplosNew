6a763cf733e4ae35fbbbcee17732f235d50e9587
Belayet Rubel ahmed <belayet@apopinternational.com>
2025-09-24 18:54:07 +0600

    packing wise sales order by invoicedate desc and invoiceno

--- Aplos.OrderManagement/SalesManagements/clsSales.cs
+++ Aplos.OrderManagement/SalesManagements/clsSales.cs
@@ -478,8 +478,8 @@ OUTER APPLY(Select * from [dbo].[SalesAdditionalInfo] Where AdditionalInfoId=A.I
         {
             try
             {
-                parameters.sort = "CAST(AddedDate AS datetime)";
-                //parameters.sort = "TAB.AddedDate,TAB.InvoiceNo";
+                //parameters.sort = "CAST(InvoiceDate AS datetime)";
+                parameters.sort = "CAST(InvoiceDate AS datetime) desc,InvoiceNo ";
                 parameters.CmdText = @"SELECT S.Id,S.Id AS SalesId, S.PartyId, P.Code AS PartyCode, P.UserName AS PartyName, S.CurrencyId,CO.BaseCurrencyId, C.Code AS CurrencyCode, S.DocRefNo, ISNULL(SM.Amount,0) + ISNULL(SS.Amount,0) AS Amount,
 									 Replace(CONVERT(VARCHAR(11), S.InvoiceDate, 106), ' ', '-') InvoiceDate,
 									Replace(CONVERT(VARCHAR(11), S.EntryDate, 106), ' ', '-') VoucherDate, Replace(CONVERT(VARCHAR(11), S.InvoiceDate, 106), ' ', '-') PostingDate
