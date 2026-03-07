dd8f6683f05463a16db2a26ffe353e83a1f05f15
Aman Kumar Rai <aman@apopinternational.com>
2026-03-07 10:24:27 +0530

    Master order

--- APLOS.Domain/OrderManagements/MasterOrderItem.cs
+++ APLOS.Domain/OrderManagements/MasterOrderItem.cs
@@ -27,7 +27,7 @@ namespace Library.Model.OrderManagements
         public string Remark { get; set; }
         public string ItemCategory { get; set; }
         public string OrderStatusId { get; set; }
-        public string CSPT { get; set; }
+        public decimal? CSPT { get; set; }
         #endregion Scalar Properties
 
         #region Audit Properties
