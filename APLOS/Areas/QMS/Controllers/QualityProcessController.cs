b084fde648014779de9f063226300d69f8d936eb
Belayet Rubel ahmed <belayet@apopinternational.com>
2026-06-09 10:46:32 +0600

    image master

--- APLOS/Areas/QMS/Controllers/QualityProcessController.cs
+++ APLOS/Areas/QMS/Controllers/QualityProcessController.cs
@@ -330,7 +330,6 @@ SELECT Emp.SystemID,EMP.EmployeeName,EMP.EmployeeCode,EMP.EmpPicPath,EMP.BudgetC
             DataTable dt = _sqlRepository.GetDataTable("SELECT  isnull(Max(Sequence),0) AS Sequence FROM " + TableName + "");
             if (dt.Rows.Count > 0)
                 return clsStaticInfo.dbl(dt.Rows[0]["Sequence"].ToString()) + 1;
-
             return 1;
         }
 
