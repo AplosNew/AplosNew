d6f972ead78782c976381de8d3264fe16a74a68c
Mizanur Rahaman <mizanur@apopinternational.com>
2026-08-13 14:22:10 +0600

    sku reg

--- APLOS/Areas/OrderManagements/Scripts/SKURegistrationController.js
+++ APLOS/Areas/OrderManagements/Scripts/SKURegistrationController.js
@@ -3,11 +3,25 @@ SKURegistrationController.$inject = ['cboService', '$window', 'commonMessage', '
 function SKURegistrationController(cboService, $window, commonMessage, $scope, $rootScope, baseService, $routeParams, $location, $http, $filter) {
     $rootScope.title = "SKU Registration";
     $scope.Action = 'Save';
+    $scope.ModelList = [];
     $scope.path = 'OrderManagements/ProductionOrder/';
+    $scope.getListUrl = $scope.path + 'GetPacketRegistrationMasterList';
+    $scope.saveUrl = $scope.path + 'CreatePacketRegistrationMaster';
+    $scope.deleteUrl = $scope.path + 'DeletePRM/';
+    $scope.searchBy = "UserName"; $scope.search = "";
+    $scope.searchByList = [{ value: 'Id', name: "Id" }, { value: 'UserName', name: "User Name" }, { value: 'Remarks', name: "Remarks" }];
+
+    $scope.tab = 1;
+    $scope.setTab = function (newTab) {
+        $scope.tab = newTab;
+    };
+    $scope.isSet = function (tabNum) {
+        return $scope.tab === tabNum;
+    };
 
     $scope.getFiltersData = function () {
         try {
-          
+
             $http({
                 method: 'GET',
                 url: 'OrderManagements/ProductionOrder/GetSalesOrderFilterData',
@@ -41,9 +55,276 @@ function SKURegistrationController(cboService, $window, commonMessage, $scope, $
     }
     $scope.getFiltersData();
 
+    $scope.ModelList = [];
+    $scope.getData = function () {
+        $http({
+            method: 'POST',
+            url: $scope.getListUrl,
+            data: { column: $scope.searchBy, value: $scope.search },
+            dataType: 'JSON'
+        }).then(function successCallback(response) {
+            $scope.ModelList = response.data;
+        });
+    }
+    $scope.getData();
 
 
+    $scope.ModelTemp = {
+        Id: null,
+        UserName: null,
+        EmployeeId: null,
+        TargetClosingDays: 0,
+        Remarks: null,
+        AddedBy: null,
+        AddedDate: null,
+        AddedFromIP: null,
+        UpdatedBy: null,
+        UpdatedDate: null,
+        UpdatedFromIP: null
 
+    };
+    $scope.ModelNew = Object.assign({}, $scope.ModelTemp);
+
+    $scope.Get = function (args) {
+        $scope.ModelNew = Object.assign({}, args.data);
+        $scope.Action = 'Update';
+        if (!$rootScope.isCollapsed) {
+            $rootScope.toggle();
+        }
+    };
+
+    $scope.popUpDataList = [];
+    $scope.getEmpPopUpData = function () {
+        try {
+
+            $scope.popUpDataList = [];
+            $http({
+                method: 'GET',
+                url: 'employees/authorizationconfig/getallemployeedata'
+
+            }).then(function successCallback(response) {
+                $scope.popUpDataList = response.data;
+            });
+            angular.element(document.querySelector('#popUp')).modal('show');
+        } catch (e) {
+            ShowResult(e, 'failure');
+        }
+    };
+
+
+    $scope.selectdblClick = function (obj) {
+        var ob = obj.data;
+        $scope.ModelNew.EmployeeId = ob.SystemId;
+        $scope.ModelNew.EmployeeName = ob.EmployeeName;
+        $scope.ModelNew.EmployeeCode = ob.EmployeeCode;
+        angular.element(document.querySelector('#popUp')).modal('hide');
+    };
+
+    $scope.closePopUp = function () {
+        angular.element(document.querySelector('#popUp')).modal('hide');
+    };
+
+
+    $scope.Save = function () {
+        $scope.$broadcast('show-errors-check-validity');
+        if ($scope.ModelNewForm.$valid) {
+            $http({
+                method: 'POST',
+                url: $scope.saveUrl,
+                data: { 'data': $scope.ModelNew },
+                dataType: 'JSON'
+            }).then(function successCallback(response) {
+                if (response.data.Error === true) {
+                    ShowResult(response.data.Message, 'failure');
+                }
+                else {
+                    ShowResult(response.data.Message, 'success');
+                    ClearFields();
+                    $scope.getData();
+
+                }
+            }), function errorCallBack(response) {
+                ShowResult(response.data.Message, 'failure');
+            }
+
+        }
+    };
+
+    $scope.Delete = function () {
+        if (!baseService.isUndefinedOrNull($scope.ModelNew.Id)) {
+            $http({
+                method: 'POST',
+                url: $scope.deleteUrl + $scope.ModelNew.Id,
+                dataType: 'JSON'
+            }).then(function successCallback(response) {
+                if (response.data.Error === true) {
+                    ShowResult(response.data.Message, 'failure');
+                }
+                else {
+                    ShowResult(response.data.Message, 'success');
+                    ClearFields();
+                    $scope.getData();
+                }
+                function errorCallBack(response) {
+                    ShowResult(response.data.Message, 'failure');
+                }
+            });
+        }
+    };
+
+    $scope.Clear = function () {
+        ClearFields();
+        return true;
+    };
+
+    function ClearFields() {
+        $scope.Action = 'Save';
+        $scope.ModelNew = Object.assign({}, $scope.ModelTemp);
+    }
+
+    $scope.recipeMaterialFilterList = [
+        { 'name': 'Master Order No', 'value': 'MasterOrderNo' },
+        { 'name': 'Buyer Order#', 'value': 'BuyerOrderNo' },
+        { 'name': 'Own Order#', 'value': 'OwnOrderNo' },
+        { 'name': 'Buyer Item#', 'value': 'BuyerReferenceNo' },
+        { 'name': 'Own Item#', 'value': 'OwnReferenceNo' },
+        {
+            'name': 'Material',
+            'value': 'MaterialMasterName'
+        },
+        {
+            'name': 'Product Name',
+            'value': 'ProductName'
+        },
+        {
+            'name': 'Buyer',
+            'value': 'Buyer'
+        },
+        {
+            'name': 'Article',
+            'value': 'Article'
+        },
+        {
+            'name': 'Customer',
+            'value': 'Customer'
+        },
+        {
+            'name': 'Commitment Date',
+            'value': 'CommitmentDate'
+        },
+        {
+            'name': 'Destination',
+            'value': 'DestinationName'
+        },
+        {
+            'name': 'Shipment Mode',
+            'value': 'ShipmentModeName'
+        },
+        {
+            'name': 'PO Number',
+            'value': 'PONumber'
+        }
+    ];
+
+    $scope.recipeMaterialParameters = {
+        limit: 10
+        , offset: 0
+        , order: 'asc'
+        , sort: 'MaterialMasterName, ArticleName'
+        , searchBy: 'MaterialMasterName'
+        , pageSize: 10
+        , total_count: 0
+        , search: null
+        , serverPagination: true
+    };
+    $scope.recipeMaterialList = [];
+    $scope.recipeMaterialParameters.searchBy = "MaterialMasterName";
+    $scope.recipeMaterialParameters.search = "";
+    $scope.recipeMaterialPopUp = function () {
+        angular.element(document.querySelector('#recipeMaterialPopUp')).modal('show'); 
+        $scope.serachSoMaterial();
+
+    };
+
+
+    $scope.serachSoMaterial = function serachSoMaterial() {
+        
+        $http({
+            method: 'GET',
+            url: $scope.path + 'GetSRSalesOrderListSearch?column=' + $scope.recipeMaterialParameters.searchBy + '&value=' + $scope.recipeMaterialParameters.search + "&packetRegistrationMasterId=" + $scope.ModelNew.Id
+        }).then(function successCallback(response) {
+
+            for (var i = 0; i < response.data.length; i++) {
+                for (var J = 0; J < $scope.recipeMaterialListSelected.length; J++) {
+                    if (response.data[i].SalesOrderId == $scope.recipeMaterialListSelected[J].SalesOrderId)
+                        response.data[i].Checked = true;
+                }
+            }
+            $scope.MaterialID = "";//important for changing color
+            $scope.recipeMaterialList = response.data;
+
+        });
+
+
+    }
+
+    $scope.recipeMaterialListSelected = [];
+    $scope.addRecipeMaterial = function () {
+
+        try {
+            var id = "";
+            var productid = "";
+            var groupid = "";
+            for (var i = 0; i < $scope.recipeMaterialList.length; i++) {
+                if ($scope.recipeMaterialList[i].Checked == true) {
+                   
+                    if (baseService.isUndefinedOrNull($scope.recipeMaterialList[i].ArticleId)
+                        || $scope.recipeMaterialList[i].ArticleId == "") {
+                        throw "Sales order items without product are not allowed";
+                    }
+
+                    if (id == "")
+                        id = $scope.recipeMaterialList[i].ArticleId;
+
+                    if (productid == "")
+                        productid = $scope.recipeMaterialList[i].ProductID;
+
+                    if (groupid == "")
+                        groupid = $scope.recipeMaterialList[i].ProductionGrouping;
+
+                    if (!baseService.isUndefinedOrNull($scope.recipeMaterialList[i].ProductionGrouping)) {
+                        if ($scope.recipeMaterialList[i].ProductionGrouping != groupid) {
+                            throw "Selecting different group materials are not allowed";
+                        }
+                        else {
+                            if ($scope.recipeMaterialList[i].ArticleId != id) {
+                                $scope.message_DiffArticleconfirmation = 'You are going to add different articles. Are you sure?';
+                                angular.element(document.querySelector('#confirmDiffArticlePopUp')).modal('show');
+                            }
+                        }
+
+                    } else {
+                        if ($scope.recipeMaterialList[i].ArticleId != id)
+                            throw "Selecting different articles are not allowed";
+
+                    }
+                }
+            }
+
+            $scope.recipeMaterialListSelected = [];
+            for (var i = 0; i < $scope.recipeMaterialList.length; i++) {
+                if ($scope.recipeMaterialList[i].Checked == true) {
+                    $scope.recipeMaterialListSelected.push($scope.recipeMaterialList[i]);
+                }
+            }
+
+            if (baseService.isUndefinedOrNull($scope.message_DiffArticleconfirmation)) {
+                $scope.CloseRecipeMaterialPopUp();
+            }
+        } catch (e) {
+            ShowResult(e, 'failure', 'recipeMaterialPopUp');
+        }
+    };
 
 
 
